ff481046eab148add98440d2996be629c0f2245e
Daenarys <androidwoz@gmail.com>
2024-03-07 23:19:12 +0100

    skip collapse/expand buttons

--- ClassicFrames/skins/Quests.lua
+++ ClassicFrames/skins/Quests.lua
@@ -100,6 +100,39 @@ ApplyScrollBarArrow(QuestRewardScrollFrame.ScrollBar)
 ApplyScrollBarTrack(QuestRewardScrollFrame.ScrollBar.Track)
 ApplyScrollBarThumb(QuestRewardScrollFrame.ScrollBar.Track.Thumb)
 
+hooksecurefunc(_G.CampaignCollapseButtonMixin, 'UpdateState', function(self, isCollapsed)
+	if isCollapsed then
+		self:SetNormalTexture("Interface\\Buttons\\UI-PlusButton-Up")
+		self:SetPushedTexture("Interface\\Buttons\\UI-PlusButton-Down")
+	else
+		self:SetNormalTexture("Interface\\Buttons\\UI-MinusButton-Up")
+		self:SetPushedTexture("Interface\\Buttons\\UI-MinusButton-Down")
+	end
+	self:SetSize(16, 16)
+end)
+
+hooksecurefunc('QuestLogQuests_Update', function()
+		for _, child in next, { _G.QuestMapFrame.QuestsFrame.Contents:GetChildren() } do
+		if child.ButtonText and not child.questID then
+			child:SetSize(16, 16)
+			for _, tex in next, { child:GetRegions() } do
+				if tex.GetAtlas then
+					local atlas = tex:GetAtlas()
+					if atlas == 'Campaign_HeaderIcon_Closed' then
+						tex:SetTexture("Interface\\Buttons\\UI-PlusButton-Up")
+					elseif atlas == 'Campaign_HeaderIcon_ClosedPressed' then
+						tex:SetTexture("Interface\\Buttons\\UI-PlusButton-Down")
+					elseif atlas == 'Campaign_HeaderIcon_Open' then
+						tex:SetTexture("Interface\\Buttons\\UI-MinusButton-Up")
+					elseif atlas == 'Campaign_HeaderIcon_OpenPressed' then
+						tex:SetTexture("Interface\\Buttons\\UI-MinusButton-Down")
+					end
+				end
+			end
+		end
+	end
+end)
+
 hooksecurefunc(_G.QuestSessionManager, 'NotifyDialogShow', function(_, dialog)
 	if dialog.isSkinned then return end
 
