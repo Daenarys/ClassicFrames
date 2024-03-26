c88dbad22d73c8dd5c4e3f2e59a7cc2ac817a85f
Daenarys <androidwoz@gmail.com>
2024-03-26 11:34:56 +0100

    skinned zonemap

---
+++ ClassicFrames/skins/ZoneMap.lua
@@ -0,0 +1,17 @@
+local f = CreateFrame("Frame")
+f:RegisterEvent("ADDON_LOADED")
+f:SetScript("OnEvent", function(self, event, name)
+	if name == "Blizzard_BattlefieldMap" then
+		ApplyCloseButton(BattlefieldMapFrame.BorderFrame.CloseButton)
+		BattlefieldMapFrame.BorderFrame.CloseButton:ClearAllPoints()
+		BattlefieldMapFrame.BorderFrame.CloseButton:SetPoint("TOPRIGHT", 5, 9)
+
+		if (BattlefieldMapFrame.BorderFrame.CloseButtonBorder == nil) then
+			BattlefieldMapFrame.BorderFrame.CloseButtonBorder = BattlefieldMapFrame.BorderFrame:CreateTexture(nil, "BORDER")
+			BattlefieldMapFrame.BorderFrame.CloseButtonBorder:SetSize(32, 32)
+			BattlefieldMapFrame.BorderFrame.CloseButtonBorder:SetTexture("Interface\\DialogFrame\\UI-DialogBox-Corner")
+			BattlefieldMapFrame.BorderFrame.CloseButtonBorder:ClearAllPoints()
+			BattlefieldMapFrame.BorderFrame.CloseButtonBorder:SetPoint("TOPRIGHT", 1, 5)
+		end
+	end
+end)
