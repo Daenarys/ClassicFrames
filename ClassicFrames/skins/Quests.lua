b3e4e1c9190ab258de0887ec58aa1ff9057929e6
Daenarys <androidwoz@gmail.com>
2024-04-28 00:14:57 +0200

    adjust title position

--- ClassicFrames/skins/Quests.lua
+++ ClassicFrames/skins/Quests.lua
@@ -3,3 +3,6 @@ if not _G.QuestFrame then return end
 QuestDetailScrollFrameTop:Hide()
 QuestDetailScrollFrameMiddle:Hide()
 QuestDetailScrollFrameBottom:Hide()
+
+QuestLogTitleText:ClearAllPoints()
+QuestLogTitleText:SetPoint("TOP", 0, -16)
