8b26745dc587fde4a42eca200ba6adc117053b3b
Daenarys <androidwoz@gmail.com>
2024-04-28 00:22:03 +0200

    it actually looked like this

--- ClassicFrames/skins/Quests.lua
+++ ClassicFrames/skins/Quests.lua
@@ -3,6 +3,3 @@ if not _G.QuestFrame then return end
 QuestDetailScrollFrameTop:Hide()
 QuestDetailScrollFrameMiddle:Hide()
 QuestDetailScrollFrameBottom:Hide()
-
-QuestLogTitleText:ClearAllPoints()
-QuestLogTitleText:SetPoint("TOP", 0, -16)
