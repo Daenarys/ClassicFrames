96a6a3b67766ebe49b491048f82686b0eeff1408
Daenarys <androidwoz@gmail.com>
2024-04-27 20:44:38 +0200

    adjust health/mana bar fonts + positions

--- ClassicFrames/skins/unitframes/PlayerFrame.lua
+++ ClassicFrames/skins/unitframes/PlayerFrame.lua
@@ -1,10 +1,31 @@
 if not _G.PlayerFrame then return end
 
+PlayerFrameHealthBarText:SetFont("Fonts\\FRIZQT__.TTF", 10, "OUTLINE")
+PlayerFrameHealthBarTextLeft:SetFont("Fonts\\FRIZQT__.TTF", 10, "OUTLINE")
+PlayerFrameHealthBarTextRight:SetFont("Fonts\\FRIZQT__.TTF", 10, "OUTLINE")
+
+PlayerFrameHealthBarText:SetTextColor(1, 1, 1)
+PlayerFrameHealthBarTextLeft:SetTextColor(1, 1, 1)
+PlayerFrameHealthBarTextRight:SetTextColor(1, 1, 1)
+
+PlayerFrameHealthBarTextLeft:ClearAllPoints()
+PlayerFrameHealthBarTextLeft:SetPoint("LEFT", 111, 3)
+
+PlayerFrameManaBarText:SetFont("Fonts\\FRIZQT__.TTF", 10, "OUTLINE")
+PlayerFrameManaBarTextLeft:SetFont("Fonts\\FRIZQT__.TTF", 10, "OUTLINE")
+PlayerFrameManaBarTextRight:SetFont("Fonts\\FRIZQT__.TTF", 10, "OUTLINE")
+
+PlayerFrameManaBarText:SetTextColor(1, 1, 1)
+PlayerFrameManaBarTextLeft:SetTextColor(1, 1, 1)
+PlayerFrameManaBarTextRight:SetTextColor(1, 1, 1)
+
+PlayerFrameManaBarTextLeft:ClearAllPoints()
+PlayerFrameManaBarTextLeft:SetPoint("LEFT", 111, -9)
+
 hooksecurefunc("PlayerFrame_ToPlayerArt", function()
 	PlayerName:ClearAllPoints()
 	PlayerName:SetPoint("CENTER", 51, 18)
 end)
-
 
 hooksecurefunc("PlayerFrame_UpdateLevelTextAnchor", function()
 	PlayerLevelText:ClearAllPoints()
--- ClassicFrames/skins/unitframes/TargetFrame.lua
+++ ClassicFrames/skins/unitframes/TargetFrame.lua
@@ -3,6 +3,28 @@ if not _G.TargetFrame then return end
 TargetFrame.textureFrame.Name:ClearAllPoints()
 TargetFrame.textureFrame.Name:SetPoint("CENTER", -49, 18)
 
+TargetFrame.textureFrame.HealthBarText:SetFont("Fonts\\FRIZQT__.TTF", 10, "OUTLINE")
+TargetFrame.textureFrame.HealthBarTextLeft:SetFont("Fonts\\FRIZQT__.TTF", 10, "OUTLINE")
+TargetFrame.textureFrame.HealthBarTextRight:SetFont("Fonts\\FRIZQT__.TTF", 10, "OUTLINE")
+
+TargetFrame.textureFrame.HealthBarText:SetTextColor(1, 1, 1)
+TargetFrame.textureFrame.HealthBarTextLeft:SetTextColor(1, 1, 1)
+TargetFrame.textureFrame.HealthBarTextRight:SetTextColor(1, 1, 1)
+
+TargetFrame.textureFrame.HealthBarTextLeft:ClearAllPoints()
+TargetFrame.textureFrame.HealthBarTextLeft:SetPoint("LEFT", 8, 3)
+
+TargetFrame.textureFrame.ManaBarText:SetFont("Fonts\\FRIZQT__.TTF", 10, "OUTLINE")
+TargetFrame.textureFrame.ManaBarTextLeft:SetFont("Fonts\\FRIZQT__.TTF", 10, "OUTLINE")
+TargetFrame.textureFrame.ManaBarTextRight:SetFont("Fonts\\FRIZQT__.TTF", 10, "OUTLINE")
+
+TargetFrame.textureFrame.ManaBarText:SetTextColor(1, 1, 1)
+TargetFrame.textureFrame.ManaBarTextLeft:SetTextColor(1, 1, 1)
+TargetFrame.textureFrame.ManaBarTextRight:SetTextColor(1, 1, 1)
+
+TargetFrame.textureFrame.ManaBarTextLeft:ClearAllPoints()
+TargetFrame.textureFrame.ManaBarTextLeft:SetPoint("LEFT", 8, -9)
+
 hooksecurefunc("TargetFrame_UpdateLevelTextAnchor", function(self)
 	self.levelText:ClearAllPoints()
 	self.levelText:SetPoint("CENTER", 63, -17)
