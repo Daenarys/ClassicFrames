ca3a52951e23d98a5bad06aa21f56897c29b6f6e
Daenarys <androidwoz@gmail.com>
2024-04-27 23:34:43 +0200

    adjust petframe position

--- ClassicFrames/skins/unitframes/PetFrame.lua
+++ ClassicFrames/skins/unitframes/PetFrame.lua
@@ -7,3 +7,18 @@ end
 if PetFrameManaBarText then
 	PetFrameManaBarText:SetAlpha(0)
 end
+
+local _, class = UnitClass("player")
+if ( class == "DEATHKNIGHT" or class == "ROGUE") then
+	PetFrame:SetPoint("TOPLEFT", PlayerFrame, "TOPLEFT", 60, -75)
+elseif ( class == "SHAMAN" or class == "DRUID" ) then
+	PetFrame:SetPoint("TOPLEFT", PlayerFrame, "TOPLEFT", 60, -100)
+elseif ( class == "WARLOCK" ) then
+	PetFrame:SetPoint("TOPLEFT", PlayerFrame, "TOPLEFT", 60, -90)
+elseif ( class == "PALADIN" ) then
+	PetFrame:SetPoint("TOPLEFT", PlayerFrame, "TOPLEFT", 60, -90)
+elseif ( class == "PRIEST" ) then
+	PetFrame:SetPoint("TOPLEFT", PlayerFrame, "TOPLEFT", 60, -90)
+elseif ( class == "MONK" ) then
+	PetFrame:SetPoint("TOPLEFT", PlayerFrame, "TOPLEFT", 90, -100)
+end
