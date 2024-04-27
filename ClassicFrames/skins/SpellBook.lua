0b12bdda715be45372ddcb22da027791ef6820dc
Daenarys <androidwoz@gmail.com>
2024-04-27 12:01:00 +0200

    fix spellbook tabs

---
+++ ClassicFrames/skins/SpellBook.lua
@@ -0,0 +1,16 @@
+if not _G.SpellBookFrame then return end
+
+hooksecurefunc("SpellBookFrame_Update", function()
+	-- Make sure the correct tab is selected
+	for i=1,5 do
+		local tab = _G["SpellBookFrameTabButton"..i]
+		if ( tab.bookType == SpellBookFrame.bookType ) then
+			PanelTemplates_SelectTab(tab)
+		else
+			PanelTemplates_DeselectTab(tab)
+		end
+	end
+
+	SpellBookFrameTabButton1:ClearAllPoints()
+	SpellBookFrameTabButton1:SetPoint("TOPLEFT", SpellBookFrame, "BOTTOMLEFT", 0, 1)
+end)
