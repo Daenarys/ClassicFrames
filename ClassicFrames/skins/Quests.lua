ca5a2941109f6dc4fe8c4a98eba28ff104c2f311
Daenarys <androidwoz@gmail.com>
2024-04-28 14:31:42 +0200

    adjust several frame positions

--- ClassicFrames/skins/Quests.lua
+++ ClassicFrames/skins/Quests.lua
@@ -3,3 +3,8 @@ if not _G.QuestFrame then return end
 QuestDetailScrollFrameTop:Hide()
 QuestDetailScrollFrameMiddle:Hide()
 QuestDetailScrollFrameBottom:Hide()
+
+_G.QuestLogDetailFrame:HookScript("OnShow", function(self)
+	self:ClearAllPoints()
+	self:SetPoint("TOPLEFT", 0, -104)
+end)
