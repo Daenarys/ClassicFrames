aba9fec9b17bdf3823b533efc06e33071427891a
Daenarys <androidwoz@gmail.com>
2024-04-28 12:41:35 +0200

    adjust friendsframe tab positions

---
+++ ClassicFrames/skins/Friends.lua
@@ -0,0 +1,13 @@
+if not _G.FriendsFrame then return end
+
+FriendsFrameTab1:ClearAllPoints()
+FriendsFrameTab1:SetPoint("BOTTOMLEFT", 5, -30)
+
+FriendsFrameTab2:ClearAllPoints()
+FriendsFrameTab2:SetPoint("LEFT", FriendsFrameTab1, "RIGHT", -15, 0)
+
+FriendsFrameTab3:ClearAllPoints()
+FriendsFrameTab3:SetPoint("LEFT", FriendsFrameTab2, "RIGHT", -15, 0)
+
+FriendsFrameTab4:ClearAllPoints()
+FriendsFrameTab4:SetPoint("LEFT", FriendsFrameTab3, "RIGHT", -15, 0)
