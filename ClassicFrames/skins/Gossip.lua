c380883da3dc9aeeef30cf033cafa9059e989ebf
Daenarys <androidwoz@gmail.com>
2024-04-28 14:16:26 +0200

    adjust scrollbox + scrollbar positions

--- ClassicFrames/skins/Gossip.lua
+++ ClassicFrames/skins/Gossip.lua
@@ -7,3 +7,10 @@ GossipFrame.TitleContainer:ClearAllPoints()
 GossipFrame.TitleContainer:SetPoint("TOP", 0, -23)
 GossipFrame.TitleContainer:SetPoint("LEFT", GossipFramePortrait, "RIGHT", 10, 0)
 GossipFrame.TitleContainer:SetPoint("RIGHT", -58, 0)
+
+GossipFrame.GreetingPanel.ScrollBox:ClearAllPoints()
+GossipFrame.GreetingPanel.ScrollBox:SetPoint("TOPLEFT", 23, -81)
+
+GossipFrame.GreetingPanel.ScrollBar:ClearAllPoints()
+GossipFrame.GreetingPanel.ScrollBar:SetPoint("TOPLEFT", GossipFrame.GreetingPanel.ScrollBox, "TOPRIGHT", 1, 3)
+GossipFrame.GreetingPanel.ScrollBar:SetPoint("BOTTOMLEFT", GossipFrame.GreetingPanel.ScrollBox, "BOTTOMRIGHT", -1, -2)
