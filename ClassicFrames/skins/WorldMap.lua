9588c507c4ab147401c92aa1468f98c3fab80bde
Daenarys <androidwoz@gmail.com>
2024-04-30 11:42:51 +0200

    or noy

--- ClassicFrames/skins/WorldMap.lua
+++
@@ -1,9 +0,0 @@
-if not _G.WorldMapFrame then return end
-
-hooksecurefunc(WorldMapFrame, "SynchronizeDisplayState", function(self)
-	if self:IsMaximized() then
-		self:SetScale(1)
-	else
-		self:SetScale(0.85)
-	end
-end)
