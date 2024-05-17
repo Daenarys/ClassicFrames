61b8b048d9ccfb12f0914e62b274978318e2e39c
Daenarys <androidwoz@gmail.com>
2024-05-17 10:37:32 +0200

    skin scrollbar

--- ClassicFrames/skins/Socketing.lua
+++ ClassicFrames/skins/Socketing.lua
@@ -16,5 +16,14 @@ f:SetScript("OnEvent", function(self, event, name)
 
 		ApplyTitleBg(ItemSocketingFrame)
 		ApplyNineSlicePortrait(ItemSocketingFrame)
+
+		ItemSocketingScrollFrame.ScrollBar:SetSize(25, 560)
+		ItemSocketingScrollFrame.ScrollBar:ClearAllPoints()
+		ItemSocketingScrollFrame.ScrollBar:SetPoint("TOPLEFT", ItemSocketingScrollFrame, "TOPRIGHT", -21, 1)
+		ItemSocketingScrollFrame.ScrollBar:SetPoint("BOTTOMLEFT", ItemSocketingScrollFrame, "BOTTOMRIGHT", -21, -4)
+
+		ApplyScrollBarArrow(ItemSocketingScrollFrame.ScrollBar)
+		ApplyScrollBarTrack(ItemSocketingScrollFrame.ScrollBar.Track)
+		ApplyScrollBarThumb(ItemSocketingScrollFrame.ScrollBar.Track.Thumb)
 	end
 end)
