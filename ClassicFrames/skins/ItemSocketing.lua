1075cf01bc7eb013e37845c981c47dcd17aa4ccb
Daenarys <androidwoz@gmail.com>
2024-05-17 15:46:37 +0200

    rename

--- ClassicFrames/skins/ItemSocketing.lua
+++ ClassicFrames/skins/ItemSocketing.lua
@@ -20,7 +20,7 @@ f:SetScript("OnEvent", function(self, event, name)
 		ItemSocketingScrollFrame.ScrollBar:SetSize(25, 560)
 		ItemSocketingScrollFrame.ScrollBar:ClearAllPoints()
 		ItemSocketingScrollFrame.ScrollBar:SetPoint("TOPLEFT", ItemSocketingScrollFrame, "TOPRIGHT", -21, 1)
-		ItemSocketingScrollFrame.ScrollBar:SetPoint("BOTTOMLEFT", ItemSocketingScrollFrame, "BOTTOMRIGHT", -21, -4)
+		ItemSocketingScrollFrame.ScrollBar:SetPoint("BOTTOMLEFT", ItemSocketingScrollFrame, "BOTTOMRIGHT", -21, -5)
 
 		ApplyScrollBarArrow(ItemSocketingScrollFrame.ScrollBar)
 		ApplyScrollBarTrack(ItemSocketingScrollFrame.ScrollBar.Track)
