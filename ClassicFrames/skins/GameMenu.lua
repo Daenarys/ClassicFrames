f0596cbce3d92f42ad95c8ba9631774654126b83
Daenarys <androidwoz@gmail.com>
2024-05-27 00:09:21 +0200

    gamemenu fixes

--- ClassicFrames/skins/GameMenu.lua
+++ ClassicFrames/skins/GameMenu.lua
@@ -2,3 +2,24 @@ if not _G.GameMenuFrame then return end
 
 ApplyDialogBorder(GameMenuFrame.Border)
 ApplyDialogHeader(GameMenuFrame.Header)
+
+GameMenuFrame.Header:SetWidth(141)
+GameMenuFrame.Header.Text:SetFontObject("GameFontNormal")
+
+hooksecurefunc(GameMenuFrame, "Layout", function(self)
+	self:SetSize(195, 320)
+end)
+
+hooksecurefunc(GameMenuFrame, 'InitButtons', function(self)
+	local tableData = self.buttonPool.activeObjects
+	for obj in pairs(tableData) do 
+		obj:SetSize(144, 21)
+
+		obj:SetNormalFontObject("GameFontHighlight")
+		obj:SetHighlightFontObject("GameFontHighlight")
+		obj:SetDisabledFontObject("GameFontDisable")
+
+		obj:GetFontString():ClearAllPoints()
+		obj:GetFontString():SetPoint("CENTER", 0, 1)
+	end
+end)
