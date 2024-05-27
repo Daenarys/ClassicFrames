c22e29882a30548e8b4de3b3574ecc872f82884d
Daenarys <androidwoz@gmail.com>
2024-05-27 20:50:11 +0200

    adjust button padding

--- ClassicFrames/skins/GameMenu.lua
+++ ClassicFrames/skins/GameMenu.lua
@@ -10,7 +10,7 @@ hooksecurefunc(GameMenuFrame, "Layout", function(self)
 	self:SetSize(195, 320)
 end)
 
-hooksecurefunc(GameMenuFrame, 'InitButtons', function(self)
+hooksecurefunc(GameMenuFrame, "InitButtons", function(self)
 	local tableData = self.buttonPool.activeObjects
 	for obj in pairs(tableData) do 
 		obj:SetSize(144, 21)
@@ -21,5 +21,13 @@ hooksecurefunc(GameMenuFrame, 'InitButtons', function(self)
 
 		obj:GetFontString():ClearAllPoints()
 		obj:GetFontString():SetPoint("CENTER", 0, 1)
+
+		obj:SetPoint("CENTER", GameMenuFrame, "TOP", 0, -42)
 	end
+end)
+
+GameMenuFrame:HookScript("OnShow", function(self)
+	self.topPadding = 32
+	self.leftPadding = 26
+	self.rightPadding = 26
 end)
