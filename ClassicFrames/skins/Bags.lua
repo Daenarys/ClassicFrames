6fd0aba920fda600fef62047d23ba3637c340e66
Daenarys <androidwoz@gmail.com>
2024-05-27 17:34:04 +0200

    bag cleanup

--- ClassicFrames/skins/Bags.lua
+++ ClassicFrames/skins/Bags.lua
@@ -26,26 +26,11 @@ local CONTAINER_BOTTOM_TEXTURE_DEFAULT_HEIGHT = CONTAINER_BOTTOM_TEXTURE_DEFAULT
 local CONTAINER_BOTTOM_TEXTURE_DEFAULT_ROW_END = 172;
 local CONTAINER_BOTTOM_TEXTURE_DEFAULT_ROW_HEIGHT = CONTAINER_BOTTOM_TEXTURE_DEFAULT_ROW_END - CONTAINER_BOTTOM_TEXTURE_DEFAULT_START;
 
-local function GetRightActionBarWidth()
-	local offset = 0;
-	if MultiBar3_IsVisible and MultiBar3_IsVisible() and MultiBarRight:IsInDefaultPosition() then
-		local point, relativeTo, relativePoint, offsetX, offsetY = MultiBarRight:GetPoint(1);
-		offset = MultiBarRight:GetWidth() - offsetX; -- Subtract x offset since it will be a negative value due to us anchoring to the right side and anchoring towards the middle
-	end
-
-	if MultiBar4_IsVisible and MultiBar4_IsVisible() and MultiBarLeft:IsInDefaultPosition() then
-		local point, relativeTo, relativePoint, offsetX, offsetY = MultiBarLeft:GetPoint(1);
-		offset = MultiBarLeft:GetWidth() - offsetX;
-	end
-
-	return offset + 10;
+if ContainerFrame1Portrait then
+	ContainerFrame1Portrait:Hide()
 end
-
-if _G.ContainerFrame1Portrait then
-	_G.ContainerFrame1Portrait:Hide()
-end
-if _G.ContainerFrame1MoneyFrame.Border then
-	_G.ContainerFrame1MoneyFrame.Border:Hide()
+if ContainerFrame1MoneyFrame.Border then
+	ContainerFrame1MoneyFrame.Border:Hide()
 end
 
 for i = 1, _G.NUM_CONTAINER_FRAMES do
@@ -110,38 +95,20 @@ for i = 1, _G.NUM_CONTAINER_FRAMES do
 		_G['ContainerFrame'..i..'BackgroundBottom']:SetPoint("TOP", _G['ContainerFrame'..i..'BackgroundMiddle1'], "BOTTOM")
 	end
 
-	for k = 1, 36 do
-		if _G['ContainerFrame'..i..'Item'..k] then
-			_G['ContainerFrame'..i..'Item'..k].emptyBackgroundAtlas = nil
-		end
-    end
-    	
-    for k = 1, 4 do
-		if _G['ContainerFrame'..i..'Item'..k] then
-			hooksecurefunc(_G['ContainerFrame'..i..'Item'..k], "UpdateExtended", function(self)
-				if self.extendedFrame and self.extendedFrame:IsShown() then
-					self.extendedFrame:DisableDrawLayer("BACKGROUND")
-					if (self.extendedFrame.Icon == nil) then
-						self.extendedFrame.Icon = self.extendedFrame:CreateTexture(nil, "ARTWORK")
-						self.extendedFrame.Icon:SetSize(27, 35)
-						self.extendedFrame.Icon:SetAtlas("worldquest-tracker-lock")
-						self.extendedFrame.Icon:SetPoint("CENTER")
-					end
-				end
-			end)
-		end
-    end
-
     hooksecurefunc(_G['ContainerFrame'..i], "SetSearchBoxPoint", function(self, searchBox)
-		searchBox:SetPoint("TOPLEFT", self, "TOPLEFT", 54, -37);
-		searchBox:SetWidth(96);
+		searchBox:SetPoint("TOPLEFT", self, "TOPLEFT", 54, -37)
+		searchBox:SetWidth(96)
+	end)
+
+	hooksecurefunc(_G['ContainerFrame'..i], "UpdateItemLayout", function(self)
+		for i, itemButton in self:EnumerateValidItems() do
+			itemButton.emptyBackgroundAtlas = nil
+		end
 	end)
 end
 
 hooksecurefunc("ContainerFrame_GenerateFrame", function(frame, size, id)
-	if frame:IsCombinedBagContainer() then return end
-
-	local name = frame:GetName();
+	local name = frame:GetName()
 	local bgTextureTop = _G[name.."BackgroundTop"];
 	local bgTextureMiddle = _G[name.."BackgroundMiddle1"];
 	local bgTextureMiddle2 = _G[name.."BackgroundMiddle2"];
@@ -153,31 +120,31 @@ hooksecurefunc("ContainerFrame_GenerateFrame", function(frame, size, id)
 
 	-- if id = 0 then its the backpack
 	if ( id == 0 ) then
-		bgTexture1Slot:Hide();
+		bgTexture1Slot:Hide()
 
 		local extended = size > BACKPACK_BASE_SIZE;
 		local extraRows = 0;
 
-		bgTextureTop:SetTexture("Interface\\ContainerFrame\\UI-BackpackBackground");
+		bgTextureTop:SetTexture("Interface\\ContainerFrame\\UI-BackpackBackground")
 		if (extended) then
-			extraRows = math.ceil((size - BACKPACK_BASE_SIZE) / columns);
-			bgTextureTop:SetHeight(BACKPACK_EXTENDED_TOPHEIGHT);
-			bgTextureTop:SetTexCoord(0, 1, 0, BACKPACK_EXTENDED_TOPHEIGHT / BACKPACK_DEFAULT_TOPHEIGHT);
-			backpackFirstButtonOffset = backpackFirstButtonOffset - (ROW_HEIGHT * extraRows);	
+			extraRows = math.ceil((size - BACKPACK_BASE_SIZE) / columns)
+			bgTextureTop:SetHeight(BACKPACK_EXTENDED_TOPHEIGHT)
+			bgTextureTop:SetTexCoord(0, 1, 0, BACKPACK_EXTENDED_TOPHEIGHT / BACKPACK_DEFAULT_TOPHEIGHT)
+			backpackFirstButtonOffset = backpackFirstButtonOffset - (ROW_HEIGHT * extraRows)	
 		else
-			bgTextureTop:SetHeight(BACKPACK_DEFAULT_TOPHEIGHT);
-			bgTextureTop:SetTexCoord(0, 1, 0, 1);
+			bgTextureTop:SetHeight(BACKPACK_DEFAULT_TOPHEIGHT)
+			bgTextureTop:SetTexCoord(0, 1, 0, 1)
 		end
-		bgTextureTop:Show();
-		bgTextureBottom:Hide();
+		bgTextureTop:Show()
+		bgTextureBottom:Hide()
 
 		_G[name.."MoneyFrame"]:ClearAllPoints()
-		_G[name.."MoneyFrame"]:SetPoint("TOPRIGHT", frame, "TOPRIGHT", -2, BACKPACK_MONEY_OFFSET_DEFAULT - (ROW_HEIGHT * extraRows));
+		_G[name.."MoneyFrame"]:SetPoint("TOPRIGHT", frame, "TOPRIGHT", -2, BACKPACK_MONEY_OFFSET_DEFAULT - (ROW_HEIGHT * extraRows))
 
 		-- Hide unused textures
 		for i=1, MAX_MIDDLE_TEXTURES do
-			_G[name.."BackgroundMiddle"..i]:SetTexture("Interface\\ContainerFrame\\UI-Bag-Components");
-			_G[name.."BackgroundMiddle"..i]:Hide();
+			_G[name.."BackgroundMiddle"..i]:SetTexture("Interface\\ContainerFrame\\UI-Bag-Components")
+			_G[name.."BackgroundMiddle"..i]:Hide()
 		end
 
 		local middleBgHeight = 0;
@@ -186,7 +153,7 @@ hooksecurefunc("ContainerFrame_GenerateFrame", function(frame, size, id)
 			local remainingRows = extraRows;
 
 			-- Calculate the number of background textures we're going to need
-			local bgTextureCount = ceil(remainingRows/ROWS_IN_BG_TEXTURE);
+			local bgTextureCount = ceil(remainingRows/ROWS_IN_BG_TEXTURE)
 			local height;
 
 			-- Try to cycle all the middle bg textures
@@ -201,50 +168,50 @@ hooksecurefunc("ContainerFrame_GenerateFrame", function(frame, size, id)
 				end
 
 				middleBgHeight = middleBgHeight + height;
-				bgTextureMiddle:SetHeight(height);
-				bgTextureMiddle:SetTexCoord(0, 1, BG_TEXTURE_MIDDLE_START / BG_TEXTURE_HEIGHT, ((BG_TEXTURE_MIDDLE_START + height) / BG_TEXTURE_HEIGHT) );
-				bgTextureMiddle:Show();
+				bgTextureMiddle:SetHeight(height)
+				bgTextureMiddle:SetTexCoord(0, 1, BG_TEXTURE_MIDDLE_START / BG_TEXTURE_HEIGHT, ((BG_TEXTURE_MIDDLE_START + height) / BG_TEXTURE_HEIGHT) )
+				bgTextureMiddle:Show()
 			end
 			
 			-- Position and setup bottom texture
-			bgTextureBottom:SetPoint("TOP", bgTextureMiddle:GetName(), "BOTTOM", 0, 0);
-			bgTextureBottom:SetTexture("Interface\\ContainerFrame\\UI-BackpackBackground");
-			bgTextureBottom:SetHeight(BACKPACK_DEFAULT_TOPHEIGHT - BACKPACK_EXTENDED_TOPHEIGHT);
-			bgTextureBottom:SetTexCoord(0, 1, BACKPACK_EXTENDED_TOPHEIGHT / BACKPACK_DEFAULT_TOPHEIGHT, 1);
-			bgTextureBottom:Show();
+			bgTextureBottom:SetPoint("TOP", bgTextureMiddle:GetName(), "BOTTOM", 0, 0)
+			bgTextureBottom:SetTexture("Interface\\ContainerFrame\\UI-BackpackBackground")
+			bgTextureBottom:SetHeight(BACKPACK_DEFAULT_TOPHEIGHT - BACKPACK_EXTENDED_TOPHEIGHT)
+			bgTextureBottom:SetTexCoord(0, 1, BACKPACK_EXTENDED_TOPHEIGHT / BACKPACK_DEFAULT_TOPHEIGHT, 1)
+			bgTextureBottom:Show()
 		end
 
 		BACKPACK_HEIGHT = BACKPACK_BASE_HEIGHT + middleBgHeight;
-		local tokenFrame = ContainerFrameSettingsManager:GetTokenTrackerIfShown(frame);
+		local tokenFrame = ContainerFrameSettingsManager:GetTokenTrackerIfShown(frame)
 		if tokenFrame then
 			if not tokenFrame.BG then
 				tokenFrame.BG = tokenFrame:CreateTexture(nil, "BACKGROUND")
-				tokenFrame.BG:SetTexture("Interface\\ContainerFrame\\UI-Backpack-TokenFrame");
-				tokenFrame.BG:SetSize(256, 32);
-				tokenFrame.BG:ClearAllPoints();
-				tokenFrame.BG:SetPoint("TOPLEFT");
+				tokenFrame.BG:SetTexture("Interface\\ContainerFrame\\UI-Backpack-TokenFrame")
+				tokenFrame.BG:SetSize(256, 32)
+				tokenFrame.BG:ClearAllPoints()
+				tokenFrame.BG:SetPoint("TOPLEFT")
 			end
 			tokenFrame:SetSize(183, 32)
 			tokenFrame.Border:Hide()
 			tokenFrame:ClearAllPoints()
 			tokenFrame:SetPoint("BOTTOMLEFT", frame, "BOTTOMLEFT", 9, 0)
-			frame:SetHeight(BACKPACK_HEIGHT+BACKPACK_TOKENFRAME_HEIGHT);
+			frame:SetHeight(BACKPACK_HEIGHT+BACKPACK_TOKENFRAME_HEIGHT)
 		else
-			frame:SetHeight(BACKPACK_HEIGHT);
+			frame:SetHeight(BACKPACK_HEIGHT)
 		end
 	else
-		bgTextureBottom:SetHeight(CONTAINER_BOTTOM_TEXTURE_DEFAULT_HEIGHT);
-		bgTextureBottom:SetTexCoord(0, 1, CONTAINER_BOTTOM_TEXTURE_DEFAULT_START / BG_TEXTURE_HEIGHT, CONTAINER_BOTTOM_TEXTURE_DEFAULT_END / BG_TEXTURE_HEIGHT);
+		bgTextureBottom:SetHeight(CONTAINER_BOTTOM_TEXTURE_DEFAULT_HEIGHT)
+		bgTextureBottom:SetTexCoord(0, 1, CONTAINER_BOTTOM_TEXTURE_DEFAULT_START / BG_TEXTURE_HEIGHT, CONTAINER_BOTTOM_TEXTURE_DEFAULT_END / BG_TEXTURE_HEIGHT)
 		if (size == 1) then
 			-- Halloween gag gift
-			bgTexture1Slot:Show();
-			bgTextureTop:Hide();
-			bgTextureMiddle:Hide();
-			bgTextureMiddle2:Hide();
-			bgTextureBottom:Hide();
+			bgTexture1Slot:Show()
+			bgTextureTop:Hide()
+			bgTextureMiddle:Hide()
+			bgTextureMiddle2:Hide()
+			bgTextureBottom:Hide()
 		else
-			bgTexture1Slot:Hide();
-			bgTextureTop:Show();
+			bgTexture1Slot:Hide()
+			bgTextureTop:Show()
 	
 			-- Not the backpack
 			-- Set whether or not its a bank bag
@@ -254,12 +221,12 @@ hooksecurefunc("ContainerFrame_GenerateFrame", function(frame, size, id)
 			end
 			
 			-- Set textures
-			bgTextureTop:SetTexture("Interface\\ContainerFrame\\UI-Bag-Components"..bagTextureSuffix);
+			bgTextureTop:SetTexture("Interface\\ContainerFrame\\UI-Bag-Components"..bagTextureSuffix)
 			for i=1, MAX_MIDDLE_TEXTURES do
-				_G[name.."BackgroundMiddle"..i]:SetTexture("Interface\\ContainerFrame\\UI-Bag-Components"..bagTextureSuffix);
-				_G[name.."BackgroundMiddle"..i]:Hide();
+				_G[name.."BackgroundMiddle"..i]:SetTexture("Interface\\ContainerFrame\\UI-Bag-Components"..bagTextureSuffix)
+				_G[name.."BackgroundMiddle"..i]:Hide()
 			end
-			bgTextureBottom:SetTexture("Interface\\ContainerFrame\\UI-Bag-Components"..bagTextureSuffix);
+			bgTextureBottom:SetTexture("Interface\\ContainerFrame\\UI-Bag-Components"..bagTextureSuffix)
 						
 			local bgTextureCount, height;
 			-- Subtract one, since the top texture contains one row already
@@ -273,29 +240,29 @@ hooksecurefunc("ContainerFrame_GenerateFrame", function(frame, size, id)
 
 			-- Bag background display stuff
 			if ( isPlusTwoBag ) then
-				bgTextureTop:SetTexCoord(0, 1, BG_TEXTURE_TOP_PLUS_TWO_START / BG_TEXTURE_HEIGHT, BG_TEXTURE_TOP_PLUS_TWO_END / BG_TEXTURE_HEIGHT);
-				bgTextureTop:SetHeight(BG_TEXTURE_TOP_PLUS_TWO_END - BG_TEXTURE_TOP_PLUS_TWO_START);
+				bgTextureTop:SetTexCoord(0, 1, BG_TEXTURE_TOP_PLUS_TWO_START / BG_TEXTURE_HEIGHT, BG_TEXTURE_TOP_PLUS_TWO_END / BG_TEXTURE_HEIGHT)
+				bgTextureTop:SetHeight(BG_TEXTURE_TOP_PLUS_TWO_END - BG_TEXTURE_TOP_PLUS_TWO_START)
 			else
 				if ( rows == 1 ) then
 					-- If only one row chop off the bottom of the texture
-					bgTextureTop:SetTexCoord(0, 1, BG_TEXTURE_TOP_START / BG_TEXTURE_HEIGHT, BG_TEXTURE_TOP_ONE_ROW_END / BG_TEXTURE_HEIGHT);
-					bgTextureTop:SetHeight(BG_TEXTURE_TOP_ONE_ROW_END - BG_TEXTURE_TOP_START);
+					bgTextureTop:SetTexCoord(0, 1, BG_TEXTURE_TOP_START / BG_TEXTURE_HEIGHT, BG_TEXTURE_TOP_ONE_ROW_END / BG_TEXTURE_HEIGHT)
+					bgTextureTop:SetHeight(BG_TEXTURE_TOP_ONE_ROW_END - BG_TEXTURE_TOP_START)
 				else
-					bgTextureTop:SetTexCoord(0, 1, BG_TEXTURE_TOP_START / BG_TEXTURE_HEIGHT, BG_TEXTURE_TOP_END / BG_TEXTURE_HEIGHT);
-					bgTextureTop:SetHeight(BG_TEXTURE_TOP_END - BG_TEXTURE_TOP_START);
+					bgTextureTop:SetTexCoord(0, 1, BG_TEXTURE_TOP_START / BG_TEXTURE_HEIGHT, BG_TEXTURE_TOP_END / BG_TEXTURE_HEIGHT)
+					bgTextureTop:SetHeight(BG_TEXTURE_TOP_END - BG_TEXTURE_TOP_START)
 				end
 			end
 			-- Calculate the number of background textures we're going to need
-			bgTextureCount = ceil(remainingRows/ROWS_IN_BG_TEXTURE);
+			bgTextureCount = ceil(remainingRows/ROWS_IN_BG_TEXTURE)
 			
 			local middleBgHeight = 0;
 			-- If one row only special case
 			if ( rows == 1 ) then
-				bgTextureBottom:SetPoint("TOP", bgTextureMiddle:GetName(), "TOP", 0, 0);
-				bgTextureBottom:Show();
+				bgTextureBottom:SetPoint("TOP", bgTextureMiddle:GetName(), "TOP", 0, 0)
+				bgTextureBottom:Show()
 				-- Hide middle bg textures
 				for i=1, MAX_MIDDLE_TEXTURES do
-					_G[name.."BackgroundMiddle"..i]:Hide();
+					_G[name.."BackgroundMiddle"..i]:Hide()
 				end
 			else
 				-- Try to cycle all the middle bg textures
@@ -317,57 +284,78 @@ hooksecurefunc("ContainerFrame_GenerateFrame", function(frame, size, id)
 					end
 
 					middleBgHeight = middleBgHeight + height;
-					bgTextureMiddle:SetHeight(height);
-					bgTextureMiddle:SetTexCoord(0, 1, BG_TEXTURE_MIDDLE_START / BG_TEXTURE_HEIGHT, ((BG_TEXTURE_MIDDLE_START + height) / BG_TEXTURE_HEIGHT) );
-					bgTextureMiddle:Show();
+					bgTextureMiddle:SetHeight(height)
+					bgTextureMiddle:SetTexCoord(0, 1, BG_TEXTURE_MIDDLE_START / BG_TEXTURE_HEIGHT, ((BG_TEXTURE_MIDDLE_START + height) / BG_TEXTURE_HEIGHT) )
+					bgTextureMiddle:Show()
 				end
 				-- Position bottom texture
-				bgTextureBottom:SetPoint("TOP", bgTextureMiddle:GetName(), "BOTTOM", 0, 0);
-				bgTextureBottom:Show();
+				bgTextureBottom:SetPoint("TOP", bgTextureMiddle:GetName(), "BOTTOM", 0, 0)
+				bgTextureBottom:Show()
 			end
 				
 			-- Set the frame height
-			frame:SetHeight(bgTextureTop:GetHeight()+bgTextureBottom:GetHeight()+middleBgHeight);	
+			frame:SetHeight(bgTextureTop:GetHeight()+bgTextureBottom:GetHeight()+middleBgHeight)	
 		end
 	end
 
 	if (size == 1) then
 		-- Halloween gag gift
-		frame:SetHeight(70);	
-		frame:SetWidth(99);
-		local itemButton = _G[name.."Item1"];
-		itemButton:SetPoint("BOTTOMRIGHT", name, "BOTTOMRIGHT", -10, 5);
+		frame:SetHeight(70)	
+		frame:SetWidth(99)
+		local itemButton = frame.Items[1]
+		itemButton:SetPoint("BOTTOMRIGHT", name, "BOTTOMRIGHT", -10, 5)
 	else
-		frame:SetWidth(CONTAINER_WIDTH);
+		frame:SetWidth(CONTAINER_WIDTH)
 
-		local baseSize = ContainerFrame_GetContainerNumSlots(id);
-		local index, itemButton;
 		for i=1, size, 1 do
-			index = size - i + 1;
-			itemButton = _G[name.."Item"..i];
+			local itemButton = frame.Items[i]
 			-- Set first button
 			if ( i == 1 ) then
 				-- Anchor the first item differently if its the backpack frame
 				if ( id == 0 ) then
-					itemButton:SetPoint("BOTTOMRIGHT", name, "TOPRIGHT", -12, backpackFirstButtonOffset);
+					itemButton:SetPoint("BOTTOMRIGHT", name, "TOPRIGHT", -12, backpackFirstButtonOffset)
 				else
-					itemButton:SetPoint("BOTTOMRIGHT", name, "BOTTOMRIGHT", -12, 9);
+					itemButton:SetPoint("BOTTOMRIGHT", name, "BOTTOMRIGHT", -12, 9)
 				end
 			else
 				if ( mod((i-1), columns) == 0 ) then
-					itemButton:SetPoint("BOTTOMRIGHT", name.."Item"..(i - columns), "TOPRIGHT", 0, 4);	
+					itemButton:SetPoint("BOTTOMRIGHT", frame.Items[i - columns], "TOPRIGHT", 0, 4)	
 				else
-					itemButton:SetPoint("BOTTOMRIGHT", name.."Item"..(i - 1), "BOTTOMLEFT", -5, 0);	
+					itemButton:SetPoint("BOTTOMRIGHT", frame.Items[i - 1], "BOTTOMLEFT", -5, 0)	
+				end
+			end
+			if itemButton.extendedFrame and itemButton.extendedFrame:IsShown() then
+				itemButton.extendedFrame:DisableDrawLayer("BACKGROUND")
+				if (itemButton.extendedFrame.Icon == nil) then
+					itemButton.extendedFrame.Icon = itemButton.extendedFrame:CreateTexture(nil, "ARTWORK")
+					itemButton.extendedFrame.Icon:SetSize(27, 35)
+					itemButton.extendedFrame.Icon:SetAtlas("worldquest-tracker-lock")
+					itemButton.extendedFrame.Icon:SetPoint("CENTER")
 				end
 			end
 		end
 	end
 end)
 
+local function GetRightActionBarWidth()
+	local offset = 0;
+	if MultiBar3_IsVisible and MultiBar3_IsVisible() and MultiBarRight:IsInDefaultPosition() then
+		local point, relativeTo, relativePoint, offsetX, offsetY = MultiBarRight:GetPoint(1)
+		offset = MultiBarRight:GetWidth() - offsetX; -- Subtract x offset since it will be a negative value due to us anchoring to the right side and anchoring towards the middle
+	end
+
+	if MultiBar4_IsVisible and MultiBar4_IsVisible() and MultiBarLeft:IsInDefaultPosition() then
+		local point, relativeTo, relativePoint, offsetX, offsetY = MultiBarLeft:GetPoint(1)
+		offset = MultiBarLeft:GetWidth() - offsetX;
+	end
+
+	return offset + 10;
+end
+
 hooksecurefunc("UpdateContainerFrameAnchors", function()
-	local containerFrameOffsetX = GetRightActionBarWidth();
+	local containerFrameOffsetX = GetRightActionBarWidth()
 	local frame, xOffset, yOffset, screenHeight, freeScreenHeight, leftMostPoint, column;
-	local screenWidth = GetScreenWidth();
+	local screenWidth = GetScreenWidth()
 	local containerScale = 1;
 	local leftLimit = 0;
 	if ( BankFrame:IsShown() ) then
@@ -385,7 +373,7 @@ hooksecurefunc("UpdateContainerFrameAnchors", function()
 		column = 1;
 		local frameHeight;
 		for index, frameName in ipairs(_G.ContainerFrameSettingsManager:GetBagsShown()) do
-			frameHeight = frameName:GetHeight();
+			frameHeight = frameName:GetHeight()
 			if ( freeScreenHeight < frameHeight ) then
 				-- Start a new column
 				column = column + 1;
@@ -414,23 +402,22 @@ hooksecurefunc("UpdateContainerFrameAnchors", function()
 	column = 0;
 	for index, frameName in ipairs(_G.ContainerFrameSettingsManager:GetBagsShown()) do
 		frame = frameName;
-		frame:SetScale(containerScale);
+		frame:SetScale(containerScale)
 		if ( index == 1 ) then
 			-- First bag
-			frame:SetPoint("BOTTOMRIGHT", frame:GetParent(), "BOTTOMRIGHT", -xOffset, yOffset );
+			frame:SetPoint("BOTTOMRIGHT", frame:GetParent(), "BOTTOMRIGHT", -xOffset, yOffset )
 		elseif ( freeScreenHeight < frame:GetHeight() ) then
 			-- Start a new column
 			column = column + 1;
 			freeScreenHeight = screenHeight - yOffset;
-			frame:SetPoint("BOTTOMRIGHT", frame:GetParent(), "BOTTOMRIGHT", -(column * CONTAINER_WIDTH) - xOffset, yOffset );
+			frame:SetPoint("BOTTOMRIGHT", frame:GetParent(), "BOTTOMRIGHT", -(column * CONTAINER_WIDTH) - xOffset, yOffset )
 		else
 			-- Anchor to the previous bag
-			frame:SetPoint("BOTTOMRIGHT", _G.ContainerFrameSettingsManager:GetBagsShown()[index - 1], "TOPRIGHT", 0, CONTAINER_SPACING);	
+			frame:SetPoint("BOTTOMRIGHT", _G.ContainerFrameSettingsManager:GetBagsShown()[index - 1], "TOPRIGHT", 0, CONTAINER_SPACING)	
 		end
 		freeScreenHeight = freeScreenHeight - frame:GetHeight() - VISIBLE_CONTAINER_SPACING;
 	end
 end)
-
 
 hooksecurefunc(ContainerFrame1, "LayoutAddSlots", function(self)
 	if self.AddSlotsButton and self.AddSlotsButton:IsShown() then
