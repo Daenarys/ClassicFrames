9a2b8bf17f301e7e0fc61cf3908af0e0d04d820b
Daenarys <androidwoz@gmail.com>
2024-05-28 12:38:20 +0200

    more gamemenu fixes

--- ClassicFrames/skins/GameMenu.lua
+++ ClassicFrames/skins/GameMenu.lua
@@ -3,11 +3,17 @@ if not _G.GameMenuFrame then return end
 ApplyDialogBorder(GameMenuFrame.Border)
 ApplyDialogHeader(GameMenuFrame.Header)
 
-GameMenuFrame.Header:SetWidth(141)
+GameMenuFrame.Header:SetWidth(139.377)
 GameMenuFrame.Header.Text:SetFontObject("GameFontNormal")
+GameMenuFrame.Header.Text:ClearAllPoints()
+GameMenuFrame.Header.Text:SetPoint("TOP", 0, -12)
 
 hooksecurefunc(GameMenuFrame, "Layout", function(self)
-	self:SetSize(195, 320)
+	if C_StorePublic.IsEnabled() then
+		self:SetSize(195, 320)
+	else
+		self:SetSize(195, 292)
+	end
 end)
 
 hooksecurefunc(GameMenuFrame, "InitButtons", function(self)
@@ -26,8 +32,14 @@ hooksecurefunc(GameMenuFrame, "InitButtons", function(self)
 	end
 end)
 
+hooksecurefunc(GameMenuFrame, "AddSection", function(self)
+	self.sectionSpacing = 15
+end)
+
 GameMenuFrame:HookScript("OnShow", function(self)
 	self.topPadding = 32
 	self.leftPadding = 26
 	self.rightPadding = 26
+	self.bottomPadding = 3
+	self.spacing = 1
 end)
