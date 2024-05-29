018b80ed3e2183977505d28cedbc27e1dd545c75
Daenarys <androidwoz@gmail.com>
2024-05-29 23:32:47 +0200

    skinned debugtools scrollbar art

--- ClassicFrames/skins/DebugTools.lua
+++ ClassicFrames/skins/DebugTools.lua
@@ -9,5 +9,26 @@ f:SetScript("OnEvent", function(self, event, name)
 		TableAttributeDisplay.CloseButton:SetHighlightTexture("Interface\\Buttons\\UI-Panel-MinimizeButton-Highlight")
 		TableAttributeDisplay.CloseButton:ClearAllPoints()
 		TableAttributeDisplay.CloseButton:SetPoint("TOPRIGHT", 2, 1)
+
+		TableAttributeDisplay.LinesScrollFrame.ScrollBar:SetSize(25, 560)
+		TableAttributeDisplay.LinesScrollFrame.ScrollBar:ClearAllPoints()
+		TableAttributeDisplay.LinesScrollFrame.ScrollBar:SetPoint("TOPLEFT", TableAttributeDisplay.LinesScrollFrame, "TOPRIGHT", 1, 3)
+		TableAttributeDisplay.LinesScrollFrame.ScrollBar:SetPoint("BOTTOMLEFT", TableAttributeDisplay.LinesScrollFrame, "BOTTOMRIGHT", 1, -4)
+
+		TableAttributeDisplay.LinesScrollFrame.ScrollBar.Track:SetWidth(18)
+		TableAttributeDisplay.LinesScrollFrame.ScrollBar.Track:ClearAllPoints()
+		TableAttributeDisplay.LinesScrollFrame.ScrollBar.Track:SetPoint("TOPLEFT", 4, -22)
+		TableAttributeDisplay.LinesScrollFrame.ScrollBar.Track:SetPoint("BOTTOMRIGHT", -4, 22)
+
+		TableAttributeDisplay.LinesScrollFrame.ScrollBar.Track.Begin:Hide()
+		TableAttributeDisplay.LinesScrollFrame.ScrollBar.Track.End:Hide()
+		TableAttributeDisplay.LinesScrollFrame.ScrollBar.Track.Middle:Hide()
+
+		ApplyScrollBarArrow(TableAttributeDisplay.LinesScrollFrame.ScrollBar)
+		ApplyScrollBarThumb(TableAttributeDisplay.LinesScrollFrame.ScrollBar.Track.Thumb)
+
+		TableAttributeDisplay.ScrollFrameArt:ClearAllPoints()
+		TableAttributeDisplay.ScrollFrameArt:SetPoint("TOPLEFT", TableAttributeDisplay.LinesScrollFrame, "TOPLEFT", 0, 6)
+		TableAttributeDisplay.ScrollFrameArt:SetPoint("BOTTOMRIGHT", TableAttributeDisplay.LinesScrollFrame.ScrollBar, "BOTTOMRIGHT",  2, -2)
 	end
 end)
