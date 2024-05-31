ebf5c0640194f40317fa296078008024484dd14a
Daenarys <androidwoz@gmail.com>
2024-05-31 17:33:13 +0200

    skinned categorylist scrollbar art

--- ClassicFrames/skins/SettingsPanel.lua
+++ ClassicFrames/skins/SettingsPanel.lua
@@ -43,6 +43,29 @@ hooksecurefunc(SettingsPanel.Container.SettingsList.ScrollBox, 'Update', functio
 	end
 end)
 
+SettingsPanel.CategoryList.ScrollBar:SetSize(25, 560)
+SettingsPanel.CategoryList.ScrollBar:ClearAllPoints()
+SettingsPanel.CategoryList.ScrollBar:SetPoint("TOPLEFT", SettingsPanel.CategoryList.ScrollBox, "TOPRIGHT", -9, 7)
+SettingsPanel.CategoryList.ScrollBar:SetPoint("BOTTOMLEFT", SettingsPanel.CategoryList.ScrollBox, "BOTTOMRIGHT", -9, 0)
+
+if (SettingsPanel.CategoryList.ScrollBar.BG == nil) then
+	SettingsPanel.CategoryList.ScrollBar.BG = SettingsPanel.CategoryList.ScrollBar:CreateTexture(nil, "BACKGROUND")
+	SettingsPanel.CategoryList.ScrollBar.BG:SetColorTexture(0, 0, 0, .1)
+	SettingsPanel.CategoryList.ScrollBar.BG:SetAllPoints()
+end
+
+SettingsPanel.CategoryList.ScrollBar.Track:SetWidth(18)
+SettingsPanel.CategoryList.ScrollBar.Track:ClearAllPoints()
+SettingsPanel.CategoryList.ScrollBar.Track:SetPoint("TOPLEFT", 4, -22)
+SettingsPanel.CategoryList.ScrollBar.Track:SetPoint("BOTTOMRIGHT", -4, 22)
+
+SettingsPanel.CategoryList.ScrollBar.Track.Begin:Hide()
+SettingsPanel.CategoryList.ScrollBar.Track.End:Hide()
+SettingsPanel.CategoryList.ScrollBar.Track.Middle:Hide()
+
+ApplyScrollBarArrow(SettingsPanel.CategoryList.ScrollBar)
+ApplyScrollBarThumb(SettingsPanel.CategoryList.ScrollBar.Track.Thumb)
+
 SettingsPanel:HookScript("OnShow", function(self)
 	self:ClearAllPoints()
 	self:SetPoint("CENTER", 0, 5)
