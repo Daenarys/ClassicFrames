4e8cc44e6c14fb2f96447b6d705fe76596009dd8
Daenarys <androidwoz@gmail.com>
2024-06-01 13:42:21 +0200

    skinned combatlog config scrollbar

--- ClassicFrames/skins/Chat.lua
+++ ClassicFrames/skins/Chat.lua
@@ -96,6 +96,15 @@ hooksecurefunc("FCFDock_UpdateTabs", function(dock)
     end
 end)
 
+ChatConfigCombatSettingsFilters.ScrollBar:SetSize(25, 560)
+ChatConfigCombatSettingsFilters.ScrollBar:ClearAllPoints()
+ChatConfigCombatSettingsFilters.ScrollBar:SetPoint("TOPLEFT", ChatConfigCombatSettingsFilters.ScrollBox, "TOPRIGHT", -1, 0)
+ChatConfigCombatSettingsFilters.ScrollBar:SetPoint("BOTTOMLEFT", ChatConfigCombatSettingsFilters.ScrollBox, "BOTTOMRIGHT", 0, -1)
+
+ApplyScrollBarArrow(ChatConfigCombatSettingsFilters.ScrollBar)
+ApplyScrollBarTrack(ChatConfigCombatSettingsFilters.ScrollBar.Track)
+ApplyScrollBarThumb(ChatConfigCombatSettingsFilters.ScrollBar.Track.Thumb)
+
 QuickJoinToastButton:HookScript("OnUpdate", function(self)
     self:ClearAllPoints()
     self:SetPoint("BOTTOMLEFT", ChatAlertFrame, "BOTTOMLEFT")
