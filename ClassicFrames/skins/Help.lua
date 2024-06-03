212fdc6cf3ec5ab0428530e892b2001c01579d7c
Daenarys <androidwoz@gmail.com>
2024-06-03 13:56:23 +0200

    remove custom spinners

--- ClassicFrames/skins/Help.lua
+++ ClassicFrames/skins/Help.lua
@@ -9,11 +9,6 @@ HelpFrame.TitleContainer:SetPoint("TOPRIGHT", HelpFrame, "TOPRIGHT", -58, 0)
 ApplyTitleBgNoPortrait(HelpFrame)
 ApplyNineSliceNoPortrait(HelpFrame)
 
-hooksecurefunc(HelpFrame, "SetInitialLoading", function(self, initialLoading)
-	self.SpinnerOverlay:Hide()
-	self.CfSpinnerOverlay:SetShown(initialLoading)
-end)
-
 HelpFrame:HookScript("OnShow", function(self)
 	self:ClearAllPoints()
 	self:SetPoint("CENTER", 0, 24)
