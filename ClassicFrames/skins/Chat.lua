7a9a10a240bbf074e86e777f9068b50ff46f2771
Daenarys <androidwoz@gmail.com>
2024-06-04 18:12:04 +0200

    cleanup

--- ClassicFrames/skins/Chat.lua
+++ ClassicFrames/skins/Chat.lua
@@ -5,8 +5,8 @@ for i = 1, NUM_CHAT_WINDOWS do
     TextToSpeechButtonFrame:Hide()
 
     -- hide chat window scrollbars
-    local b = _G["ChatFrame"..i].ScrollBar b:UnregisterAllEvents() b:SetScript("OnShow", b.Hide) b:Hide();
-    local c = _G["ChatFrame"..i].ScrollToBottomButton c:UnregisterAllEvents() c:SetScript("OnShow", c.Hide) c:Hide();
+    local b = _G["ChatFrame"..i].ScrollBar b:UnregisterAllEvents() b:SetScript("OnShow", b.Hide) b:Hide()
+    local c = _G["ChatFrame"..i].ScrollToBottomButton c:UnregisterAllEvents() c:SetScript("OnShow", c.Hide) c:Hide()
     
     local ChatFrameButtonFrameUpButton = _G["ChatFrame"..i.."ButtonFrameUpButton"]
     if (ChatFrameButtonFrameUpButton == nil) then
@@ -100,24 +100,16 @@ for i = 1, NUM_CHAT_WINDOWS do
         ChatFrameButtonFrameMenuButton:SetDisabledTexture("Interface\\ChatFrame\\UI-ChatIcon-Chat-Disabled")
         ChatFrameButtonFrameMenuButton:SetHighlightTexture("Interface\\Buttons\\UI-Common-MouseHilight", "ADD")
         ChatFrameButtonFrameMenuButton:SetScript("OnClick", function(self, button)
-            PlaySound(SOUNDKIT.IG_CHAT_EMOTE_BUTTON);
-            ChatFrame_ToggleMenu();
+            PlaySound(SOUNDKIT.IG_CHAT_EMOTE_BUTTON)
+            ChatFrame_ToggleMenu()
 
             if ChatMenu:IsShown() and HelpTip:IsShowingAny(self) then
-                HelpTip:HideAll(self);
+                HelpTip:HideAll(self)
             end
         end)
     end
     
     hooksecurefunc("ChatFrame_OnUpdate", ChatFrameButtonFrameBottomButton.func_ChatFrame_OnUpdate)
-
-    -- fix blizz 10.1 bug
-    hooksecurefunc("FloatingChatFrame_UpdateBackgroundAnchors", function()
-        _G["ChatFrame"..i].Background:SetPoint("TOPLEFT", _G["ChatFrame"..i], "TOPLEFT", -2, 3);
-        _G["ChatFrame"..i].Background:SetPoint("TOPRIGHT", _G["ChatFrame"..i], "TOPRIGHT", 13, 3);
-        _G["ChatFrame"..i].Background:SetPoint("BOTTOMLEFT", _G["ChatFrame"..i], "BOTTOMLEFT", -2, -6);
-        _G["ChatFrame"..i].Background:SetPoint("BOTTOMRIGHT", _G["ChatFrame"..i], "BOTTOMRIGHT", 13, -6);
-    end)
 
     ChatFrameButtonFrameMenuButton:ClearAllPoints()
     ChatFrameButtonFrameMenuButton:SetPoint("BOTTOM", ChatFrameButtonFrameUpButton, "TOP", 0, 0)
@@ -135,7 +127,7 @@ for i = 1, NUM_CHAT_WINDOWS do
 end
 
 hooksecurefunc("FCFDock_UpdateTabs", function(dock)
-    local scrollChild = dock.scrollFrame:GetScrollChild();
+    local scrollChild = dock.scrollFrame:GetScrollChild()
     local lastDockedStaticTab = nil;
     local lastDockedDynamicTab = nil;
     local numDynFrames = 0;
@@ -143,19 +135,19 @@ hooksecurefunc("FCFDock_UpdateTabs", function(dock)
     for index, chatFrame in ipairs(dock.DOCKED_CHAT_FRAMES) do
         local chatTab = _G[chatFrame:GetName().."Tab"];
         if ( chatFrame.isStaticDocked ) then
-            PanelTemplates_TabResize(chatTab, chatTab.sizePadding or 8);
+            PanelTemplates_TabResize(chatTab, chatTab.sizePadding or 8)
             if ( lastDockedStaticTab ) then
-                chatTab:SetPoint("LEFT", lastDockedStaticTab, "RIGHT", 4, 0);
+                chatTab:SetPoint("LEFT", lastDockedStaticTab, "RIGHT", 4, 0)
             else
-                chatTab:SetPoint("LEFT", dock, "LEFT", 2, 0);
+                chatTab:SetPoint("LEFT", dock, "LEFT", 2, 0)
             end
             lastDockedStaticTab = chatTab;
         else
             numDynFrames = numDynFrames + 1;
             if ( lastDockedDynamicTab ) then
-                chatTab:SetPoint("LEFT", lastDockedDynamicTab, "RIGHT", 4, 0);
+                chatTab:SetPoint("LEFT", lastDockedDynamicTab, "RIGHT", 4, 0)
             else
-                chatTab:SetPoint("LEFT", scrollChild, "LEFT", 4, -2);
+                chatTab:SetPoint("LEFT", scrollChild, "LEFT", 4, -2)
             end
             lastDockedDynamicTab = chatTab;
         end
