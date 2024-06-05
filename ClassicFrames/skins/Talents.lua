7877023e162cb4d82373ec4076061ee9c9ced40e
Daenarys <androidwoz@gmail.com>
2024-06-06 01:19:16 +0200

    talentframe fixes

--- ClassicFrames/skins/Talents.lua
+++ ClassicFrames/skins/Talents.lua
@@ -1,47 +1,63 @@
 local f = CreateFrame("Frame")
 f:RegisterEvent("ADDON_LOADED")
 f:SetScript("OnEvent", function(self, event, name)
-	if name == "Blizzard_ClassTalentUI" then
-		ApplyCloseButton(ClassTalentFrameCloseButton)
+	if name == "Blizzard_PlayerSpells" then
+		ApplyCloseButton(PlayerSpellsFrameCloseButton)
 
-		ClassTalentFrame.PortraitContainer.CircleMask:Hide()
+		PlayerSpellsFrame.PortraitContainer.CircleMask:Hide()
 
-		ClassTalentFramePortrait:SetSize(61, 61)
-		ClassTalentFramePortrait:ClearAllPoints()
-		ClassTalentFramePortrait:SetPoint("TOPLEFT", -6, 8)
+		PlayerSpellsFramePortrait:SetSize(61, 61)
+		PlayerSpellsFramePortrait:ClearAllPoints()
+		PlayerSpellsFramePortrait:SetPoint("TOPLEFT", -6, 8)
 
-		ClassTalentFrame.TitleContainer:ClearAllPoints()
-		ClassTalentFrame.TitleContainer:SetPoint("TOPLEFT", ClassTalentFrame, "TOPLEFT", 58, 0)
-		ClassTalentFrame.TitleContainer:SetPoint("TOPRIGHT", ClassTalentFrame, "TOPRIGHT", -58, 0)
+		PlayerSpellsFrame.TitleContainer:ClearAllPoints()
+		PlayerSpellsFrame.TitleContainer:SetPoint("TOPLEFT", PlayerSpellsFrame, "TOPLEFT", 58, 0)
+		PlayerSpellsFrame.TitleContainer:SetPoint("TOPRIGHT", PlayerSpellsFrame, "TOPRIGHT", -58, 0)
 
-		ApplyTitleBg(ClassTalentFrame)
-		ApplyNineSlicePortrait(ClassTalentFrame)
+		PlayerSpellsFrame.MaximizeMinimizeButton:SetSize(32, 32)
+		PlayerSpellsFrame.MaximizeMinimizeButton:ClearAllPoints()
+		PlayerSpellsFrame.MaximizeMinimizeButton:SetPoint("RIGHT", PlayerSpellsFrameCloseButton, "LEFT", 8.5, 0)
+		PlayerSpellsFrame.MaximizeMinimizeButton:SetFrameLevel(2)
 
-		for i = 1, _G.ClassTalentFrame.TabSystem:GetNumChildren() do
-			local tab = select(i, _G.ClassTalentFrame.TabSystem:GetChildren())
+		PlayerSpellsFrame.MaximizeMinimizeButton.MaximizeButton:SetNormalTexture("Interface\\Buttons\\UI-Panel-BiggerButton-Up")
+		PlayerSpellsFrame.MaximizeMinimizeButton.MaximizeButton:SetPushedTexture("Interface\\Buttons\\UI-Panel-BiggerButton-Down")
+		PlayerSpellsFrame.MaximizeMinimizeButton.MaximizeButton:SetDisabledTexture("Interface\\Buttons\\UI-Panel-BiggerButton-Disabled")
+		PlayerSpellsFrame.MaximizeMinimizeButton.MaximizeButton:SetHighlightTexture("Interface\\Buttons\\UI-Panel-MinimizeButton-Highlight")
+
+		PlayerSpellsFrame.MaximizeMinimizeButton.MinimizeButton:SetNormalTexture("Interface\\Buttons\\UI-Panel-SmallerButton-Up")
+		PlayerSpellsFrame.MaximizeMinimizeButton.MinimizeButton:SetPushedTexture("Interface\\Buttons\\UI-Panel-SmallerButton-Down")
+		PlayerSpellsFrame.MaximizeMinimizeButton.MinimizeButton:SetDisabledTexture("Interface\\Buttons\\UI-Panel-SmallerButton-Disabled")
+		PlayerSpellsFrame.MaximizeMinimizeButton.MinimizeButton:SetHighlightTexture("Interface\\Buttons\\UI-Panel-MinimizeButton-Highlight")
+
+		ApplyTitleBg(PlayerSpellsFrame)
+		ApplyNineSlicePortrait(PlayerSpellsFrame)
+
+		PlayerSpellsFrame.SpellBookFrame:HookScript("OnShow", function()
+			ApplyNineSlicePortraitMinimizable(PlayerSpellsFrame)
+        end)
+
+        PlayerSpellsFrame.SpellBookFrame:HookScript("OnHide", function()
+			ApplyNineSlicePortrait(PlayerSpellsFrame)
+        end)
+
+		for i = 1, PlayerSpellsFrame.TabSystem:GetNumChildren() do
+			local tab = select(i, PlayerSpellsFrame.TabSystem:GetChildren())
 
 			ApplyBottomTab(tab)
 		end
 
-		hooksecurefunc(ClassTalentFrame.TabSystem, 'Layout', function(self)
+		hooksecurefunc(PlayerSpellsFrame.TabSystem, 'Layout', function(self)
 			self.tabs[1]:SetWidth(45 + self.tabs[1]:GetFontString():GetStringWidth())
 			self.tabs[2]:ClearAllPoints()
 			self.tabs[2]:SetPoint("LEFT", self.tabs[1], "RIGHT", -15, 0)
-        end)
-
-        hooksecurefunc(ClassTalentFrame.SpecTab, 'UpdateSpecFrame', function(frame)
-			for specContentFrame in frame.SpecContentFramePool:EnumerateActive() do
-				local role = GetSpecializationRole(specContentFrame.specIndex, false, false)
-				
-				specContentFrame.RoleIcon:SetTexCoord(GetTexCoordsForRole(role))
-				specContentFrame.RoleIcon:SetTexture("Interface\\LFGFrame\\UI-LFG-ICON-ROLES")
-			end
+			self.tabs[3]:ClearAllPoints()
+			self.tabs[3]:SetPoint("LEFT", self.tabs[2], "RIGHT", -15, 0)
         end)
 
 		ApplyDialogBorder(ClassTalentLoadoutCreateDialog.Border)
 		ApplyDialogBorder(ClassTalentLoadoutImportDialog.Border)
 
-		ApplySearchBox(ClassTalentFrame.TalentsTab.SearchBox)
+		ApplySearchBox(PlayerSpellsFrame.TalentsFrame.SearchBox)
 		ApplySearchBox(ClassTalentLoadoutCreateDialog.NameControl.EditBox)
 		ApplySearchBox(ClassTalentLoadoutImportDialog.NameControl.EditBox)
 	end
