740a008ee80af66905c82f30f0b102475ef6e63c
Daenarys <androidwoz@gmail.com>
2024-06-06 10:20:57 +0200

    gamemenuframe size fixes

--- ClassicFrames/skins/GameMenu.lua
+++ ClassicFrames/skins/GameMenu.lua
@@ -2,3 +2,26 @@ if not _G.GameMenuFrame then return end
 
 ApplyDialogBorder(GameMenuFrame.Border)
 ApplyDialogHeader(GameMenuFrame.Header)
+
+GameMenuFrame.Header:SetWidth(139.377)
+GameMenuFrame.Header.Text:SetFontObject("GameFontNormal")
+
+hooksecurefunc(GameMenuFrame, "InitButtons", function(self)
+	for obj in self.buttonPool:EnumerateActive() do
+		obj:SetSize(144, 21)
+
+		obj:SetNormalFontObject("GameFontHighlight")
+		obj:SetHighlightFontObject("GameFontHighlight")
+		obj:SetDisabledFontObject("GameFontDisable")
+
+		obj:GetFontString():ClearAllPoints()
+		obj:GetFontString():SetPoint("CENTER", 0, 1)
+	end
+end)
+
+GameMenuFrame:HookScript("OnShow", function(self)
+	self.topPadding = 32
+	self.leftPadding = 26
+	self.rightPadding = 26
+	self.bottomPadding = 17
+end)
