ef97c7d2db221e5f99ee32b9ff5155a9856dbcc0
Daenarys <androidwoz@gmail.com>
2024-06-06 16:48:39 +0200

    clean up

--- ClassicFrames/skins/SettingsPanel.lua
+++ ClassicFrames/skins/SettingsPanel.lua
@@ -31,18 +31,6 @@ SettingsPanel.Container.SettingsList.ScrollBar.Track.Middle:Hide()
 ApplyScrollBarArrow(SettingsPanel.Container.SettingsList.ScrollBar)
 ApplyScrollBarThumb(SettingsPanel.Container.SettingsList.ScrollBar.Track.Thumb)
 
-hooksecurefunc(SettingsPanel.Container.SettingsList.ScrollBox, 'Update', function(frame)
-	for _, child in next, { frame.ScrollTarget:GetChildren() } do
-		if child.CheckBox then
-			child.CheckBox:SetNormalTexture("Interface\\Buttons\\UI-CheckBox-Up")
-			child.CheckBox:SetPushedTexture("Interface\\Buttons\\UI-CheckBox-Down")
-			child.CheckBox:SetHighlightTexture("Interface\\Buttons\\UI-CheckBox-Highlight")
-			child.CheckBox:SetCheckedTexture("Interface\\Buttons\\UI-CheckBox-Check")
-			child.CheckBox:SetDisabledCheckedTexture("Interface\\Buttons\\UI-CheckBox-Check-Disabled")
-		end
-	end
-end)
-
 SettingsPanel.CategoryList.ScrollBar:SetSize(25, 560)
 SettingsPanel.CategoryList.ScrollBar:ClearAllPoints()
 SettingsPanel.CategoryList.ScrollBar:SetPoint("TOPLEFT", SettingsPanel.CategoryList.ScrollBox, "TOPRIGHT", -9, 7)
