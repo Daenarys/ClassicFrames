0adb5fca4fb922ced8e9faa96195642cb04b5380
Daenarys <androidwoz@gmail.com>
2024-06-08 15:07:37 +0200

    skinned currencytransfermenu frame

--- ClassicFrames/skins/CurrencyTransfer.lua
+++ ClassicFrames/skins/CurrencyTransfer.lua
@@ -1,25 +1,38 @@
-if not _G.CurrencyTransferLog then return end
+if _G.CurrencyTransferLog then
+	ApplyCloseButton(CurrencyTransferLogCloseButton)
 
-ApplyCloseButton(CurrencyTransferLogCloseButton)
+	CurrencyTransferLog.TitleContainer:ClearAllPoints()
+	CurrencyTransferLog.TitleContainer:SetPoint("TOPLEFT", CurrencyTransferLog, "TOPLEFT", 58, 0)
+	CurrencyTransferLog.TitleContainer:SetPoint("TOPRIGHT", CurrencyTransferLog, "TOPRIGHT", -58, 0)
 
-CurrencyTransferLog.TitleContainer:ClearAllPoints()
-CurrencyTransferLog.TitleContainer:SetPoint("TOPLEFT", CurrencyTransferLog, "TOPLEFT", 58, 0)
-CurrencyTransferLog.TitleContainer:SetPoint("TOPRIGHT", CurrencyTransferLog, "TOPRIGHT", -58, 0)
+	ApplyTitleBgNoPortrait(CurrencyTransferLog)
+	ApplyNineSliceNoPortrait(CurrencyTransferLog)
 
-ApplyTitleBgNoPortrait(CurrencyTransferLog)
-ApplyNineSliceNoPortrait(CurrencyTransferLog)
+	CurrencyTransferLog.ScrollBar:SetSize(25, 560)
+	CurrencyTransferLog.ScrollBar:ClearAllPoints()
+	CurrencyTransferLog.ScrollBar:SetPoint("TOPLEFT", CurrencyTransferLog.ScrollBox, "TOPRIGHT", -2, 2)
+	CurrencyTransferLog.ScrollBar:SetPoint("BOTTOMLEFT", CurrencyTransferLog.ScrollBox, "BOTTOMRIGHT", -2, 0)
 
-CurrencyTransferLog.ScrollBar:SetSize(25, 560)
-CurrencyTransferLog.ScrollBar:ClearAllPoints()
-CurrencyTransferLog.ScrollBar:SetPoint("TOPLEFT", CurrencyTransferLog.ScrollBox, "TOPRIGHT", -2, 2)
-CurrencyTransferLog.ScrollBar:SetPoint("BOTTOMLEFT", CurrencyTransferLog.ScrollBox, "BOTTOMRIGHT", -2, 0)
+	if (CurrencyTransferLog.ScrollBar.BG == nil) then
+		CurrencyTransferLog.ScrollBar.BG = CurrencyTransferLog.ScrollBar:CreateTexture(nil, "BACKGROUND")
+		CurrencyTransferLog.ScrollBar.BG:SetColorTexture(0, 0, 0, 1)
+		CurrencyTransferLog.ScrollBar.BG:SetAllPoints()
+	end
 
-if (CurrencyTransferLog.ScrollBar.BG == nil) then
-	CurrencyTransferLog.ScrollBar.BG = CurrencyTransferLog.ScrollBar:CreateTexture(nil, "BACKGROUND")
-	CurrencyTransferLog.ScrollBar.BG:SetColorTexture(0, 0, 0, 1)
-	CurrencyTransferLog.ScrollBar.BG:SetAllPoints()
+	ApplyScrollBarArrow(CurrencyTransferLog.ScrollBar)
+	ApplyScrollBarTrack(CurrencyTransferLog.ScrollBar.Track)
+	ApplyScrollBarThumb(CurrencyTransferLog.ScrollBar.Track.Thumb)
 end
 
-ApplyScrollBarArrow(CurrencyTransferLog.ScrollBar)
-ApplyScrollBarTrack(CurrencyTransferLog.ScrollBar.Track)
-ApplyScrollBarThumb(CurrencyTransferLog.ScrollBar.Track.Thumb)
+if _G.CurrencyTransferMenu then
+	ApplyCloseButton(CurrencyTransferMenuCloseButton)
+
+	CurrencyTransferMenu.TitleContainer:ClearAllPoints()
+	CurrencyTransferMenu.TitleContainer:SetPoint("TOPLEFT", CurrencyTransferMenu, "TOPLEFT", 58, 0)
+	CurrencyTransferMenu.TitleContainer:SetPoint("TOPRIGHT", CurrencyTransferMenu, "TOPRIGHT", -58, 0)
+
+	ApplyTitleBgNoPortrait(CurrencyTransferMenu)
+	ApplyNineSliceNoPortrait(CurrencyTransferMenu)
+
+	ApplyDropDown(CurrencyTransferMenu.SourceSelector.Dropdown)
+end
