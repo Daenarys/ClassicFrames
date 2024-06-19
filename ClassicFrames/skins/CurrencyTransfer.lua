925da68ab31513a94e95d1538b69ce64e05bb75c
Daenarys <androidwoz@gmail.com>
2024-06-19 12:31:13 +0200

    hide currency transfer bg

--- ClassicFrames/skins/CurrencyTransfer.lua
+++ ClassicFrames/skins/CurrencyTransfer.lua
@@ -8,16 +8,12 @@ if _G.CurrencyTransferLog then
 	ApplyTitleBgNoPortrait(CurrencyTransferLog)
 	ApplyNineSliceNoPortrait(CurrencyTransferLog)
 
+	CurrencyTransferLog.Background:Hide()
+
 	CurrencyTransferLog.ScrollBar:SetSize(25, 560)
 	CurrencyTransferLog.ScrollBar:ClearAllPoints()
 	CurrencyTransferLog.ScrollBar:SetPoint("TOPLEFT", CurrencyTransferLog.ScrollBox, "TOPRIGHT", -2, 2)
 	CurrencyTransferLog.ScrollBar:SetPoint("BOTTOMLEFT", CurrencyTransferLog.ScrollBox, "BOTTOMRIGHT", -2, -2)
-
-	if (CurrencyTransferLog.ScrollBar.BG == nil) then
-		CurrencyTransferLog.ScrollBar.BG = CurrencyTransferLog.ScrollBar:CreateTexture(nil, "BACKGROUND")
-		CurrencyTransferLog.ScrollBar.BG:SetColorTexture(0, 0, 0, .75)
-		CurrencyTransferLog.ScrollBar.BG:SetAllPoints()
-	end
 
 	ApplyScrollBarArrow(CurrencyTransferLog.ScrollBar)
 	ApplyScrollBarTrack(CurrencyTransferLog.ScrollBar.Track)
