30703b567d1c3e655ad3c9018fd81bcc97f0b526
Daenarys <androidwoz@gmail.com>
2024-06-20 18:41:31 +0200

    update icon again

--- ClassicFrames/skins/Minimap.lua
+++ ClassicFrames/skins/Minimap.lua
@@ -568,13 +568,34 @@ Minimap:HookScript("OnEvent", function(self, event, ...)
 			ExpansionLandingPageMinimapButton:Hide()
 		end
 
-		ExpansionLandingPageMinimapButton:SetScale(0.84)
 		ExpansionLandingPageMinimapButton:ClearAllPoints()
-		ExpansionLandingPageMinimapButton:SetPoint("TOPLEFT", 47, -144)
+		ExpansionLandingPageMinimapButton:SetPoint("TOPLEFT", 32, -118)
+
+		ExpansionLandingPageMinimapButton.faction = UnitFactionGroup("player")
+		if ( ExpansionLandingPageMinimapButton.faction == "Horde" ) then
+			ExpansionLandingPageMinimapButton:GetNormalTexture():SetAtlas("GarrLanding-MinimapIcon-Horde-Up", true)
+			ExpansionLandingPageMinimapButton:GetPushedTexture():SetAtlas("GarrLanding-MinimapIcon-Horde-Down", true)
+		else
+			ExpansionLandingPageMinimapButton:GetNormalTexture():SetAtlas("GarrLanding-MinimapIcon-Alliance-Up", true)
+			ExpansionLandingPageMinimapButton:GetPushedTexture():SetAtlas("GarrLanding-MinimapIcon-Alliance-Down", true)
+		end
+		ExpansionLandingPageMinimapButton:GetHighlightTexture():SetTexture(136477, "ADD")
+		ExpansionLandingPageMinimapButton.LoopingGlow:SetAtlas("GarrLanding-CircleGlow", true)
 
 		hooksecurefunc(ExpansionLandingPageMinimapButton, "UpdateIcon", function(self)
 			self:ClearAllPoints()
-			self:SetPoint("TOPLEFT", 47, -144)
+			self:SetPoint("TOPLEFT", 32, -118)
+
+			self.faction = UnitFactionGroup("player")
+			if ( self.faction == "Horde" ) then
+				self:GetNormalTexture():SetAtlas("GarrLanding-MinimapIcon-Horde-Up", true)
+				self:GetPushedTexture():SetAtlas("GarrLanding-MinimapIcon-Horde-Down", true)
+			else
+				self:GetNormalTexture():SetAtlas("GarrLanding-MinimapIcon-Alliance-Up", true)
+				self:GetPushedTexture():SetAtlas("GarrLanding-MinimapIcon-Alliance-Down", true)
+			end
+			self:GetHighlightTexture():SetTexture(136477, "ADD")
+			self.LoopingGlow:SetAtlas("GarrLanding-CircleGlow", true)
 		end)
 
 		hooksecurefunc(ExpansionLandingPageMinimapButton, "UpdateIconForGarrison", function(self)
