c350eec61ce72ff7bdb33aafcdd60aed7b8862bf
Daenarys <androidwoz@gmail.com>
2024-07-11 15:21:28 +0200

    fix header

--- ClassicFrames/skins/Chat.lua
+++ ClassicFrames/skins/Chat.lua
@@ -136,7 +136,7 @@ ApplyScrollBarTrack(ChatConfigCombatSettingsFilters.ScrollBar.Track)
 ApplyScrollBarThumb(ChatConfigCombatSettingsFilters.ScrollBar.Track.Thumb)
 
 ApplyDialogBorder(ChatConfigFrame.Border)
-ApplyDialogHeader(ChatConfigFrame)
+ApplyDialogHeader(ChatConfigFrame.Header)
 
 ChatConfigFrame:HookScript("OnShow", function(self)
     self:ClearAllPoints()
