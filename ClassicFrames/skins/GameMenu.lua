b23c3e7a21d66ff1ef4976e0b139d54bb4e060d8
Daenarys <androidwoz@gmail.com>
2024-07-18 11:32:51 +0200

    gamemenu cleanup

--- ClassicFrames/skins/GameMenu.lua
+++ ClassicFrames/skins/GameMenu.lua
@@ -10,42 +10,12 @@ GameMenuFrame:HookScript("OnShow", function(self)
 	self.topPadding = 32
 	self.leftPadding = 26
 	self.rightPadding = 26
-	self.bottomPadding = 19
+	self.bottomPadding = 16
 
 	for button in GameMenuFrame.buttonPool:EnumerateActive() do
-		button:HookScript("OnUpdate", function()
-			buttonState = button:GetButtonState()
-
-			button:SetSize(144, 21)
-			button:SetNormalFontObject("GameFontHighlight")
-			button:SetHighlightFontObject("GameFontHighlight")
-			button:SetDisabledFontObject("GameFontDisable")
-
-			button:SetHighlightTexture("Interface\\Buttons\\UI-Panel-Button-Highlight", "ADD")
-			button:GetHighlightTexture():SetTexCoord(0, 0.625, 0, 0.6875)
-
-			if buttonState == "DISABLED" then
-				button.Left:SetTexture("Interface\\Buttons\\UI-Panel-Button-Disabled")
-				button.Center:SetTexture("Interface\\Buttons\\UI-Panel-Button-Disabled")
-				button.Right:SetTexture("Interface\\Buttons\\UI-Panel-Button-Disabled")
-			elseif buttonState == "PUSHED" then
-				button.Left:SetTexture("Interface\\Buttons\\UI-Panel-Button-Down");
-				button.Center:SetTexture("Interface\\Buttons\\UI-Panel-Button-Down");
-				button.Right:SetTexture("Interface\\Buttons\\UI-Panel-Button-Down");
-			else
-				button.Left:SetTexture("Interface\\Buttons\\UI-Panel-Button-Up")
-				button.Center:SetTexture("Interface\\Buttons\\UI-Panel-Button-Up")
-				button.Right:SetTexture("Interface\\Buttons\\UI-Panel-Button-Up")
-			end
-
-			button.Left:SetScale(1)
-			button.Left:SetSize(12, 21)
-			button.Left:SetTexCoord(0, 0.09375, 0, 0.6875)
-			button.Center:SetScale(1)
-			button.Center:SetTexCoord(0.09375, 0.53125, 0, 0.6875)
-			button.Right:SetScale(1)
-			button.Right:SetSize(12, 21)
-			button.Right:SetTexCoord(0.53125, 0.625, 0, 0.6875)
-		end)
+		button:SetSize(144, 21)
+		button:SetNormalFontObject("GameFontHighlight")
+		button:SetHighlightFontObject("GameFontHighlight")
+		button:SetDisabledFontObject("GameFontDisable")
 	end
 end)
