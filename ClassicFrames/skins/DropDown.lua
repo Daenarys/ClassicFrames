609e283653a68e99fb1a1fe6c821dc8511f29252
Daenarys <androidwoz@gmail.com>
2024-08-18 11:37:15 +0200

    skinned dropdown menu scrollbars

--- ClassicFrames/skins/DropDown.lua
+++ ClassicFrames/skins/DropDown.lua
@@ -12,5 +12,24 @@ hooksecurefunc(MenuManagerProxy, 'OpenMenu', function()
 				ApplySearchBox(child.MaxLevel)
 			end
 		end
+		if MenuFrame.ScrollBar then
+			MenuFrame.ScrollBar.Track.Begin:Hide()
+			MenuFrame.ScrollBar.Track.End:Hide()
+			MenuFrame.ScrollBar.Track.Middle:Hide()
+
+			ApplyScrollBarThumb(MenuFrame.ScrollBar.Track.Thumb)
+
+			MenuFrame.ScrollBar.Back:SetSize(18, 16)
+			MenuFrame.ScrollBar.Back:SetNormalAtlas("UI-ScrollBar-ScrollUpButton-Up")
+			MenuFrame.ScrollBar.Back:SetPushedAtlas("UI-ScrollBar-ScrollUpButton-Down")
+			MenuFrame.ScrollBar.Back:SetDisabledAtlas("UI-ScrollBar-ScrollUpButton-Disabled")
+			MenuFrame.ScrollBar.Back:SetHighlightAtlas("UI-ScrollBar-ScrollUpButton-Highlight")
+
+			MenuFrame.ScrollBar.Forward:SetSize(18, 16)
+			MenuFrame.ScrollBar.Forward:SetNormalAtlas("UI-ScrollBar-ScrollDownButton-Up")
+			MenuFrame.ScrollBar.Forward:SetPushedAtlas("UI-ScrollBar-ScrollDownButton-Down")
+			MenuFrame.ScrollBar.Forward:SetDisabledAtlas("UI-ScrollBar-ScrollDownButton-Disabled")
+			MenuFrame.ScrollBar.Forward:SetHighlightAtlas("UI-ScrollBar-ScrollDownButton-Highlight")
+		end
 	end
 end)
