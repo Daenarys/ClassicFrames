6b6ab47c33e31fc6fd4aad43cf9fe000105af3f5
Daenarys <androidwoz@gmail.com>
2024-08-18 21:41:06 +0200

    health/mana bar text fixes

--- ClassicFrames/skins/unitframes/PlayerFrame.lua
+++ ClassicFrames/skins/unitframes/PlayerFrame.lua
@@ -13,6 +13,10 @@ function CfPlayerFrame_OnLoad(self)
 		CfPlayerFrameOverHealAbsorbGlow, CfPlayerFrameHealAbsorbBar, CfPlayerFrameHealAbsorbBarLeftShadow,
 		CfPlayerFrameHealAbsorbBarRightShadow)
 
+	CfPlayerFrameHealthBarTextLeft:SetParent(PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual)
+	CfPlayerFrameHealthBarTextRight:SetParent(PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual)
+	CfPlayerFrameManaBarTextLeft:SetParent(PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual)
+	CfPlayerFrameManaBarTextRight:SetParent(PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual)
 	CfPlayerFrameOverAbsorbGlow:SetParent(PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual)
 
 	self:RegisterEvent("PLAYER_ENTERING_WORLD")
