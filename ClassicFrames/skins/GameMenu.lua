9be8518c1ffc669f4bfc42035bc934b4f5afd54a
Daenarys <androidwoz@gmail.com>
2024-09-01 18:00:09 +0200

    small tweaks

--- ClassicFrames/skins/GameMenu.lua
+++ ClassicFrames/skins/GameMenu.lua
@@ -3,14 +3,14 @@ if not _G.GameMenuFrame then return end
 ApplyDialogBorder(GameMenuFrame.Border)
 ApplyDialogHeader(GameMenuFrame.Header)
 
-GameMenuFrame.Header:SetWidth(141)
+GameMenuFrame.Header:SetWidth(139.3777)
 GameMenuFrame.Header.Text:SetFontObject("GameFontNormal")
 
 GameMenuFrame:HookScript("OnShow", function(self)
 	self.topPadding = 32
 	self.leftPadding = 26
 	self.rightPadding = 26
-	self.bottomPadding = 18
+	self.bottomPadding = 12
 
 	for button in self.buttonPool:EnumerateActive() do
 		button:SetSize(144, 21)
