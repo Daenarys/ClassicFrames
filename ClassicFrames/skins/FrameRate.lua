10808c2d4ae9b3bd1dd4208d2c19b49ad499f130
Daenarys <androidwoz@gmail.com>
2024-09-02 15:25:30 +0200

    remove this

--- ClassicFrames/skins/FrameRate.lua
+++
@@ -1,6 +0,0 @@
-if not _G.FramerateFrame then return end
-
-hooksecurefunc(FramerateFrame, "UpdatePosition", function(self)
-	self:ClearAllPoints()
-	self:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, 75)
-end)
