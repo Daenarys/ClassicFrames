527993be43751b0651ee8919b99e0f7629a55f29
Daenarys <androidwoz@gmail.com>
2024-09-05 11:22:08 +0200

    update position for certain classes

--- ClassicFrames/skins/TotemFrame.lua
+++ ClassicFrames/skins/TotemFrame.lua
@@ -1,6 +1,11 @@
 if not _G.TotemFrame then return end
 
 hooksecurefunc(TotemFrame, "Update", function(self)
+	local _, class = UnitClass("player")
+	if (class == "PALADIN" or class == "WARLOCK" or class == "DEATHKNIGHT" or class == "MONK") then
+		self:ClearAllPoints()
+		self:SetPoint("TOPLEFT", PlayerFrame, "TOPLEFT", -6, -91)
+	end
 	for child in self.totemPool:EnumerateActive() do
 		child.Border:SetSize(38, 38)
 		child.Border:SetTexture("Interface\\CharacterFrame\\TotemBorder")
