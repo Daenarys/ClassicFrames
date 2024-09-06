2515e32ecb1a5e0f492ba3a2030d3d911dce0baf
Daenarys <androidwoz@gmail.com>
2024-09-06 17:13:41 +0200

    update gamemenu button art

--- ClassicFrames/skins/GameMenu.lua
+++ ClassicFrames/skins/GameMenu.lua
@@ -13,10 +13,65 @@ GameMenuFrame:HookScript("OnShow", function(self)
 	self.bottomPadding = 12
 
 	for button in self.buttonPool:EnumerateActive() do
-		button:SetSize(144, 21)
-		button:SetNormalFontObject("GameFontHighlight")
-		button:SetHighlightFontObject("GameFontHighlight")
-		button:SetDisabledFontObject("GameFontDisable")
+		if not button.IsSkinned then
+			button:SetSize(144, 21)
+			button:SetNormalFontObject("GameFontHighlight")
+			button:SetHighlightFontObject("GameFontHighlight")
+			button:SetDisabledFontObject("GameFontDisable")
+
+			button.Left:Hide()
+			button.Center:Hide()
+			button.Right:Hide()
+
+			button:SetHighlightTexture("Interface\\Buttons\\UI-Panel-Button-Highlight", "ADD")
+			button:GetHighlightTexture():SetTexCoord(0, 0.625, 0, 0.6875)
+
+			button:SetPushedTextOffset(1.57, -1.57)
+
+			if (button.CfLeft == nil) then
+				button.CfLeft = button:CreateTexture(nil, "BACKGROUND")
+				button.CfLeft:SetSize(12, 21)
+				button.CfLeft:SetPoint("TOPLEFT")
+				button.CfLeft:SetPoint("BOTTOMLEFT")
+			end
+
+			if (button.CfRight == nil) then
+				button.CfRight = button:CreateTexture(nil, "BACKGROUND")
+				button.CfRight:SetSize(12, 21)
+				button.CfRight:SetPoint("TOPRIGHT")
+				button.CfRight:SetPoint("BOTTOMRIGHT")
+			end
+
+			if (button.CfMiddle == nil) then
+				button.CfMiddle = button:CreateTexture(nil, "BACKGROUND")
+				button.CfMiddle:SetSize(144, 21)
+				button.CfMiddle:SetPoint("TOPLEFT", button.CfLeft, "TOPRIGHT")
+				button.CfMiddle:SetPoint("BOTTOMRIGHT", button.CfRight, "BOTTOMLEFT")
+			end
+
+			button:HookScript("OnUpdate", function()
+				local buttonState = button:GetButtonState()
+
+				button.CfLeft:SetTexCoord(0, 0.09375, 0, 0.6875)
+				button.CfRight:SetTexCoord(0.53125, 0.625, 0, 0.6875)
+				button.CfMiddle:SetTexCoord(0.09375, 0.53125, 0, 0.6875)
+
+				if buttonState == "DISABLED" then
+					button.CfLeft:SetTexture("Interface\\Buttons\\UI-Panel-Button-Disabled")
+					button.CfMiddle:SetTexture("Interface\\Buttons\\UI-Panel-Button-Disabled")
+					button.CfRight:SetTexture("Interface\\Buttons\\UI-Panel-Button-Disabled")
+				elseif buttonState == "PUSHED" then
+					button.CfLeft:SetTexture("Interface\\Buttons\\UI-Panel-Button-Down")
+					button.CfMiddle:SetTexture("Interface\\Buttons\\UI-Panel-Button-Down")
+					button.CfRight:SetTexture("Interface\\Buttons\\UI-Panel-Button-Down")
+				else
+					button.CfLeft:SetTexture("Interface\\Buttons\\UI-Panel-Button-Up")
+					button.CfMiddle:SetTexture("Interface\\Buttons\\UI-Panel-Button-Up")
+					button.CfRight:SetTexture("Interface\\Buttons\\UI-Panel-Button-Up")
+				end
+			end)
+			button.IsSkinned = true
+		end
 	end
 end)
 
