7e4d1ff7bb4a08ab111711e00a07bf545b064fc0
Daenarys <androidwoz@gmail.com>
2024-09-21 23:22:26 +0200

    updated flight map pin art

--- ClassicFrames/skins/FlightMap.lua
+++ ClassicFrames/skins/FlightMap.lua
@@ -16,6 +16,54 @@ f:SetScript("OnEvent", function(self, event, name)
 
 		ApplyTitleBg(FlightMapFrame)
 		ApplyNineSlicePortrait(FlightMapFrame.BorderFrame)
+
+		hooksecurefunc(FlightMap_FlightPointPinMixin, 'SetFlightPathStyle', function(self)
+			local info = self.taxiNodeData
+			if info then
+				if (self.Icon:GetAtlas() == "Taxi_Frame_Gray") then
+					self.Icon:SetTexture("Interface\\AddOns\\ClassicFrames\\icons\\taxiassets")
+					self.Icon:SetTexCoord(0.0078125, 0.585938, 0.0683594, 0.212891)
+				elseif (self.Icon:GetAtlas() == "Taxi_Frame_Yellow") then
+					self.Icon:SetTexture("Interface\\AddOns\\ClassicFrames\\icons\\taxiassets")
+					self.Icon:SetTexCoord(0.0078125, 0.585938, 0.365234, 0.509766)
+				elseif (self.Icon:GetAtlas() == "Taxi_Frame_Green") then
+					self.Icon:SetTexture("Interface\\AddOns\\ClassicFrames\\icons\\taxiassets")
+					self.Icon:SetTexCoord(0.0078125, 0.585938, 0.216797, 0.361328)
+				end
+			end
+		end)
+
+		hooksecurefunc(FlightMap_FlightPointPinMixin, 'OnMouseEnter', function(self)
+			local info = self.taxiNodeData
+			if info then
+				if (self.Icon:GetAtlas() == "Taxi_Frame_Gray") then
+					self.Icon:SetTexture("Interface\\AddOns\\ClassicFrames\\icons\\taxiassets")
+					self.Icon:SetTexCoord(0.0078125, 0.585938, 0.0683594, 0.212891)
+				elseif (self.Icon:GetAtlas() == "Taxi_Frame_Yellow") then
+					self.Icon:SetTexture("Interface\\AddOns\\ClassicFrames\\icons\\taxiassets")
+					self.Icon:SetTexCoord(0.0078125, 0.585938, 0.365234, 0.509766)
+				elseif (self.Icon:GetAtlas() == "Taxi_Frame_Green") then
+					self.Icon:SetTexture("Interface\\AddOns\\ClassicFrames\\icons\\taxiassets")
+					self.Icon:SetTexCoord(0.0078125, 0.585938, 0.216797, 0.361328)
+				end
+			end
+		end)
+
+		hooksecurefunc(FlightMap_FlightPointPinMixin, 'OnMouseLeave', function(self)
+			local info = self.taxiNodeData
+			if info then
+				if (self.Icon:GetAtlas() == "Taxi_Frame_Gray") then
+					self.Icon:SetTexture("Interface\\AddOns\\ClassicFrames\\icons\\taxiassets")
+					self.Icon:SetTexCoord(0.0078125, 0.585938, 0.0683594, 0.212891)
+				elseif (self.Icon:GetAtlas() == "Taxi_Frame_Yellow") then
+					self.Icon:SetTexture("Interface\\AddOns\\ClassicFrames\\icons\\taxiassets")
+					self.Icon:SetTexCoord(0.0078125, 0.585938, 0.365234, 0.509766)
+				elseif (self.Icon:GetAtlas() == "Taxi_Frame_Green") then
+					self.Icon:SetTexture("Interface\\AddOns\\ClassicFrames\\icons\\taxiassets")
+					self.Icon:SetTexCoord(0.0078125, 0.585938, 0.216797, 0.361328)
+				end
+			end
+		end)
 		
 		FlightMapFrame:HookScript("OnShow", function(self)
 			self:ClearAllPoints()
