902cb3aeb92d7e33e6d51fbc9ebcdb8e291f33f3
Daenarys <androidwoz@gmail.com>
2024-10-03 13:40:09 +0200

    fix overlapping textures

--- ClassicFrames/skins/OrderHallTalents.lua
+++ ClassicFrames/skins/OrderHallTalents.lua
@@ -2,21 +2,32 @@ local f = CreateFrame("Frame")
 f:RegisterEvent("ADDON_LOADED")
 f:SetScript("OnEvent", function(self, event, name)
 	if name == "Blizzard_OrderHallUI" then
-		hooksecurefunc(OrderHallTalentFrame, 'RefreshAllData', function(frame)
-			ApplyCloseButton(OrderHallTalentFrameCloseButton)
+		hooksecurefunc(OrderHallTalentFrame, 'SetUseThemedTextures', function(frame, isThemed)
+			if isThemed then
+				OrderHallTalentFrameCloseButton:SetSize(32, 32)
+				OrderHallTalentFrameCloseButton:SetDisabledTexture("Interface\\Buttons\\UI-Panel-MinimizeButton-Disabled")
+				OrderHallTalentFrameCloseButton:SetNormalTexture("Interface\\Buttons\\UI-Panel-MinimizeButton-Up")
+				OrderHallTalentFrameCloseButton:SetPushedTexture("Interface\\Buttons\\UI-Panel-MinimizeButton-Down")
+				OrderHallTalentFrameCloseButton:SetHighlightTexture("Interface\\Buttons\\UI-Panel-MinimizeButton-Highlight")
+				OrderHallTalentFrameCloseButton:ClearAllPoints()
+				OrderHallTalentFrameCloseButton:SetPoint("TOPRIGHT", 4, 4)
+				OrderHallTalentFrameCloseButton.Border:SetPoint("CENTER", -1, -1)
+			else
+				ApplyCloseButton(OrderHallTalentFrameCloseButton)
 
-			OrderHallTalentFrame.PortraitContainer.CircleMask:Hide()
+				OrderHallTalentFrame.PortraitContainer.CircleMask:Hide()
 
-			OrderHallTalentFramePortrait:SetSize(61, 61)
-			OrderHallTalentFramePortrait:ClearAllPoints()
-			OrderHallTalentFramePortrait:SetPoint("TOPLEFT", -6, 8)
+				OrderHallTalentFramePortrait:SetSize(61, 61)
+				OrderHallTalentFramePortrait:ClearAllPoints()
+				OrderHallTalentFramePortrait:SetPoint("TOPLEFT", -6, 8)
 
-			OrderHallTalentFrame.TitleContainer:ClearAllPoints()
-			OrderHallTalentFrame.TitleContainer:SetPoint("TOPLEFT", OrderHallTalentFrame, "TOPLEFT", 58, 0)
-			OrderHallTalentFrame.TitleContainer:SetPoint("TOPRIGHT", OrderHallTalentFrame, "TOPRIGHT", -58, 0)
+				OrderHallTalentFrame.TitleContainer:ClearAllPoints()
+				OrderHallTalentFrame.TitleContainer:SetPoint("TOPLEFT", OrderHallTalentFrame, "TOPLEFT", 58, 0)
+				OrderHallTalentFrame.TitleContainer:SetPoint("TOPRIGHT", OrderHallTalentFrame, "TOPRIGHT", -58, 0)
 
-			ApplyTitleBg(OrderHallTalentFrame)
-			ApplyNineSlicePortrait(OrderHallTalentFrame)
+				ApplyTitleBg(OrderHallTalentFrame)
+				ApplyNineSlicePortrait(OrderHallTalentFrame)
+			end
 		end)
 	end
 end)
