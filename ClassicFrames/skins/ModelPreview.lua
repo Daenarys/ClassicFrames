635805423fa545badd9655d3d344ebb5d0c69552
Daenarys <androidwoz@gmail.com>
2024-10-03 15:03:39 +0200

    skinned modelpreview frame

---
+++ ClassicFrames/skins/ModelPreview.lua
@@ -0,0 +1,10 @@
+if not _G.ModelPreviewFrame then return end
+
+ApplyCloseButton(ModelPreviewFrameCloseButton)
+
+ModelPreviewFrame.TitleContainer:ClearAllPoints()
+ModelPreviewFrame.TitleContainer:SetPoint("TOPLEFT", ModelPreviewFrame, "TOPLEFT", 58, 0)
+ModelPreviewFrame.TitleContainer:SetPoint("TOPRIGHT", ModelPreviewFrame, "TOPRIGHT", -58, 0)
+
+ApplyTitleBgNoPortrait(ModelPreviewFrame)
+ApplyNineSliceNoPortrait(ModelPreviewFrame)
