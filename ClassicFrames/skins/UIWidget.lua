480d026f842228331130f8f7787602d4785a3dc6
Daenarys <androidwoz@gmail.com>
2024-10-09 17:01:45 +0200

    revert this

--- ClassicFrames/skins/UIWidget.lua
+++ ClassicFrames/skins/UIWidget.lua
@@ -1,10 +1,3 @@
-if UIWidgetBelowMinimapContainerFrame then
-	UIWidgetBelowMinimapContainerFrame:HookScript("OnUpdate", function(self)
-		self:ClearAllPoints()
-		self:SetPoint("TOPRIGHT", MinimapCluster, "BOTTOMRIGHT")
-	end)
-end
+if not _G.UIWidgetCenterDisplayFrame then return end
 
-if UIWidgetCenterDisplayFrame then
-	ApplyDialogBorder(UIWidgetCenterDisplayFrame.NineSlice)
-end
+ApplyDialogBorder(UIWidgetCenterDisplayFrame.NineSlice)
