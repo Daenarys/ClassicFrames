fa6e80791913b0b8fc33b7e2af846d225dec4df9
Daenarys <androidwoz@gmail.com>
2024-10-25 17:10:32 +0200

    for targetframe aswell

--- ClassicFrames/skins/unitframes/TargetFrame.lua
+++ ClassicFrames/skins/unitframes/TargetFrame.lua
@@ -9,9 +9,9 @@ function CfTargetFrame_OnLoad(self, unit)
 		_G[thisName.."HealthBar"], _G[thisName.."HealthBarText"],
 		_G[thisName.."ManaBar"], _G[thisName.."ManaBarText"],
 		nil, nil, nil,
-		_G[thisName.."MyHealPredictionBar"], _G[thisName.."OtherHealPredictionBar"],
-		_G[thisName.."TotalAbsorbBar"], _G[thisName.."OverAbsorbGlow"],
-		_G[thisName.."OverHealAbsorbGlow"], _G[thisName.."HealAbsorbBar"],
+		_G[thisName.."HealthBar"].MyHealPredictionBar, _G[thisName.."HealthBar"].OtherHealPredictionBar,
+		_G[thisName.."HealthBar"].TotalAbsorbBar, _G[thisName.."HealthBar"].OverAbsorbGlow,
+		_G[thisName.."HealthBar"].OverHealAbsorbGlow, _G[thisName.."HealthBar"].HealAbsorbBar,
 		nil, nil)
 
 	if CfTargetFrame then
@@ -23,7 +23,7 @@ function CfTargetFrame_OnLoad(self, unit)
 		CfTargetFrameManaBarTextRight:SetParent(TargetFrame.TargetFrameContent.TargetFrameContentContextual)
 		CfTargetFrameDeadText:SetParent(TargetFrame.TargetFrameContent.TargetFrameContentContextual)
 		CfTargetFrameUnconsciousText:SetParent(TargetFrame.TargetFrameContent.TargetFrameContentContextual)
-		CfTargetFrameOverAbsorbGlow:SetParent(TargetFrame.TargetFrameContent.TargetFrameContentContextual)
+		CfTargetFrameHealthBar.OverAbsorbGlow:SetParent(TargetFrame.TargetFrameContent.TargetFrameContentContextual)
 	end
 
 	if CfFocusFrame then
@@ -35,7 +35,7 @@ function CfTargetFrame_OnLoad(self, unit)
 		CfFocusFrameManaBarTextRight:SetParent(FocusFrame.TargetFrameContent.TargetFrameContentContextual)
 		CfFocusFrameDeadText:SetParent(FocusFrame.TargetFrameContent.TargetFrameContentContextual)
 		CfFocusFrameUnconsciousText:SetParent(FocusFrame.TargetFrameContent.TargetFrameContentContextual)
-		CfFocusFrameOverAbsorbGlow:SetParent(FocusFrame.TargetFrameContent.TargetFrameContentContextual)
+		CfFocusFrameHealthBar.OverAbsorbGlow:SetParent(FocusFrame.TargetFrameContent.TargetFrameContentContextual)
 	end
 
 	self:UnregisterEvent("UNIT_NAME_UPDATE")
