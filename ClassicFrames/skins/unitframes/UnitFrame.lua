c7dabc5899b1b1d4ba7f1d8909162da7c4ab66f1
Daenarys <androidwoz@gmail.com>
2024-10-27 18:04:22 +0100

    remove lockcolor

--- ClassicFrames/skins/unitframes/UnitFrame.lua
+++ ClassicFrames/skins/unitframes/UnitFrame.lua
@@ -742,19 +742,17 @@ hooksecurefunc("UnitFrameManaBar_UpdateType", function(manaBar)
 	manaBar:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
 
 	if ( info ) then
-		if ( not manaBar.lockColor ) then
-			local playerDeadOrGhost = (manaBar.unit == "player" and (UnitIsDead("player") or UnitIsGhost("player")))
-			if ( info.atlas ) then
-				manaBar:SetStatusBarTexture(info.atlas)
-				manaBar:SetStatusBarColor(1, 1, 1)
-				manaBar:GetStatusBarTexture():SetDesaturated(playerDeadOrGhost)
-				manaBar:GetStatusBarTexture():SetAlpha(playerDeadOrGhost and 0.5 or 1)
+		local playerDeadOrGhost = (manaBar.unit == "player" and (UnitIsDead("player") or UnitIsGhost("player")));
+		if ( info.atlas ) then
+			manaBar:SetStatusBarTexture(info.atlas);
+			manaBar:SetStatusBarColor(1, 1, 1);
+			manaBar:GetStatusBarTexture():SetDesaturated(playerDeadOrGhost);
+			manaBar:GetStatusBarTexture():SetAlpha(playerDeadOrGhost and 0.5 or 1);
+		else
+			if ( playerDeadOrGhost ) then
+				manaBar:SetStatusBarColor(0.6, 0.6, 0.6, 0.5);
 			else
-				if ( playerDeadOrGhost ) then
-					manaBar:SetStatusBarColor(0.6, 0.6, 0.6, 0.5)
-				else
-					manaBar:SetStatusBarColor(info.r, info.g, info.b)
-				end
+				manaBar:SetStatusBarColor(info.r, info.g, info.b);
 			end
 		end
 	else
