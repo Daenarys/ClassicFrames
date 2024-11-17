016696e7c410934447f359c29f28dc1c0d4d6166
Daenarys <androidwoz@gmail.com>
2024-11-17 12:39:38 +0100

    remove ilvl show req

--- ClassicFrames/skins/Character.lua
+++ ClassicFrames/skins/Character.lua
@@ -163,23 +163,6 @@ CharacterFrame:HookScript("OnHide", function()
 	CfPlayerFrameManaBar:HideStatusBarText()
 end)
 
-hooksecurefunc("PaperDollFrame_UpdateStats", function()
-	local level = UnitLevel("player")
-	local MIN_PLAYER_LEVEL_FOR_ITEM_LEVEL_DISPLAY = 70
-
-	if ( level >= MIN_PLAYER_LEVEL_FOR_ITEM_LEVEL_DISPLAY ) then
-		CharacterStatsPane.ItemLevelCategory:Show()
-		CharacterStatsPane.ItemLevelFrame:Show()
-		CharacterStatsPane.AttributesCategory:ClearAllPoints()
-		CharacterStatsPane.AttributesCategory:SetPoint("TOP", CharacterStatsPane.ItemLevelFrame, "BOTTOM", 0, 0)
-	else
-		CharacterStatsPane.ItemLevelCategory:Hide()
-		CharacterStatsPane.ItemLevelFrame:Hide()
-		CharacterStatsPane.AttributesCategory:ClearAllPoints()
-		CharacterStatsPane.AttributesCategory:SetPoint("TOP", CharacterStatsPane, "TOP", 0, -2)
-	end
-end)
-
 PaperDollFrame:HookScript("OnShow", function()
 	CharacterModelScene.ControlFrame:Hide()
 end)
