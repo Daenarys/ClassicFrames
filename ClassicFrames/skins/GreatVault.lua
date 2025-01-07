380e7507472aac27157b94c9e71143bb0ad70dc0
Daenarys <androidwoz@gmail.com>
2025-01-08 00:25:23 +0100

    remove this

--- ClassicFrames/skins/GreatVault.lua
+++ ClassicFrames/skins/GreatVault.lua
@@ -11,15 +11,6 @@ f:SetScript("OnEvent", function(self, event, name)
 		WeeklyRewardsFrame.CloseButton:SetPoint("TOPRIGHT", -5, -2)
 		UIPanelCloseButton_SetBorderAtlas(WeeklyRewardsFrame.CloseButton, "UI-Frame-Oribos-ExitButtonBorder", -1, 1)
 
-		WeeklyRewardsFrame.BorderContainer.TopDecor:SetAlpha(0)
-
-		hooksecurefunc(WeeklyRewardsFrame, "UpdateOverlay", function()
-			local overlay = WeeklyRewardsFrame.Overlay
-			if overlay then
-				ApplyDialogBorder(WeeklyRewardsFrame.Overlay.NineSlice)
-			end
-		end)
-
 		WeeklyRewardsFrame:HookScript("OnShow", function(self)
 			self:ClearAllPoints()
 			self:SetPoint("CENTER", 0, 39)
