1c2b7981efe81089252cfb3c41445bccfa13a515
Daenarys <androidwoz@gmail.com>
2025-01-07 11:29:53 +0100

    skinned playerframe groupindicator

--- ClassicFrames/skins/unitframes/PlayerFrame.lua
+++ ClassicFrames/skins/unitframes/PlayerFrame.lua
@@ -51,6 +51,29 @@ PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HitIndicator:SetParent(Pla
 PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HitIndicator.HitText:ClearAllPoints()
 PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HitIndicator.HitText:SetPoint("CENTER", PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HitIndicator, "TOPLEFT", 54, -46)
 
+local groupIndicator = PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual.GroupIndicator
+if groupIndicator then
+	groupIndicator.GroupIndicatorLeft:SetSize(24, 16)
+	groupIndicator.GroupIndicatorLeft:SetTexture("Interface\\CharacterFrame\\UI-CharacterFrame-GroupIndicator")
+	groupIndicator.GroupIndicatorLeft:SetTexCoord(0, 0.1875, 0, 1)
+	groupIndicator.GroupIndicatorLeft:SetAlpha(0.3)
+	groupIndicator.GroupIndicatorRight:SetSize(24, 16)
+	groupIndicator.GroupIndicatorRight:SetTexture("Interface\\CharacterFrame\\UI-CharacterFrame-GroupIndicator")
+	groupIndicator.GroupIndicatorRight:SetTexCoord(0.53125, 0.71875, 0, 1)
+	groupIndicator.GroupIndicatorRight:SetAlpha(0.3)
+	if (groupIndicator.GroupIndicatorMiddle == nil) then
+		groupIndicator.GroupIndicatorMiddle = groupIndicator:CreateTexture(nil, "BACKGROUND")
+		groupIndicator.GroupIndicatorMiddle:SetSize(0, 16)
+		groupIndicator.GroupIndicatorMiddle:SetTexture("Interface\\CharacterFrame\\UI-CharacterFrame-GroupIndicator")
+		groupIndicator.GroupIndicatorMiddle:SetTexCoord(0.1875, 0.53125, 0, 1)
+		groupIndicator.GroupIndicatorMiddle:SetPoint("LEFT", groupIndicator.GroupIndicatorLeft, "RIGHT")
+		groupIndicator.GroupIndicatorMiddle:SetPoint("RIGHT", groupIndicator.GroupIndicatorRight, "LEFT")
+		groupIndicator.GroupIndicatorMiddle:SetAlpha(0.3)
+	end
+	select(3, groupIndicator:GetRegions()):SetAlpha(0)
+	PlayerFrameGroupIndicatorText:SetPoint("LEFT", 20, -2)
+end
+
 if (_G.AlternatePowerBar) then
 	AlternatePowerBar:SetSize(104, 12)
 	AlternatePowerBar:ClearAllPoints()
@@ -216,6 +239,8 @@ hooksecurefunc("PlayerFrame_ToPlayerArt", function(self)
 	StatusTexture:SetPoint("TOPLEFT", 16, -12)
 	StatusTexture:SetBlendMode("ADD")
 
+	self.PlayerFrameContent.PlayerFrameContentContextual.GroupIndicator:ClearAllPoints()
+	self.PlayerFrameContent.PlayerFrameContentContextual.GroupIndicator:SetPoint("BOTTOMLEFT", CfPlayerFrame, "TOPLEFT", 97, -20)
 	self.PlayerFrameContent.PlayerFrameContentContextual.RoleIcon:SetPoint("TOPLEFT", 76, -19)
 
 	CfPlayerFrameHealthBar:SetWidth(119)
@@ -254,8 +279,10 @@ hooksecurefunc("PlayerFrame_ToVehicleArt", function(self)
 	FrameFlash:SetDrawLayer("BACKGROUND")
 
 	local StatusTexture = self.PlayerFrameContent.PlayerFrameContentMain.StatusTexture
-	StatusTexture:Hide()
+	StatusTexture:SetAlpha(0)
 
+	self.PlayerFrameContent.PlayerFrameContentContextual.GroupIndicator:ClearAllPoints()
+	self.PlayerFrameContent.PlayerFrameContentContextual.GroupIndicator:SetPoint("BOTTOMLEFT", CfPlayerFrame, "TOPLEFT", 97, -13)
 	self.PlayerFrameContent.PlayerFrameContentContextual.RoleIcon:SetPoint("TOPLEFT", 76, -19)
 
 	PlayerName:SetParent(self.PlayerFrameContainer)
