2724654cfad33ccfb2e00d139cdd73f6c9a337e8
Daenarys <androidwoz@gmail.com>
2025-02-13 12:39:29 +0100

    reflect 7.x

--- ClassicFrames/skins/ObjectiveTracker.lua
+++ ClassicFrames/skins/ObjectiveTracker.lua
@@ -34,7 +34,7 @@ for _, tracker in pairs(trackers) do
 	tracker.Header.Background:SetPoint("TOPLEFT", -19, 14)
 	tracker.Header.Text:SetPoint("LEFT", 14, 0)
 	tracker.Header.MinimizeButton:SetSize(15, 14)
-	tracker.Header.MinimizeButton:SetPoint("RIGHT", -15, 0)
+	tracker.Header.MinimizeButton:SetPoint("RIGHT", -6, 1)
 	tracker.Header.MinimizeButton:SetHighlightAtlas("UI-QuestTrackerButton-Red-Highlight", "ADD")
 	SetCollapsed(tracker.Header, _G.ObjectiveTrackerFrame.isCollapsed)
 	hooksecurefunc(tracker.Header, 'SetCollapsed', SetCollapsed)
@@ -72,6 +72,7 @@ hooksecurefunc(ScenarioObjectiveTracker.StageBlock, "UpdateStageBlock", function
 	block.FinalBG:SetAtlas("ScenarioTrackerToast-FinalFiligree", true)
 	block.FinalBG:ClearAllPoints()
 	block.FinalBG:SetPoint("TOPLEFT", 4, -5)
+	block.Stage:SetFontObject("QuestTitleFont")
 	if bit.band(flags, SCENARIO_FLAG_SUPRESS_STAGE_TEXT) == SCENARIO_FLAG_SUPRESS_STAGE_TEXT then
 		block.Stage:SetSize(172, 36)
 	else
