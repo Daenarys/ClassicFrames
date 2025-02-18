3a8ecd70e77dd7d7de6eea87e136246fa4cb401c
Daenarys <androidwoz@gmail.com>
2025-02-18 21:07:43 +0100

    remove separators

--- ClassicFrames/skins/LFG.lua
+++ ClassicFrames/skins/LFG.lua
@@ -356,10 +356,6 @@ ApplyDropDown(LFGListEntryCreationActivityDropdown)
 ApplyDropDown(LFGListEntryCreationPlayStyleDropdown)
 ApplyFilterDropDown(LFGListFrame.SearchPanel.FilterButton)
 
-ApplySeparator(LFDQueueFrameFindGroupButton)
-ApplySeparator(ScenarioQueueFrameFindGroupButton)
-ApplySeparator(RaidFinderFrameFindRaidButton)
-
 hooksecurefunc("LFG_UpdateAvailableRoleButton", function(button, canBeRole)
 	if ( button.background ) then
 		button.background:SetTexture("Interface\\LFGFrame\\UI-LFG-ICONS-ROLEBACKGROUNDS")
