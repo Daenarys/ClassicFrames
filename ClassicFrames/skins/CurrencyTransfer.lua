7c1b70fdaccea84f4930ba300ea941976706878a
Daenarys <androidwoz@gmail.com>
2025-02-18 22:59:55 +0100

    update bg texture

--- ClassicFrames/skins/CurrencyTransfer.lua
+++ ClassicFrames/skins/CurrencyTransfer.lua
@@ -8,5 +8,7 @@ if _G.CurrencyTransferMenu then
 	ApplyTitleBgNoPortrait(CurrencyTransferMenu)
 	ApplyNineSliceNoPortrait(CurrencyTransferMenu)
 
+	CurrencyTransferMenu.Bg:SetTexture("Interface\\AddOns\\ClassicFrames\\icons\\UI-Background-Rock", true, true)
+
 	ApplyDropDown(CurrencyTransferMenu.SourceSelector.Dropdown)
 end
