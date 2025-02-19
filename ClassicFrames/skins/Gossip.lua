a4df0e962a9bed825cf4cba3856df22b4c27c5c2
Daenarys <androidwoz@gmail.com>
2025-02-19 01:29:49 +0100

    remove those

--- ClassicFrames/skins/Gossip.lua
+++ ClassicFrames/skins/Gossip.lua
@@ -12,8 +12,6 @@ GossipFrame.TitleContainer:ClearAllPoints()
 GossipFrame.TitleContainer:SetPoint("TOPLEFT", GossipFrame, "TOPLEFT", 66, -1)
 GossipFrame.TitleContainer:SetPoint("TOPRIGHT", GossipFrame, "TOPRIGHT", -60, 1)
 
-GossipFrameTitleText:SetTextColor(255, 255, 255, 1)
-
 ApplyTitleBg(GossipFrame)
 ApplyNineSlicePortrait(GossipFrame)
 
