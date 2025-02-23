05c4b82d9ad2f3eb420d7f2255371b8b67980411
Daenarys <androidwoz@gmail.com>
2025-02-23 18:04:55 +0100

    sync 11.1

--- ClassicFrames/skins/QuestLog.lua
+++ ClassicFrames/skins/QuestLog.lua
@@ -1,16 +1,16 @@
 if not _G.QuestScrollFrame then return end
 
-QuestScrollFrame.SearchBox:Hide()
-QuestScrollFrame.BorderFrame.Shadow:SetAlpha(0)
-QuestScrollFrame.BorderFrame.TopDetail:SetAlpha(0)
-QuestMapFrame.CampaignOverview.BorderFrame.TopDetail:SetAlpha(0)
-QuestMapFrame.DetailsFrame.BorderFrame.TopDetail:SetAlpha(0)
-QuestMapFrame.SettingsDropdown:Hide()
-
 QuestScrollFrame:SetPoint("TOPLEFT")
 QuestScrollFrame.Background:SetPoint("BOTTOMRIGHT", 25, 0)
 QuestScrollFrame.BorderFrame:SetPoint("TOPLEFT", -3, 3)
-QuestMapFrame.CampaignOverview.BorderFrame:SetPoint("TOPLEFT", -3, 3)
+QuestScrollFrame.BorderFrame.Shadow:SetAlpha(0)
+QuestScrollFrame.BorderFrame.TopDetail:SetAlpha(0)
+QuestScrollFrame.SearchBox:Hide()
+QuestScrollFrame.SettingsDropdown:Hide()
+
+QuestMapFrame.DetailsFrame.BorderFrame.TopDetail:SetAlpha(0)
+QuestMapFrame.QuestsFrame.CampaignOverview.BorderFrame:SetPoint("TOPLEFT", -3, 3)
+QuestMapFrame.QuestsFrame.CampaignOverview.BorderFrame.TopDetail:SetAlpha(0)
 
 ApplySearchBox(QuestScrollFrame.SearchBox)
 
@@ -20,4 +20,10 @@ hooksecurefunc(QuestScrollFrame, 'UpdateBackground', function(self)
 	elseif (self.Background:GetAtlas() == "QuestLog-empty-quest-background") then
 		self.Background:SetAtlas("NoQuestsBackground")
 	end
+end)
+
+hooksecurefunc(QuestMapFrame, 'ValidateTabs', function(self)
+	self.QuestsTab:Hide()
+	self.EventsTab:Hide()
+	self.MapLegendTab:Hide()
 end)
