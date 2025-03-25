0674b7ac9ddb10c3d952664f04f5d473fd27f12b
Daenarys <androidwoz@gmail.com>
2025-03-25 12:02:27 +0100

    also adjust position here

--- ClassicFrames/skins/TotemFrame.lua
+++ ClassicFrames/skins/TotemFrame.lua
@@ -1,6 +1,9 @@
 if not _G.TotemFrame then return end
 
 hooksecurefunc(TotemFrame, "Update", function(self)
+	self:ClearAllPoints()
+	self:SetPoint("TOPLEFT", PlayerFrame, "TOPLEFT", -5, -91)
+
 	for child in self.totemPool:EnumerateActive() do
 		child.Border:SetSize(38, 38)
 		child.Border:SetTexture("Interface\\CharacterFrame\\TotemBorder")
