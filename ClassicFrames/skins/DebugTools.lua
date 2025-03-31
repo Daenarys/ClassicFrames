270aa5dc2515b301a35baab600d4ea11ffe07b00
Daenarys <androidwoz@gmail.com>
2025-03-31 12:30:07 +0200

    adjust scrollbar position

--- ClassicFrames/skins/DebugTools.lua
+++ ClassicFrames/skins/DebugTools.lua
@@ -4,5 +4,8 @@ f:SetScript("OnEvent", function(self, event, name)
 	if name == "Blizzard_DebugTools" then
 		TableAttributeDisplay.CloseButton:ClearAllPoints()
 		TableAttributeDisplay.CloseButton:SetPoint("TOPRIGHT", 2, 1)
+
+		TableAttributeDisplay.LinesScrollFrame.ScrollBar:SetPoint("TOPLEFT", TableAttributeDisplay.LinesScrollFrame, "TOPRIGHT", -6, -16)
+		TableAttributeDisplay.LinesScrollFrame.ScrollBar:SetPoint("BOTTOMLEFT", TableAttributeDisplay.LinesScrollFrame, "BOTTOMRIGHT", 6, 2)
 	end
 end)
