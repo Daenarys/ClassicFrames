1a40914710e5761413b8be19112fcd6b887e99c4
Daenarys <androidwoz@gmail.com>
2025-04-02 17:20:55 +0200

    hide those

--- ClassicFrames/skins/unitframes/PetFrame.lua
+++ ClassicFrames/skins/unitframes/PetFrame.lua
@@ -1,20 +1,12 @@
 if not _G.PetFrame then return end
 
-PetFrameHealthBarText:SetFontObject(SystemFont_Outline_Small)
-PetFrameHealthBarTextLeft:SetFontObject(SystemFont_Outline_Small)
-PetFrameHealthBarTextRight:SetFontObject(SystemFont_Outline_Small)
+PetFrameHealthBarText:SetAlpha(0)
+PetFrameHealthBarTextLeft:SetAlpha(0)
+PetFrameHealthBarTextRight:SetAlpha(0)
 
-PetFrameHealthBarText:SetTextColor(1, 1, 1)
-PetFrameHealthBarTextLeft:SetTextColor(1, 1, 1)
-PetFrameHealthBarTextRight:SetTextColor(1, 1, 1)
-
-PetFrameManaBarText:SetFontObject(SystemFont_Outline_Small)
-PetFrameManaBarTextLeft:SetFontObject(SystemFont_Outline_Small)
-PetFrameManaBarTextRight:SetFontObject(SystemFont_Outline_Small)
-
-PetFrameManaBarText:SetTextColor(1, 1, 1)
-PetFrameManaBarTextLeft:SetTextColor(1, 1, 1)
-PetFrameManaBarTextRight:SetTextColor(1, 1, 1)
+PetFrameManaBarText:SetAlpha(0)
+PetFrameManaBarTextLeft:SetAlpha(0)
+PetFrameManaBarTextRight:SetAlpha(0)
 
 local _, class = UnitClass("player")
 if ( class == "DEATHKNIGHT" or class == "ROGUE") then
