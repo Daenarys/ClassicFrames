429566c7c74a3a106a9219fcf59880f4781c3ceb
Daenarys <androidwoz@gmail.com>
2025-04-08 11:44:56 +0200

    move tabs down

--- ClassicFrames/skins/AuctionHouse.lua
+++ ClassicFrames/skins/AuctionHouse.lua
@@ -2,6 +2,8 @@ local f = CreateFrame("Frame")
 f:RegisterEvent("ADDON_LOADED")
 f:SetScript("OnEvent", function(self, event, name)
 	if name == "Blizzard_AuctionHouseUI" then
+		AuctionHouseFrameBuyTab:SetPoint("BOTTOMLEFT", 20, -31)
+
 		ApplyDropDown(AuctionHouseFrame.CommoditiesSellFrame.Duration.Dropdown)
 		ApplyDropDown(AuctionHouseFrame.ItemSellFrame.Duration.Dropdown)
 		ApplyFilterDropDown(AuctionHouseFrame.SearchBar.FilterButton)
