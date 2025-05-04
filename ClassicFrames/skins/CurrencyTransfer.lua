b7f59772cbde703c5e9eb08914bd58ddd4c6f1c5
Daenarys <androidwoz@gmail.com>
2025-05-04 12:16:31 +0200

    clean this up a bit

--- ClassicFrames/skins/CurrencyTransfer.lua
+++ ClassicFrames/skins/CurrencyTransfer.lua
@@ -1,15 +1,15 @@
-if _G.CurrencyTransferMenu then
-	ApplyCloseButton(CurrencyTransferMenuCloseButton)
+if not _G.CurrencyTransferMenu then return end
 
-	CurrencyTransferMenu.TitleContainer:ClearAllPoints()
-	CurrencyTransferMenu.TitleContainer:SetPoint("TOPLEFT", CurrencyTransferMenu, "TOPLEFT", 58, 0)
-	CurrencyTransferMenu.TitleContainer:SetPoint("TOPRIGHT", CurrencyTransferMenu, "TOPRIGHT", -58, 0)
+ApplyCloseButton(CurrencyTransferMenuCloseButton)
 
-	ApplyTitleBgNoPortrait(CurrencyTransferMenu)
-	ApplyNineSliceNoPortrait(CurrencyTransferMenu)
+CurrencyTransferMenu.TitleContainer:ClearAllPoints()
+CurrencyTransferMenu.TitleContainer:SetPoint("TOPLEFT", CurrencyTransferMenu, "TOPLEFT", 58, 0)
+CurrencyTransferMenu.TitleContainer:SetPoint("TOPRIGHT", CurrencyTransferMenu, "TOPRIGHT", -58, 0)
 
-	CurrencyTransferMenu.Background:Hide()
-	CurrencyTransferMenu.Bg:SetTexture("Interface\\AddOns\\ClassicFrames\\icons\\UI-Background-Rock", true, true)
+ApplyTitleBgNoPortrait(CurrencyTransferMenu)
+ApplyNineSliceNoPortrait(CurrencyTransferMenu)
 
-	ApplyDropDown(CurrencyTransferMenu.SourceSelector.Dropdown)
-end
+CurrencyTransferMenu.Background:Hide()
+CurrencyTransferMenu.Bg:SetTexture("Interface\\AddOns\\ClassicFrames\\icons\\UI-Background-Rock", true, true)
+
+ApplyDropDown(CurrencyTransferMenu.SourceSelector.Dropdown)
