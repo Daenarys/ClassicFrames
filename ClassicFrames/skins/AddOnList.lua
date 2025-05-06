da2c4105d1b700ecae1f39f0df0d2fb8b37febdd
Daenarys <androidwoz@gmail.com>
2025-05-06 11:57:46 +0200

    sync 7.x skin

--- ClassicFrames/skins/AddOnList.lua
+++ ClassicFrames/skins/AddOnList.lua
@@ -11,6 +11,8 @@ AddonList.TitleContainer:SetPoint("TOPRIGHT", AddonList, "TOPRIGHT", -58, 0)
 ApplyTitleBgNoPortrait(AddonList)
 ApplyNineSliceNoPortrait(AddonList)
 
+AddonList.Bg:SetTexture("Interface\\AddOns\\ClassicFrames\\icons\\UI-Background-Rock", true, true)
+
 AddonList.ScrollBar:SetSize(25, 560)
 AddonList.ScrollBar:ClearAllPoints()
 AddonList.ScrollBar:SetPoint("TOPLEFT", AddonList.ScrollBox, "TOPRIGHT", 5, 3)
