8258698a6c73d71af7d9dcfb6af834aad201749e
Daenarys <androidwoz@gmail.com>
2025-05-24 11:49:32 +0200

    gossip frame improvements

--- ClassicFrames/skins/Gossip.lua
+++ ClassicFrames/skins/Gossip.lua
@@ -1,3 +1,9 @@
 if not _G.GossipFrame then return end
 
-GossipFramePortrait:SetParent(GossipFrame)
+GossipFrame:SetHeight(496)
+
+ _G.select(18, _G.GossipFrame:GetRegions()):SetSize(510, 620)
+ _G.select(18, _G.GossipFrame:GetRegions()):SetPoint("TOPLEFT", 7, -62)
+
+GossipFrame.GreetingPanel.ScrollBar:SetPoint("TOPLEFT", GossipFrame.GreetingPanel.ScrollBox, "TOPRIGHT", 0, 5)
+GossipFrame.GreetingPanel.ScrollBar:SetPoint("BOTTOMLEFT", GossipFrame.GreetingPanel.ScrollBox, "BOTTOMRIGHT", 0, -71)
