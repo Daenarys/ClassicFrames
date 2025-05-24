554e292ee96c3cee9e4086d7804323e797671dcb
Daenarys <androidwoz@gmail.com>
2025-05-24 13:50:13 +0200

    cleanup

--- ClassicFrames/skins/Friends.lua
+++ ClassicFrames/skins/Friends.lua
@@ -8,13 +8,10 @@ FriendsFrame.TitleContainer:SetPoint("TOPRIGHT", FriendsFrame, "TOPRIGHT", -58, 
 
 FriendsFrameTab1:ClearAllPoints()
 FriendsFrameTab1:SetPoint("BOTTOMLEFT", 5, -30)
-
 FriendsFrameTab2:ClearAllPoints()
 FriendsFrameTab2:SetPoint("LEFT", FriendsFrameTab1, "RIGHT", -15, 0)
-
 FriendsFrameTab3:ClearAllPoints()
 FriendsFrameTab3:SetPoint("LEFT", FriendsFrameTab2, "RIGHT", -15, 0)
-
 FriendsFrameTab4:ClearAllPoints()
 FriendsFrameTab4:SetPoint("LEFT", FriendsFrameTab3, "RIGHT", -15, 0)
 
