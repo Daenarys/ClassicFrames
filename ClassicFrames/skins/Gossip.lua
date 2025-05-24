0b58d47bca9347befabbfe1ac287389a91d763f8
Daenarys <androidwoz@gmail.com>
2025-05-24 12:47:10 +0200

    adjust title color

--- ClassicFrames/skins/Gossip.lua
+++ ClassicFrames/skins/Gossip.lua
@@ -2,6 +2,8 @@ if not _G.GossipFrame then return end
 
 GossipFrame:SetHeight(496)
 
+GossipFrame.TitleText:SetTextColor(255, 255, 255, 1)
+
  _G.select(18, _G.GossipFrame:GetRegions()):SetSize(510, 620)
  _G.select(18, _G.GossipFrame:GetRegions()):SetPoint("TOPLEFT", 7, -62)
 
