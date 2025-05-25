a8c1386176ad8a48d0847d7139de3a0690193a45
Daenarys <androidwoz@gmail.com>
2025-05-25 17:19:22 +0200

    adjust scrollbox position

--- ClassicFrames/skins/Gossip.lua
+++ ClassicFrames/skins/Gossip.lua
@@ -7,5 +7,6 @@ GossipFrame.TitleText:SetTextColor(255, 255, 255, 1)
  _G.select(18, _G.GossipFrame:GetRegions()):SetSize(510, 620)
  _G.select(18, _G.GossipFrame:GetRegions()):SetPoint("TOPLEFT", 7, -62)
 
-GossipFrame.GreetingPanel.ScrollBar:SetPoint("TOPLEFT", GossipFrame.GreetingPanel.ScrollBox, "TOPRIGHT", 0, 5)
-GossipFrame.GreetingPanel.ScrollBar:SetPoint("BOTTOMLEFT", GossipFrame.GreetingPanel.ScrollBox, "BOTTOMRIGHT", 0, -71)
+GossipFrame.GreetingPanel.ScrollBox:SetPoint("TOPLEFT", GossipFrame, "TOPLEFT", 5, -65)
+GossipFrame.GreetingPanel.ScrollBar:SetPoint("TOPLEFT", GossipFrame.GreetingPanel.ScrollBox, "TOPRIGHT", 2, 5)
+GossipFrame.GreetingPanel.ScrollBar:SetPoint("BOTTOMLEFT", GossipFrame.GreetingPanel.ScrollBox, "BOTTOMRIGHT", 2, -71)
