2b321d80e8d68959032425924b959ed387112320
Daenarys <androidwoz@gmail.com>
2025-05-30 23:26:59 +0200

    adjust height for ptr

--- ClassicFrames/skins/GameMenu.lua
+++ ClassicFrames/skins/GameMenu.lua
@@ -1,5 +1,9 @@
 if not _G.GameMenuFrame then return end
 
 hooksecurefunc("GameMenuFrame_UpdateVisibleButtons", function(self)
-	self:SetHeight(268)
+	if C_StorePublic.IsEnabled() then
+		self:SetHeight(268)
+	else
+		self:SetHeight(248)
+	end
 end)
