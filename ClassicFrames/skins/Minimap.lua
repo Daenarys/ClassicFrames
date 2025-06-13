a41aa8e6d24a0c0ec72b9758a611077ad96180d3
Daenarys <androidwoz@gmail.com>
2025-06-13 11:45:46 +0200

    minimap stuffz

--- ClassicFrames/skins/Minimap.lua
+++ ClassicFrames/skins/Minimap.lua
@@ -1,13 +1,6 @@
 if (C_AddOns.IsAddOnLoaded("SexyMap")) then return end
 
 -- minimap
-MinimapZoomIn:ClearAllPoints()
-MinimapZoomIn:SetPoint("CENTER", 72, -25)
-MinimapZoomOut:ClearAllPoints()
-MinimapZoomOut:SetPoint("CENTER", 50, -43)
-
-MiniMapTracking:ClearAllPoints()
-MiniMapTracking:SetPoint("TOPLEFT", 9, -45)
 MiniMapTrackingIcon:SetTexture("Interface\\Minimap\\Tracking\\None")
 
 hooksecurefunc("MiniMapTracking_Update", function()
@@ -23,29 +16,10 @@ MiniMapTrackingButton:SetScript("OnMouseUp", function()
 	MiniMapTrackingIconOverlay:Hide()
 end)
 
-MiniMapMailFrame:ClearAllPoints()
-MiniMapMailFrame:SetPoint("TOPRIGHT", 24, -37)
-
 GameTimeFrame:ClearAllPoints()
 GameTimeFrame:SetPoint("TOPRIGHT", Minimap, "TOPRIGHT", 20, -2)
 GameTimeFrame:GetFontString():ClearAllPoints()
 GameTimeFrame:GetFontString():SetPoint("CENTER", -1, -1)
-
-MiniMapWorldBorder:Hide()
-
-MiniMapWorldMapButton:SetSize(32, 32)
-MiniMapWorldMapButton:ClearAllPoints()
-MiniMapWorldMapButton:SetPoint("TOPRIGHT", MinimapBackdrop, "TOPRIGHT", 0, 21)
-MiniMapWorldMapButton:SetNormalTexture("Interface\\Minimap\\UI-Minimap-WorldMapSquare")
-MiniMapWorldMapButton:GetNormalTexture():SetSize(32, 32)
-MiniMapWorldMapButton:GetNormalTexture():SetTexCoord(0.0, 1, 0, 0.5)
-MiniMapWorldMapButton:SetPushedTexture("Interface\\Minimap\\UI-Minimap-WorldMapSquare")
-MiniMapWorldMapButton:GetPushedTexture():SetSize(32, 32)
-MiniMapWorldMapButton:GetPushedTexture():SetTexCoord(0.0, 1, 0.5, 1)
-MiniMapWorldMapButton:SetHighlightTexture("Interface\\Buttons\\UI-Common-MouseHilight")
-MiniMapWorldMapButton:GetHighlightTexture():SetSize(28, 28)
-MiniMapWorldMapButton:GetHighlightTexture():ClearAllPoints()
-MiniMapWorldMapButton:GetHighlightTexture():SetPoint("TOPRIGHT", MiniMapWorldMapButton, "TOPRIGHT")
 
 Minimap:HookScript("OnEvent", function(self, event, ...)
 	TimeManagerClockTicker:ClearAllPoints()
