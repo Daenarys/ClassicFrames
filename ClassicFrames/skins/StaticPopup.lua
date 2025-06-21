e04d25da62e7a1fb02ed6668a7fc1453fc2deb20
Daenarys <androidwoz@gmail.com>
2025-06-21 10:25:17 +0200

    fix/update game popup art

--- ClassicFrames/skins/StaticPopup.lua
+++ ClassicFrames/skins/StaticPopup.lua
@@ -1,3 +1,8 @@
-for i = 1, STATICPOPUP_NUMDIALOGS do
-	ApplyDialogBorder(_G["StaticPopup"..i].Border)
+for i = 1, 4 do
+	if not _G["StaticPopup"..i].BG.SetBackdrop then
+		Mixin(_G["StaticPopup"..i].BG, BackdropTemplateMixin)
+	end
+	_G["StaticPopup"..i].BG:SetBackdrop(BACKDROP_DIALOG_32_32)
+	_G["StaticPopup"..i].BG.Top:SetAlpha(0)
+	_G["StaticPopup"..i].BG.Bottom:SetAlpha(0)
 end
