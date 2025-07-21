fe69dc7529b8f22c89941aa8055b245ad8147fc7
Daenarys <androidwoz@gmail.com>
2025-07-21 11:33:33 +0200

    adjust closebutton position

--- ClassicFrames/skins/ClassTrainer.lua
+++ ClassicFrames/skins/ClassTrainer.lua
@@ -2,6 +2,8 @@ local f = CreateFrame("Frame")
 f:RegisterEvent("ADDON_LOADED")
 f:SetScript("OnEvent", function(self, event, name)
 	if name == "Blizzard_TrainerUI" then
+		ClassTrainerFrameCloseButton:SetPoint("TOPRIGHT", -30, -8)
+
 		ApplyFilterDropDown(ClassTrainerFrame.FilterDropdown)
 	end
 end)
