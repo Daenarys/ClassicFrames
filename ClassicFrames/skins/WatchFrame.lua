6002fa3f2c2a75670d450899f421b52bea4be922
Daenarys <androidwoz@gmail.com>
2025-07-25 00:31:18 +0200

    do this instead

--- ClassicFrames/skins/WatchFrame.lua
+++ ClassicFrames/skins/WatchFrame.lua
@@ -27,20 +27,9 @@ hooksecurefunc("QuestPOI_UpdateButtonStyle", function(poiButton)
 	local isSuperTracked = poiButton.questID == GetSuperTrackedQuestID()
 
 	if isSuperTracked then
-		poiButton:GetNormalTexture():SetTexCoord(0.500, 0.625, 0.375, 0.5)
-		poiButton:GetPushedTexture():SetTexCoord(0.375, 0.500, 0.375, 0.5)
+		poiButton.selected = true
 	else
-		if poiButton.style == "numeric" then
-			poiButton:GetNormalTexture():SetTexCoord(0.875, 1.00, 0.375, 0.5)
-			poiButton:GetPushedTexture():SetTexCoord(0.750, 0.875, 0.375, 0.5)
-		else
-			poiButton:GetNormalTexture():SetTexCoord(0.500, 0.625, 0.875, 1)
-			poiButton:GetPushedTexture():SetTexCoord(0.500, 0.625, 0.875, 1)
-		end
-	end
-
-	if poiButton.Glow then
-		poiButton.Glow:SetShown(isSuperTracked)
+		poiButton.selected = nil
 	end
 end)
 
