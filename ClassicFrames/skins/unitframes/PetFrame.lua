91868b6d702e4f0acb97debc16b3eb3c7d3d6f32
Daenarys <androidwoz@gmail.com>
2025-07-25 19:55:12 +0200

    no longer needed

--- ClassicFrames/skins/unitframes/PetFrame.lua
+++ ClassicFrames/skins/unitframes/PetFrame.lua
@@ -7,18 +7,3 @@ PetFrameHealthBarTextRight:SetAlpha(0)
 PetFrameManaBarText:SetAlpha(0)
 PetFrameManaBarTextLeft:SetAlpha(0)
 PetFrameManaBarTextRight:SetAlpha(0)
-
-local _, class = UnitClass("player")
-if ( class == "DEATHKNIGHT" or class == "ROGUE") then
-	PetFrame:SetPoint("TOPLEFT", PlayerFrame, "TOPLEFT", 60, -75)
-elseif ( class == "SHAMAN" or class == "DRUID" ) then
-	PetFrame:SetPoint("TOPLEFT", PlayerFrame, "TOPLEFT", 60, -100)
-elseif ( class == "WARLOCK" ) then
-	PetFrame:SetPoint("TOPLEFT", PlayerFrame, "TOPLEFT", 60, -90)
-elseif ( class == "PALADIN" ) then
-	PetFrame:SetPoint("TOPLEFT", PlayerFrame, "TOPLEFT", 60, -90)
-elseif ( class == "PRIEST" ) then
-	PetFrame:SetPoint("TOPLEFT", PlayerFrame, "TOPLEFT", 60, -90)
-elseif ( class == "MONK" ) then
-	PetFrame:SetPoint("TOPLEFT", PlayerFrame, "TOPLEFT", 90, -100)
-end
