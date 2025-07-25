0ce5651c532c737b9931159250b73a9ca375c9f6
Daenarys <androidwoz@gmail.com>
2025-07-25 16:59:22 +0200

    dont think this does anything

--- ClassicFrames/skins/WatchFrame.lua
+++ ClassicFrames/skins/WatchFrame.lua
@@ -1,28 +1,5 @@
 if not _G.WatchFrame then return end
 
-hooksecurefunc("WatchFrame_DisplayTrackedQuests", function()
-	local numQuestWatches = GetNumQuestWatches()
-
-	-- if supertracked quest is not in the quest log anymore, stop supertracking it
-	if ( GetQuestLogIndexByID(GetSuperTrackedQuestID()) == 0 ) then
-		SetSuperTrackedQuestID(0)
-	end
-
-	for i = 1, numQuestWatches do
-		local questIndex = GetQuestIndexForWatch(i)
-		local title, level, questTag, isHeader, isCollapsed, isComplete, frequency, questID, startEvent, displayQuestID, isOnMap, hasLocalPOI, isTask, isBounty, isStory, isHidden, isScaling = GetQuestLogTitle(questIndex)
-
-		if (GetSuperTrackedQuestID() == 0) then
-			SetSuperTrackedQuestID(questID)
-		end
-	end
-
-	local trackedQuestID = GetSuperTrackedQuestID()
-	if ( trackedQuestID ) then
-		QuestPOI_SelectButtonByQuestId(WatchFrameLines, trackedQuestID)
-	end
-end)
-
 hooksecurefunc("QuestPOI_UpdateButtonStyle", function(poiButton)
 	if ( WorldMapFrame and WorldMapFrame:IsShown() ) then
 		return
