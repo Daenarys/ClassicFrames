bb02ad24b63c6f9361c57135ce11759fcd36387f
Daenarys <androidwoz@gmail.com>
2025-10-07 14:21:06 +0200

    remove voidstorage

--- ClassicFrames/skins/VoidStorage.lua
+++
@@ -1,8 +0,0 @@
-local f = CreateFrame("Frame")
-f:RegisterEvent("ADDON_LOADED")
-f:SetScript("OnEvent", function(self, event, name)
-	if name == "Blizzard_VoidStorageUI" then
-		ApplyCloseButton(VoidStorageBorderFrame.CloseButton)
-		ApplySearchBox(VoidItemSearchBox)
-	end
-end)
