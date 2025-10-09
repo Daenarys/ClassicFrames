124c3ec4e0f1c8a3161715c862b1aef06feee1d4
Daenarys <androidwoz@gmail.com>
2025-10-09 13:58:42 +0200

    skinned cooldown viewer

---
+++ ClassicFrames/skins/CooldownViewer.lua
@@ -0,0 +1,27 @@
+if not _G.CooldownViewerSettings then return end
+
+ApplyCloseButton(CooldownViewerSettingsCloseButton)
+
+CooldownViewerSettings.PortraitContainer.CircleMask:Hide()
+
+CooldownViewerSettingsPortrait:SetSize(61, 61)
+CooldownViewerSettingsPortrait:ClearAllPoints()
+CooldownViewerSettingsPortrait:SetPoint("TOPLEFT", -6, 8)
+
+CooldownViewerSettings.TitleContainer:ClearAllPoints()
+CooldownViewerSettings.TitleContainer:SetPoint("TOPLEFT", CooldownViewerSettings, "TOPLEFT", 66, -1)
+CooldownViewerSettings.TitleContainer:SetPoint("TOPRIGHT", CooldownViewerSettings, "TOPRIGHT", -60, 1)
+
+ApplyTitleBg(CooldownViewerSettings)
+ApplyNineSlicePortrait(CooldownViewerSettings)
+
+CooldownViewerSettings.CooldownScroll.ScrollBar:SetSize(25, 560)
+CooldownViewerSettings.CooldownScroll.ScrollBar:ClearAllPoints()
+CooldownViewerSettings.CooldownScroll.ScrollBar:SetPoint("TOPLEFT", CooldownViewerSettings.CooldownScroll, "TOPRIGHT", 0, 2)
+CooldownViewerSettings.CooldownScroll.ScrollBar:SetPoint("BOTTOMLEFT", CooldownViewerSettings.CooldownScroll, "BOTTOMRIGHT", 0, -2)
+
+ApplyScrollBarArrow(CooldownViewerSettings.CooldownScroll.ScrollBar)
+ApplyScrollBarTrack(CooldownViewerSettings.CooldownScroll.ScrollBar.Track)
+ApplyScrollBarThumb(CooldownViewerSettings.CooldownScroll.ScrollBar.Track.Thumb)
+
+ApplySearchBox(CooldownViewerSettings.SearchBox)
