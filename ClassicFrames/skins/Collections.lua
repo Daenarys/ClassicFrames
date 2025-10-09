b551669dedd871bc24ac6d4dd762bcbd4cdefdbe
Daenarys <androidwoz@gmail.com>
2025-10-09 17:06:38 +0200

    use default stuff

--- ClassicFrames/skins/Collections.lua
+++ ClassicFrames/skins/Collections.lua
@@ -108,22 +108,5 @@ f:SetScript("OnEvent", function(self, event, name)
 		ApplyFilterDropDown(ToyBox.FilterDropdown)
 		ApplyFilterDropDown(HeirloomsJournal.FilterDropdown)
 		ApplyFilterDropDown(WardrobeCollectionFrame.FilterButton)
-
-		hooksecurefunc(WardrobeCollectionFrame, "SetContainer", function(self, parent)
-			if parent == CollectionsJournal then
-				self.ItemsCollectionFrame.ModelR1C1:SetPoint("TOP", -238, -85)
-				self.ItemsCollectionFrame.PagingFrame:SetPoint("BOTTOM", 22, 38)
-				self.ItemsCollectionFrame.SlotsFrame:SetPoint("TOPLEFT", 18, -20)
-				self.ItemsCollectionFrame.WeaponDropdown:SetPoint("TOPRIGHT", -20, -23)
-			end
-		end)
-
-		hooksecurefunc(WardrobeCollectionFrame, "SetTab", function(self)
-			if self.activeFrame == self.ItemsCollectionFrame then
-				self.ClassDropdown:Hide()
-			elseif self.activeFrame == self.SetsCollectionFrame then
-				self.ClassDropdown:Show()
-			end
-		end)
 	end
 end)
