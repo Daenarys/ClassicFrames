709ed64616f6775a04f2eef3f452ababf930fb90
Daenarys <androidwoz@gmail.com>
2025-10-19 20:34:51 +0200

    skin filterdropdowns

--- ClassicFrames/skins/Housing.lua
+++ ClassicFrames/skins/Housing.lua
@@ -90,6 +90,9 @@ d:SetScript("OnEvent", function(self, event, name)
 		HousingHouseSettingsFrame.CloseButton:SetPoint("TOPRIGHT", -4, -4)
 
 		ApplyDropDown(HousingHouseSettingsFrame.HouseOwnerDropdown)
+		ApplyFilterDropDown(HousingHouseSettingsFrame.PlotAccess.AccessTypeDropdown)
+		ApplyFilterDropDown(HousingHouseSettingsFrame.HouseAccess.AccessTypeDropdown)
+
 	end
 end)
 
