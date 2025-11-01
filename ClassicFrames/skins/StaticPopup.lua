b10db634dbe70a12f7961b518bd4e792c16b9b03
Daenarys <androidwoz@gmail.com>
2025-11-02 00:36:36 +0100

    use classic asset

--- ClassicFrames/skins/StaticPopup.lua
+++ ClassicFrames/skins/StaticPopup.lua
@@ -1,10 +1,5 @@
 for i = 1, 4 do
-	if not _G["StaticPopup"..i].BG.SetBackdrop then
-		Mixin(_G["StaticPopup"..i].BG, BackdropTemplateMixin)
-	end
-	_G["StaticPopup"..i].BG:SetBackdrop(BACKDROP_DIALOG_32_32)
-	_G["StaticPopup"..i].BG.Top:SetAlpha(0)
-	_G["StaticPopup"..i].BG.Bottom:SetAlpha(0)
+	_G["StaticPopup"..i].BG.Top:SetAtlas("UI-DiamondDialogBox-ClassicBorder")
 	_G["StaticPopup"..i.."EditBox"].NineSlice:SetAlpha(0)
 
 	if (_G["StaticPopup"..i.."EditBox"].Left == nil) then
