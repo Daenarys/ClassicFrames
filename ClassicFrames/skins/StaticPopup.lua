20e653b2e23528b20b7c7f316e00aa8936626f03
Daenarys <androidwoz@gmail.com>
2025-11-15 17:02:45 +0100

    sync

--- ClassicFrames/skins/StaticPopup.lua
+++ ClassicFrames/skins/StaticPopup.lua
@@ -24,3 +24,16 @@ for i = 1, 4 do
 		_G["StaticPopup"..i.."EditBox"].Mid:SetPoint("TOPRIGHT", _G["StaticPopup"..i.."EditBox"].Right, "TOPLEFT")
 	end
 end
+
+if RPETurnStrafeStyleMixin then
+	hooksecurefunc(RPETurnStrafeStyleMixin, "OnLoad", function(self)
+		self.BG.Top:SetAtlas("UI-DiamondDialogBox-ClassicBorder")
+		self.CloseButton:SetSize(32, 32)
+		self.CloseButton:SetDisabledTexture("Interface\\Buttons\\UI-Panel-MinimizeButton-Disabled")
+		self.CloseButton:SetNormalTexture("Interface\\Buttons\\UI-Panel-MinimizeButton-Up")
+		self.CloseButton:SetPushedTexture("Interface\\Buttons\\UI-Panel-MinimizeButton-Down")
+		self.CloseButton:SetHighlightTexture("Interface\\Buttons\\UI-Panel-MinimizeButton-Highlight")
+		self.CloseButton:ClearAllPoints()
+		self.CloseButton:SetPoint("TOPRIGHT", -4, -4)
+	end)
+end
