8793476b8b939289037bf66d8621d14c1f701682
Daenarys <androidwoz@gmail.com>
2025-11-16 14:05:18 +0100

    dropdown improvements

--- ClassicFrames/skins/Merchant.lua
+++ ClassicFrames/skins/Merchant.lua
@@ -44,6 +44,9 @@ for i = 1, 2 do
 end
 
 ApplyDropDown(MerchantFrame.FilterDropdown)
+
+MerchantFrame.FilterDropdown:SetWidth(147)
+MerchantFrame.FilterDropdown:SetPoint("TOPRIGHT", -17, -29)
 MerchantFrame.FilterDropdown.Text:SetJustifyH("RIGHT")
 MerchantFrame.FilterDropdown.Text:SetPoint("TOPLEFT", 9, -7)
 
