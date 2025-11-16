04db31d13dccc994a80a704488ec4c39b6d92e0e
Daenarys <androidwoz@gmail.com>
2025-11-16 13:11:00 +0100

    skin item preview frame

--- ClassicFrames/skins/Housing.lua
+++ ClassicFrames/skins/Housing.lua
@@ -43,5 +43,14 @@ f:SetScript("OnEvent", function(self, event, name)
 
 		ApplyTitleBg(HouseFinderFrame)
 		ApplyNineSlicePortrait(HouseFinderFrame)
+	elseif name == "Blizzard_HousingModelPreview" then
+		ApplyCloseButton(HousingModelPreviewFrameCloseButton)
+
+		HousingModelPreviewFrame.TitleContainer:ClearAllPoints()
+		HousingModelPreviewFrame.TitleContainer:SetPoint("TOPLEFT", HousingModelPreviewFrame, "TOPLEFT", 58, 0)
+		HousingModelPreviewFrame.TitleContainer:SetPoint("TOPRIGHT", HousingModelPreviewFrame, "TOPRIGHT", -58, 0)
+
+		ApplyTitleBgNoPortrait(HousingModelPreviewFrame)
+		ApplyNineSliceNoPortrait(HousingModelPreviewFrame)
 	end
 end)
