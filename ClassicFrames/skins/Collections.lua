af664aed632fd1d0cc7037e078153182c1a08cfb
Daenarys <androidwoz@gmail.com>
2025-12-06 14:09:08 +0100

    re-enable dropdown

--- ClassicFrames/skins/Collections.lua
+++ ClassicFrames/skins/Collections.lua
@@ -109,11 +109,14 @@ f:SetScript("OnEvent", function(self, event, name)
 		ApplyScrollBarThumb(WardrobeCollectionFrame.SetsCollectionFrame.ListContainer.ScrollBar.Track.Thumb)
 
 		ApplyDropDown(HeirloomsJournal.ClassDropdown)
+		ApplyDropDown(WardrobeCollectionFrame.ClassDropdown)
 		ApplyDropDown(WardrobeCollectionFrame.ItemsCollectionFrame.WeaponDropdown)
 		ApplyDropDown(WardrobeCollectionFrame.SetsCollectionFrame.DetailsFrame.VariantSetsDropdown)
 
 		HeirloomsJournal.ClassDropdown.Text:SetJustifyH("RIGHT")
 		HeirloomsJournal.ClassDropdown.Text:SetPoint("TOPLEFT", 9, -7)
+		WardrobeCollectionFrame.ClassDropdown.Text:SetJustifyH("RIGHT")
+		WardrobeCollectionFrame.ClassDropdown.Text:SetPoint("TOPLEFT", 9, -7)
 		WardrobeCollectionFrame.ItemsCollectionFrame.WeaponDropdown.Text:SetJustifyH("RIGHT")
 		WardrobeCollectionFrame.ItemsCollectionFrame.WeaponDropdown.Text:SetPoint("TOPLEFT", 9, -7)
 
@@ -122,18 +125,5 @@ f:SetScript("OnEvent", function(self, event, name)
 		ApplyFilterDropDown(ToyBox.FilterDropdown)
 		ApplyFilterDropDown(HeirloomsJournal.FilterDropdown)
 		ApplyFilterDropDown(WardrobeCollectionFrame.FilterButton)
-
-		hooksecurefunc(WardrobeCollectionFrame, "SetContainer", function(self, parent)
-			if parent == CollectionsJournal then
-				self.ItemsCollectionFrame.ModelR1C1:SetPoint("TOP", -238, -85)
-				self.ItemsCollectionFrame.PagingFrame:SetPoint("BOTTOM", 22, 38)
-				self.ItemsCollectionFrame.SlotsFrame:SetPoint("TOPLEFT", 18, -20)
-				self.ItemsCollectionFrame.WeaponDropdown:SetPoint("TOPRIGHT", -20, -23)
-			end
-		end)
-
-		hooksecurefunc(WardrobeCollectionFrame, "SetTab", function(self)
-			self.ClassDropdown:Hide()
-		end)
 	end
 end)
