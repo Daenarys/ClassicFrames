35bc699bb5a3bad7fd505b76e30306d41f68c5bf
Daenarys <androidwoz@gmail.com>
2026-02-18 19:30:16 +0100

    hide cd numbers

--- ClassicFrames/skins/Bags.lua
+++ ClassicFrames/skins/Bags.lua
@@ -104,6 +104,7 @@ for i = 1, _G.NUM_CONTAINER_FRAMES do
 
 	hooksecurefunc(_G['ContainerFrame'..i], "UpdateItemLayout", function(self)
 		for i, itemButton in self:EnumerateValidItems() do
+			itemButton.Cooldown:SetHideCountdownNumbers(true)
 			itemButton.emptyBackgroundAtlas = nil
 		end
 	end)
