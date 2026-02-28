b6cbe5f9b364b80f39329fa17f2a1394a65b4e08
Daenarys <androidwoz@gmail.com>
2026-02-28 20:52:01 +0100

    move quest title text

--- ClassicFrames/skins/Quests.lua
+++ ClassicFrames/skins/Quests.lua
@@ -6,8 +6,8 @@ if _G.QuestFrame then
 	QuestFramePortrait:SetPoint("TOPLEFT", -6, 8)
 
 	QuestFrame.TitleContainer:ClearAllPoints()
-	QuestFrame.TitleContainer:SetPoint("TOPLEFT", QuestFrame, "TOPLEFT", 60, -1)
-	QuestFrame.TitleContainer:SetPoint("TOPRIGHT", QuestFrame, "TOPRIGHT", -60, 1)
+	QuestFrame.TitleContainer:SetPoint("TOPLEFT", QuestFrame, "TOPLEFT", 58, -1)
+	QuestFrame.TitleContainer:SetPoint("TOPRIGHT", QuestFrame, "TOPRIGHT", -34, 1)
 
 	QuestFrameTitleText:SetTextColor(255, 255, 255, 1)
 
