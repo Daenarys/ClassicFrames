cfa4738d7f3ed6c32199f1596d7c9c7e9291a850
Daenarys <androidwoz@gmail.com>
2026-03-04 12:36:45 +0100

    remove this

--- ClassicFrames_Minimap/skins/Minimap.lua
+++ ClassicFrames_Minimap/skins/Minimap.lua
@@ -1,12 +1,5 @@
 if not _G.MinimapCluster then return end
 
-local ldbi = LibStub ~= nil and LibStub:GetLibrary("LibDBIcon-1.0")
-if (ldbi ~= nil) then
-	for _, v in pairs(ldbi:GetButtonList()) do
-		ldbi:Refresh(v)
-	end
-end
-
 MinimapCluster:SetScale(1)
 MinimapCluster:SetSize(192, 192)
 MinimapCluster.BorderTop:Hide()
