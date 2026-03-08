bbc85a20de5905e72ad6de5678b586fa17b605d5
Daenarys <androidwoz@gmail.com>
2026-03-08 11:09:34 +0100

    use border template for these

--- ClassicFrames/skins/RPE.lua
+++ ClassicFrames/skins/RPE.lua
@@ -1,6 +1,11 @@
 if RPETurnStrafeStyleMixin then
 	hooksecurefunc(RPETurnStrafeStyleMixin, "OnLoad", function(self)
-		self.BG.Top:SetAtlas("UI-DiamondDialogBox-ClassicBorder")
+		self.BG:SetAlpha(0)
+
+		local border = CreateFrame("Frame", nil, self, "DialogBorderDarkTemplate")
+		ApplyDialogBorder(border)
+		self.Border = border
+
 		self.CloseButton:SetSize(32, 32)
 		self.CloseButton:SetDisabledTexture("Interface\\Buttons\\UI-Panel-MinimizeButton-Disabled")
 		self.CloseButton:SetNormalTexture("Interface\\Buttons\\UI-Panel-MinimizeButton-Up")
