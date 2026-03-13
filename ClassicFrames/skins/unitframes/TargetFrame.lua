cfc3141a313ac01277a7e323d461640ccceb91c0
Daenarys <androidwoz@gmail.com>
2026-03-13 13:42:44 +0100

    remove glow for now

--- ClassicFrames/skins/unitframes/TargetFrame.lua
+++ ClassicFrames/skins/unitframes/TargetFrame.lua
@@ -160,12 +160,6 @@ local function SkinFrame(frame)
 	FrameHealthBarContainer.DeadText:SetParent(frame.TargetFrameContainer)
 	FrameHealthBarContainer.UnconsciousText:SetParent(frame.TargetFrameContainer)
 
-	FrameHealthBar.OverAbsorbGlow:SetParent(contextual)
-	FrameHealthBar.OverAbsorbGlow:RemoveMaskTexture(FrameHealthBarContainer.HealthBarMask)
-	FrameHealthBar.OverAbsorbGlow:ClearAllPoints()
-	FrameHealthBar.OverAbsorbGlow:SetPoint("TOPLEFT", FrameHealthBar, "TOPRIGHT", -10, -5)
-	FrameHealthBar.OverAbsorbGlow:SetPoint("BOTTOMLEFT", FrameHealthBar, "BOTTOMRIGHT", -10, 3)
-
 	FrameManaBar:SetAlpha(0)
 	FrameManaBar.TextString:SetParent(frame.TargetFrameContainer)
 	FrameManaBar.RightText:SetParent(frame.TargetFrameContainer)
