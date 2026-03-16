869f0a3a34024b4fbf845e7f3364d95fbf012aca
Daenarys <androidwoz@gmail.com>
2026-03-16 19:42:29 +0100

    adjust bg pos

--- ClassicFrames/skins/Character.lua
+++ ClassicFrames/skins/Character.lua
@@ -134,7 +134,7 @@ hooksecurefunc(ReputationEntryMixin, 'Initialize', function(self)
 			self.Content.Background:SetTexture("Interface\\PaperDollInfoFrame\\UI-Character-ReputationBar")
 			self.Content.Background:SetTexCoord(0.0, 0.7578125, 0.0, 0.328125)
 			self.Content.Background:SetPoint("TOPRIGHT", self.Content.ReputationBar.LeftTexture, "TOPLEFT")
-			self.Content.Background:SetPoint("LEFT", 10, 0)
+			self.Content.Background:SetPoint("LEFT", 15, 0)
 		end
 
 		if self.ToggleCollapseButton then
