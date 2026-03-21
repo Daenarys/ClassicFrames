65d96b84cd9861ec61ec9dd354fa8cc577d49434
Daenarys <androidwoz@gmail.com>
2026-03-21 13:35:25 +0100

    cleanup

--- ClassicFrames/skins/Menu.lua
+++ ClassicFrames/skins/Menu.lua
@@ -1,41 +1,22 @@
-local TargetMenus = {
+local DialogBorderMenus = {
     ["LFDQueueFrameTypeDropdown"] = true,
     ["RaidFinderQueueFrameSelectionDropdown"] = true,
+    ["LFGListEntryCreationGroupDropdown"] = true,
+    ["LFGListEntryCreationActivityDropdown"] = true,
+    ["LFGListEntryCreationPlayStyleDropdown"] = true,
+    ["HonorFrameTypeDropdown"] = true,
+    ["TrainingGroundsFrameTypeDropdown"] = true,
 }
 
 local DialogSkin = CreateFrame("Frame", nil, UIParent, "BackdropTemplate")
-DialogSkin:SetBackdrop({
-    bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background-Dark",
-    edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
-    tileSize = 32,
-    edgeSize = 32,
-    insets = { left = 11, right = 12, top = 12, bottom = 9 }
-})
+DialogSkin:SetBackdrop(BACKDROP_DARK_DIALOG_32_32)
 DialogSkin:Hide()
 
-local TooltipSkin = CreateFrame("Frame", nil, UIParent, "BackdropTemplate")
-TooltipSkin:SetBackdrop({
-    bgFile = "Interface\\Tooltips\\UI-Tooltip-Background",
-    edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
-    tileSize = 16,
-    edgeSize = 16,
-    insets = { left = 5, right = 5, top = 5, bottom = 4 }
-})
-TooltipSkin:SetBackdropBorderColor(TOOLTIP_DEFAULT_COLOR.r, TOOLTIP_DEFAULT_COLOR.g, TOOLTIP_DEFAULT_COLOR.b);
-TooltipSkin:SetBackdropColor(TOOLTIP_DEFAULT_BACKGROUND_COLOR.r, TOOLTIP_DEFAULT_BACKGROUND_COLOR.g, TOOLTIP_DEFAULT_BACKGROUND_COLOR.b)
+local TooltipSkin = CreateFrame("Frame", nil, UIParent, "TooltipBackdropTemplate")
 TooltipSkin:Hide()
 
-local SubTooltipSkin = CreateFrame("Frame", nil, UIParent, "BackdropTemplate")
-SubTooltipSkin:SetBackdrop({
-    bgFile = "Interface\\Tooltips\\UI-Tooltip-Background",
-    edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
-    tileSize = 16,
-    edgeSize = 16,
-    insets = { left = 5, right = 5, top = 5, bottom = 4 }
-})
-SubTooltipSkin:SetBackdropBorderColor(TOOLTIP_DEFAULT_COLOR.r, TOOLTIP_DEFAULT_COLOR.g, TOOLTIP_DEFAULT_COLOR.b);
-SubTooltipSkin:SetBackdropColor(TOOLTIP_DEFAULT_BACKGROUND_COLOR.r, TOOLTIP_DEFAULT_BACKGROUND_COLOR.g, TOOLTIP_DEFAULT_BACKGROUND_COLOR.b)
-SubTooltipSkin:Hide()
+local SubMenuSkin = CreateFrame("Frame", nil, UIParent, "TooltipBackdropTemplate")
+SubMenuSkin:Hide()
 
 local function SkinMenu(manager, owner, menuDescription)
     local menu = manager:GetOpenMenu()
@@ -43,40 +24,33 @@ local function SkinMenu(manager, owner, menuDescription)
 
     select(1, menu:GetRegions()):SetAlpha(0)
 
-    local activeSkin
-    local ownerName = owner and owner.GetName and owner:GetName()
-    if ownerName and TargetMenus[ownerName] then
-        activeSkin = DialogSkin
+    local ownerName = owner and owner:GetName()
+    if ownerName and DialogBorderMenus[ownerName] then
         TooltipSkin:Hide()
-        SubTooltipSkin:Hide()
+        DialogSkin:SetParent(menu)
+        DialogSkin:ClearAllPoints()
+        DialogSkin:SetPoint("TOPLEFT", menu, "TOPLEFT", -8, 3)
+        DialogSkin:SetPoint("BOTTOMRIGHT", menu, "BOTTOMRIGHT", 8, 0)
+        DialogSkin:SetFrameLevel(menu:GetFrameLevel())
+        DialogSkin:Show()
     else
-        activeSkin = TooltipSkin
         DialogSkin:Hide()
-        SubTooltipSkin:Hide()
+        TooltipSkin:SetParent(menu)
+        TooltipSkin:ClearAllPoints()
+        TooltipSkin:SetPoint("TOPLEFT", menu, "TOPLEFT", -4, 3)
+        TooltipSkin:SetPoint("BOTTOMRIGHT", menu, "BOTTOMRIGHT", 1, 4)
+        TooltipSkin:SetFrameLevel(menu:GetFrameLevel())
+        TooltipSkin:Show()
     end
-
-    activeSkin:SetParent(menu)
-    activeSkin:ClearAllPoints()
-    
-    if activeSkin == DialogSkin then
-        activeSkin:SetPoint("TOPLEFT", menu, "TOPLEFT", -8, 3)
-        activeSkin:SetPoint("BOTTOMRIGHT", menu, "BOTTOMRIGHT", 8, 0)
-    else
-        activeSkin:SetPoint("TOPLEFT", menu, "TOPLEFT", -4, 1)
-        activeSkin:SetPoint("BOTTOMRIGHT", menu, "BOTTOMRIGHT", 0, 3)
-    end
-    
-    activeSkin:SetFrameLevel(menu:GetFrameLevel())
-    activeSkin:Show()
 
     menuDescription:AddMenuAcquiredCallback(function(self)
         select(1, self:GetRegions()):SetAlpha(0)
-        SubTooltipSkin:SetParent(self)
-        SubTooltipSkin:ClearAllPoints()
-        SubTooltipSkin:SetPoint("TOPLEFT", self, "TOPLEFT")
-        SubTooltipSkin:SetPoint("BOTTOMRIGHT", self, "BOTTOMRIGHT", 0, 4)
-        SubTooltipSkin:SetFrameLevel(self:GetFrameLevel())
-        SubTooltipSkin:Show()
+        SubMenuSkin:SetParent(self)
+        SubMenuSkin:ClearAllPoints()
+        SubMenuSkin:SetPoint("TOPLEFT", self, "TOPLEFT", 0, 3)
+        SubMenuSkin:SetPoint("BOTTOMRIGHT", self, "BOTTOMRIGHT", 0, 4)
+        SubMenuSkin:SetFrameLevel(self:GetFrameLevel())
+        SubMenuSkin:Show()
     end)
 end
 
