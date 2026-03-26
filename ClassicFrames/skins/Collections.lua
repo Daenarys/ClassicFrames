6c74bb10fa91a6aec4f9666d51bbd5aa75f0eccf
Daenarys <androidwoz@gmail.com>
2026-03-26 21:51:11 +0100

    cleanup

--- ClassicFrames/skins/Collections.lua
+++ ClassicFrames/skins/Collections.lua
@@ -107,14 +107,12 @@ f:SetScript("OnEvent", function(self, event, name)
 		ApplyScrollBarThumb(WardrobeCollectionFrame.SetsCollectionFrame.ListContainer.ScrollBar.Track.Thumb)
 
 		ApplyDropDown(HeirloomsJournal.ClassDropdown)
-		ApplyDropDown(WardrobeCollectionFrame.ClassDropdown)
 		ApplyDropDown(WardrobeCollectionFrame.ItemsCollectionFrame.WeaponDropdown)
 
 		HeirloomsJournal.ClassDropdown:SetWidth(155)
 		HeirloomsJournal.ClassDropdown:SetPoint("TOPLEFT", 72, -31)
 		HeirloomsJournal.ClassDropdown.Text:SetJustifyH("RIGHT")
 		HeirloomsJournal.ClassDropdown.Text:SetPoint("TOPLEFT", 9, -8)
-		WardrobeCollectionFrame.ClassDropdown.Text:SetJustifyH("RIGHT")
 		WardrobeCollectionFrame.ItemsCollectionFrame.WeaponDropdown:SetWidth(155)
 		WardrobeCollectionFrame.ItemsCollectionFrame.WeaponDropdown.Text:SetJustifyH("RIGHT")
 
@@ -133,13 +131,34 @@ f:SetScript("OnEvent", function(self, event, name)
 
 		hooksecurefunc(WardrobeCollectionFrame, "SetTab", function(self, tabID)
 			if tabID == WARDROBE_TAB_ITEMS then
-				self.ClassDropdown:Hide()
 				self.ItemsCollectionFrame.ModelR1C1:SetPoint("TOP", -238, -85)
 				self.ItemsCollectionFrame.PagingFrame:SetPoint("BOTTOM", 22, 38)
 				self.ItemsCollectionFrame.SlotsFrame:SetPoint("TOPLEFT", 18, -20)
+				self.ItemsCollectionFrame.WeaponDropdown:SetEnabled(false)
 				self.ItemsCollectionFrame.WeaponDropdown:SetPoint("TOPRIGHT", -23, -23)
-			elseif tabID == WARDROBE_TAB_SETS then
-				self.ClassDropdown:Show()
+			end
+			self.ClassDropdown:Hide()
+		end)
+
+		hooksecurefunc(WardrobeCollectionFrame.ItemsCollectionFrame, "SetActiveCategory", function(self)
+			local _name, isActiveCategoryWeapon;
+			if self.transmogLocation:IsAppearance() then
+				_name, isActiveCategoryWeapon = C_TransmogCollection.GetCategoryInfo(self:GetActiveCategory())
+			end
+
+			self.WeaponDropdown:Show()
+
+			if not isActiveCategoryWeapon then
+				self.WeaponDropdown:SetEnabled(false)
+			else
+				self.WeaponDropdown:SetEnabled(true)
+			end
+		end)
+
+		hooksecurefunc("CollectionsJournal_UpdateSelectedTab", function(self)
+			local selected = CollectionsJournal_GetTab(self)
+			if selected == 5 then
+				WardrobeCollectionFrame.ItemsCollectionFrame.WeaponDropdown:SetEnabled(false)
 			end
 		end)
 
