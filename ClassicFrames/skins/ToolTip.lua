89a93468138f2ee9d8efb2534d6eb0a6476fa906
Daenarys <androidwoz@gmail.com>
2026-04-03 10:40:51 +0200

    cleanup

--- ClassicFrames/skins/ToolTip.lua
+++ ClassicFrames/skins/ToolTip.lua
@@ -1,17 +1,17 @@
 if not _G.TooltipDataProcessor then return end
 
-TooltipDataProcessor.AddTooltipPreCall(Enum.TooltipDataType.Item, function(tooltip, data)
-	if tooltip == ShoppingTooltip1 or tooltip == ShoppingTooltip2 then
-		local isPrimaryTooltip = (tooltip == ShoppingTooltip1)
+TooltipDataProcessor.AddTooltipPreCall(Enum.TooltipDataType.Item, function(self)
+	if self == ShoppingTooltip1 or self == ShoppingTooltip2 then
+		local isPrimaryTooltip = (self == ShoppingTooltip1)
 		local isPairedItem = (ShoppingTooltip2 and ShoppingTooltip2:IsShown())
 
 		local header = CURRENTLY_EQUIPPED
 		if not isPrimaryTooltip and isPairedItem then
 			header = IF_EQUIPPED_TOGETHER
 		end
-		GameTooltip_AddDisabledLine(tooltip, header, false)
+		GameTooltip_AddDisabledLine(self, header, false)
 
-		local line = _G[tooltip:GetName().."TextLeft"..tooltip:NumLines()]
+		local line = _G[self:GetName().."TextLeft"..self:NumLines()]
 		if line then
 			line:SetFontObject(GameFontDisableSmall)
 		end
