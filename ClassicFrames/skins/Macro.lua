5af74c923fe5b617d0dcc3eba04bd41c701f490b
Daenarys <androidwoz@gmail.com>
2026-04-13 00:27:26 +0200

    cleanup

--- ClassicFrames/skins/Macro.lua
+++ ClassicFrames/skins/Macro.lua
@@ -39,23 +39,12 @@ f:SetScript("OnEvent", function(self, event, name)
 		MacroFrameScrollFrame.ScrollBar:SetPoint("TOPLEFT", MacroFrameScrollFrame, "TOPRIGHT", 8, 0)
 		MacroFrameScrollFrame.ScrollBar:SetPoint("BOTTOMLEFT", MacroFrameScrollFrame, "BOTTOMRIGHT", 8, 0)
 
-		ApplyScrollBarThumb(MacroFrameScrollFrame.ScrollBar.Track.Thumb)
-
 		MacroFrameScrollFrame.ScrollBar.Track.Begin:Hide()
 		MacroFrameScrollFrame.ScrollBar.Track.End:Hide()
 		MacroFrameScrollFrame.ScrollBar.Track.Middle:Hide()
 
-		MacroFrameScrollFrame.ScrollBar.Back:SetSize(18, 16)
-		MacroFrameScrollFrame.ScrollBar.Back:SetNormalAtlas("UI-ScrollBar-ScrollUpButton-Up")
-		MacroFrameScrollFrame.ScrollBar.Back:SetPushedAtlas("UI-ScrollBar-ScrollUpButton-Down")
-		MacroFrameScrollFrame.ScrollBar.Back:SetDisabledAtlas("UI-ScrollBar-ScrollUpButton-Disabled")
-		MacroFrameScrollFrame.ScrollBar.Back:SetHighlightAtlas("UI-ScrollBar-ScrollUpButton-Highlight")
-
-		MacroFrameScrollFrame.ScrollBar.Forward:SetSize(18, 16)
-		MacroFrameScrollFrame.ScrollBar.Forward:SetNormalAtlas("UI-ScrollBar-ScrollDownButton-Up")
-		MacroFrameScrollFrame.ScrollBar.Forward:SetPushedAtlas("UI-ScrollBar-ScrollDownButton-Down")
-		MacroFrameScrollFrame.ScrollBar.Forward:SetDisabledAtlas("UI-ScrollBar-ScrollDownButton-Disabled")
-		MacroFrameScrollFrame.ScrollBar.Forward:SetHighlightAtlas("UI-ScrollBar-ScrollDownButton-Highlight")
+		ApplyScrollBarArrow(MacroFrameScrollFrame.ScrollBar, true)
+		ApplyScrollBarThumb(MacroFrameScrollFrame.ScrollBar.Track.Thumb)
 
 		MacroPopupFrame.IconSelector.ScrollBar:SetSize(25, 560)
 		MacroPopupFrame.IconSelector.ScrollBar:ClearAllPoints()
@@ -67,5 +56,6 @@ f:SetScript("OnEvent", function(self, event, name)
 		ApplyScrollBarThumb(MacroPopupFrame.IconSelector.ScrollBar.Track.Thumb)
 
 		ApplyDropDown(MacroPopupFrame.BorderBox.IconTypeDropdown)
+		MacroPopupFrame.BorderBox.IconTypeDropdown.Text:SetJustifyH("RIGHT")
 	end
 end)
