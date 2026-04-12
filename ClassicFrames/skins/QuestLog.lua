3461d766382e37eb482a51e5de06aa43c7e14247
Daenarys <androidwoz@gmail.com>
2026-04-12 10:29:28 +0200

    questlog improvements

--- ClassicFrames/skins/QuestLog.lua
+++ ClassicFrames/skins/QuestLog.lua
@@ -12,6 +12,39 @@ QuestScrollFrame.Background:SetPoint("BOTTOMRIGHT", 22, 0)
 QuestMapFrame.VerticalSeparator:SetAlpha(1)
 QuestMapFrame.VerticalSeparator:Show()
 
+hooksecurefunc('QuestLogQuests_Update', function()
+	for button in _G.QuestScrollFrame.headerFramePool:EnumerateActive() do
+		if not button.IsSkinned then
+			button:GetNormalTexture():SetAlpha(0)
+			button:GetHighlightTexture():SetAlpha(0)
+			if button.ButtonText then
+				button.ButtonText:ClearAllPoints()
+				button.ButtonText:SetPoint("LEFT", button.CollapseButton, "RIGHT", 5, 0)
+			end
+			if button.CollapseButton then
+				button.CollapseButton:ClearAllPoints()
+				button.CollapseButton:SetPoint("LEFT", -4, 0)
+				hooksecurefunc(button.CollapseButton, "UpdateCollapsedState", function(self, collapsed)
+					local atlas = collapsed and "Campaign_HeaderIcon_Closed" or "Campaign_HeaderIcon_Open"
+					self.Icon:SetSize(20, 20)
+					self.Icon:SetAtlas(atlas)
+					self:SetHighlightTexture("Interface\\Buttons\\UI-PlusButton-Hilight", "ADD")
+				end)
+			end
+			button.IsSkinned = true
+		end
+	end
+
+	for button in _G.QuestScrollFrame.titleFramePool:EnumerateActive() do
+		if not button.IsSkinned then
+			if button.Checkbox then
+				button.Checkbox:Hide()
+			end
+			button.IsSkinned = true
+		end
+	end
+end)
+
 hooksecurefunc(QuestMapFrame, "ValidateTabs", function(self)
 	self.QuestsTab:Hide()
 	self.EventsTab:Hide()
@@ -20,7 +53,7 @@ end)
 
 hooksecurefunc("QuestMapFrame_UpdateQuestSessionState", function(self)
 	if self.QuestSessionManagement:IsShown() then
-		self.ContentsAnchor:SetPoint("BOTTOM", self.QuestSessionManagement, "TOP", 0, 5);
+		self.ContentsAnchor:SetPoint("BOTTOM", self.QuestSessionManagement, "TOP", 0, 5)
 	else
 		self.ContentsAnchor:SetPoint("BOTTOM", self, "BOTTOM")
 	end
