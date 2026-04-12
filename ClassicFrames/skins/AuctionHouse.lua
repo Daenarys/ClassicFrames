db002113d8df5d73692d4dd05bd8027a5729d650
Daenarys <androidwoz@gmail.com>
2026-04-12 12:33:34 +0200

    update ah scrollbars

--- ClassicFrames/skins/AuctionHouse.lua
+++ ClassicFrames/skins/AuctionHouse.lua
@@ -56,79 +56,39 @@ f:SetScript("OnEvent", function(self, event, name)
 		ApplyScrollBarTrack(AuctionHouseFrame.CategoriesList.ScrollBar.Track)
 		ApplyScrollBarThumb(AuctionHouseFrame.CategoriesList.ScrollBar.Track.Thumb)
 
-		AuctionHouseFrame.BrowseResultsFrame.ItemList.ScrollBar:SetSize(25, 560)
 		AuctionHouseFrame.BrowseResultsFrame.ItemList.ScrollBar:ClearAllPoints()
-		AuctionHouseFrame.BrowseResultsFrame.ItemList.ScrollBar:SetPoint("TOPLEFT", AuctionHouseFrame.BrowseResultsFrame.ItemList.ScrollBox, "TOPRIGHT", 2, 3)
-		AuctionHouseFrame.BrowseResultsFrame.ItemList.ScrollBar:SetPoint("BOTTOMLEFT", AuctionHouseFrame.BrowseResultsFrame.ItemList.ScrollBox, "BOTTOMRIGHT", 5, -3)
+		AuctionHouseFrame.BrowseResultsFrame.ItemList.ScrollBar:SetPoint("TOPLEFT", AuctionHouseFrame.BrowseResultsFrame.ItemList.ScrollBox, "TOPRIGHT", 10, 4)
+		AuctionHouseFrame.BrowseResultsFrame.ItemList.ScrollBar:SetPoint("BOTTOMLEFT", AuctionHouseFrame.BrowseResultsFrame.ItemList.ScrollBox, "BOTTOMRIGHT", 10, -3)
 
-		if (AuctionHouseFrame.BrowseResultsFrame.ItemList.ScrollBar.BG == nil) then
-			AuctionHouseFrame.BrowseResultsFrame.ItemList.ScrollBar.BG = AuctionHouseFrame.BrowseResultsFrame.ItemList.ScrollBar:CreateTexture(nil, "BACKGROUND")
-			AuctionHouseFrame.BrowseResultsFrame.ItemList.ScrollBar.BG:SetColorTexture(0, 0, 0, .85)
-			AuctionHouseFrame.BrowseResultsFrame.ItemList.ScrollBar.BG:SetAllPoints()
-		end
-
-		ApplyScrollBarArrow(AuctionHouseFrame.BrowseResultsFrame.ItemList.ScrollBar)
-		ApplyScrollBarTrack(AuctionHouseFrame.BrowseResultsFrame.ItemList.ScrollBar.Track)
+		ApplyScrollBarHybrid(AuctionHouseFrame.BrowseResultsFrame.ItemList.ScrollBar)
 		ApplyScrollBarThumb(AuctionHouseFrame.BrowseResultsFrame.ItemList.ScrollBar.Track.Thumb)
 
-		AuctionHouseFrame.CommoditiesBuyFrame.ItemList.ScrollBar:SetSize(25, 560)
 		AuctionHouseFrame.CommoditiesBuyFrame.ItemList.ScrollBar:ClearAllPoints()
-		AuctionHouseFrame.CommoditiesBuyFrame.ItemList.ScrollBar:SetPoint("TOPLEFT", AuctionHouseFrame.CommoditiesBuyFrame.ItemList.ScrollBox, "TOPRIGHT", 2, 3)
-		AuctionHouseFrame.CommoditiesBuyFrame.ItemList.ScrollBar:SetPoint("BOTTOMLEFT", AuctionHouseFrame.CommoditiesBuyFrame.ItemList.ScrollBox, "BOTTOMRIGHT", 5, -3)
+		AuctionHouseFrame.CommoditiesBuyFrame.ItemList.ScrollBar:SetPoint("TOPLEFT", AuctionHouseFrame.CommoditiesBuyFrame.ItemList.ScrollBox, "TOPRIGHT", 10, 4)
+		AuctionHouseFrame.CommoditiesBuyFrame.ItemList.ScrollBar:SetPoint("BOTTOMLEFT", AuctionHouseFrame.CommoditiesBuyFrame.ItemList.ScrollBox, "BOTTOMRIGHT", 10, -3)
 
-		if (AuctionHouseFrame.CommoditiesBuyFrame.ItemList.ScrollBar.BG == nil) then
-			AuctionHouseFrame.CommoditiesBuyFrame.ItemList.ScrollBar.BG = AuctionHouseFrame.CommoditiesBuyFrame.ItemList.ScrollBar:CreateTexture(nil, "BACKGROUND")
-			AuctionHouseFrame.CommoditiesBuyFrame.ItemList.ScrollBar.BG:SetColorTexture(0, 0, 0, .85)
-			AuctionHouseFrame.CommoditiesBuyFrame.ItemList.ScrollBar.BG:SetAllPoints()
-		end
-
-		ApplyScrollBarArrow(AuctionHouseFrame.CommoditiesBuyFrame.ItemList.ScrollBar)
-		ApplyScrollBarTrack(AuctionHouseFrame.CommoditiesBuyFrame.ItemList.ScrollBar.Track)
+		ApplyScrollBarHybrid(AuctionHouseFrame.CommoditiesBuyFrame.ItemList.ScrollBar)
 		ApplyScrollBarThumb(AuctionHouseFrame.CommoditiesBuyFrame.ItemList.ScrollBar.Track.Thumb)
 
-		AuctionHouseFrame.ItemBuyFrame.ItemList.ScrollBar:SetSize(25, 560)
 		AuctionHouseFrame.ItemBuyFrame.ItemList.ScrollBar:ClearAllPoints()
-		AuctionHouseFrame.ItemBuyFrame.ItemList.ScrollBar:SetPoint("TOPLEFT", AuctionHouseFrame.ItemBuyFrame.ItemList.ScrollBox, "TOPRIGHT", 2, 3)
-		AuctionHouseFrame.ItemBuyFrame.ItemList.ScrollBar:SetPoint("BOTTOMLEFT", AuctionHouseFrame.ItemBuyFrame.ItemList.ScrollBox, "BOTTOMRIGHT", 5, -3)
+		AuctionHouseFrame.ItemBuyFrame.ItemList.ScrollBar:SetPoint("TOPLEFT", AuctionHouseFrame.ItemBuyFrame.ItemList.ScrollBox, "TOPRIGHT", 10, 4)
+		AuctionHouseFrame.ItemBuyFrame.ItemList.ScrollBar:SetPoint("BOTTOMLEFT", AuctionHouseFrame.ItemBuyFrame.ItemList.ScrollBox, "BOTTOMRIGHT", 10, -3)
 
-		if (AuctionHouseFrame.ItemBuyFrame.ItemList.ScrollBar.BG == nil) then
-			AuctionHouseFrame.ItemBuyFrame.ItemList.ScrollBar.BG = AuctionHouseFrame.ItemBuyFrame.ItemList.ScrollBar:CreateTexture(nil, "BACKGROUND")
-			AuctionHouseFrame.ItemBuyFrame.ItemList.ScrollBar.BG:SetColorTexture(0, 0, 0, .85)
-			AuctionHouseFrame.ItemBuyFrame.ItemList.ScrollBar.BG:SetAllPoints()
-		end
-
-		ApplyScrollBarArrow(AuctionHouseFrame.ItemBuyFrame.ItemList.ScrollBar)
-		ApplyScrollBarTrack(AuctionHouseFrame.ItemBuyFrame.ItemList.ScrollBar.Track)
+		ApplyScrollBarHybrid(AuctionHouseFrame.ItemBuyFrame.ItemList.ScrollBar)
 		ApplyScrollBarThumb(AuctionHouseFrame.ItemBuyFrame.ItemList.ScrollBar.Track.Thumb)
 
-		AuctionHouseFrame.CommoditiesSellList.ScrollBar:SetSize(25, 560)
 		AuctionHouseFrame.CommoditiesSellList.ScrollBar:ClearAllPoints()
-		AuctionHouseFrame.CommoditiesSellList.ScrollBar:SetPoint("TOPLEFT", AuctionHouseFrame.CommoditiesSellList.ScrollBox, "TOPRIGHT", 2, 3)
-		AuctionHouseFrame.CommoditiesSellList.ScrollBar:SetPoint("BOTTOMLEFT", AuctionHouseFrame.CommoditiesSellList.ScrollBox, "BOTTOMRIGHT", 5, -3)
+		AuctionHouseFrame.CommoditiesSellList.ScrollBar:SetPoint("TOPLEFT", AuctionHouseFrame.CommoditiesSellList.ScrollBox, "TOPRIGHT", 10, 4)
+		AuctionHouseFrame.CommoditiesSellList.ScrollBar:SetPoint("BOTTOMLEFT", AuctionHouseFrame.CommoditiesSellList.ScrollBox, "BOTTOMRIGHT", 10, -3)
 
-		if (AuctionHouseFrame.CommoditiesSellList.ScrollBar.BG == nil) then
-			AuctionHouseFrame.CommoditiesSellList.ScrollBar.BG = AuctionHouseFrame.CommoditiesSellList.ScrollBar:CreateTexture(nil, "BACKGROUND")
-			AuctionHouseFrame.CommoditiesSellList.ScrollBar.BG:SetColorTexture(0, 0, 0, .85)
-			AuctionHouseFrame.CommoditiesSellList.ScrollBar.BG:SetAllPoints()
-		end
-
-		ApplyScrollBarArrow(AuctionHouseFrame.CommoditiesSellList.ScrollBar)
-		ApplyScrollBarTrack(AuctionHouseFrame.CommoditiesSellList.ScrollBar.Track)
+		ApplyScrollBarHybrid(AuctionHouseFrame.CommoditiesSellList.ScrollBar)
 		ApplyScrollBarThumb(AuctionHouseFrame.CommoditiesSellList.ScrollBar.Track.Thumb)
 
-		AuctionHouseFrame.ItemSellList.ScrollBar:SetSize(25, 560)
 		AuctionHouseFrame.ItemSellList.ScrollBar:ClearAllPoints()
-		AuctionHouseFrame.ItemSellList.ScrollBar:SetPoint("TOPLEFT", AuctionHouseFrame.ItemSellList.ScrollBox, "TOPRIGHT", 2, 3)
-		AuctionHouseFrame.ItemSellList.ScrollBar:SetPoint("BOTTOMLEFT", AuctionHouseFrame.ItemSellList.ScrollBox, "BOTTOMRIGHT", 5, -3)
+		AuctionHouseFrame.ItemSellList.ScrollBar:SetPoint("TOPLEFT", AuctionHouseFrame.ItemSellList.ScrollBox, "TOPRIGHT", 10, 4)
+		AuctionHouseFrame.ItemSellList.ScrollBar:SetPoint("BOTTOMLEFT", AuctionHouseFrame.ItemSellList.ScrollBox, "BOTTOMRIGHT", 10, -3)
 
-		if (AuctionHouseFrame.ItemSellList.ScrollBar.BG == nil) then
-			AuctionHouseFrame.ItemSellList.ScrollBar.BG = AuctionHouseFrame.ItemSellList.ScrollBar:CreateTexture(nil, "BACKGROUND")
-			AuctionHouseFrame.ItemSellList.ScrollBar.BG:SetColorTexture(0, 0, 0, .85)
-			AuctionHouseFrame.ItemSellList.ScrollBar.BG:SetAllPoints()
-		end
-
-		ApplyScrollBarArrow(AuctionHouseFrame.ItemSellList.ScrollBar)
-		ApplyScrollBarTrack(AuctionHouseFrame.ItemSellList.ScrollBar.Track)
+		ApplyScrollBarHybrid(AuctionHouseFrame.ItemSellList.ScrollBar)
 		ApplyScrollBarThumb(AuctionHouseFrame.ItemSellList.ScrollBar.Track.Thumb)
 
 		ApplyDialogBorder(AuctionHouseFrame.BuyDialog.Border)
@@ -148,34 +108,18 @@ f:SetScript("OnEvent", function(self, event, name)
 		ApplyScrollBarTrack(AuctionHouseFrameAuctionsFrame.SummaryList.ScrollBar.Track)
 		ApplyScrollBarThumb(AuctionHouseFrameAuctionsFrame.SummaryList.ScrollBar.Track.Thumb)
 
-		AuctionHouseFrameAuctionsFrame.AllAuctionsList.ScrollBar:SetSize(25, 560)
 		AuctionHouseFrameAuctionsFrame.AllAuctionsList.ScrollBar:ClearAllPoints()
-		AuctionHouseFrameAuctionsFrame.AllAuctionsList.ScrollBar:SetPoint("TOPLEFT", AuctionHouseFrameAuctionsFrame.AllAuctionsList.ScrollBox, "TOPRIGHT", 2, 3)
-		AuctionHouseFrameAuctionsFrame.AllAuctionsList.ScrollBar:SetPoint("BOTTOMLEFT", AuctionHouseFrameAuctionsFrame.AllAuctionsList.ScrollBox, "BOTTOMRIGHT", 5, -3)
+		AuctionHouseFrameAuctionsFrame.AllAuctionsList.ScrollBar:SetPoint("TOPLEFT", AuctionHouseFrameAuctionsFrame.AllAuctionsList.ScrollBox, "TOPRIGHT", 10, 4)
+		AuctionHouseFrameAuctionsFrame.AllAuctionsList.ScrollBar:SetPoint("BOTTOMLEFT", AuctionHouseFrameAuctionsFrame.AllAuctionsList.ScrollBox, "BOTTOMRIGHT", 10, -3)
 
-		if (AuctionHouseFrameAuctionsFrame.AllAuctionsList.ScrollBar.BG == nil) then
-			AuctionHouseFrameAuctionsFrame.AllAuctionsList.ScrollBar.BG = AuctionHouseFrameAuctionsFrame.AllAuctionsList.ScrollBar:CreateTexture(nil, "BACKGROUND")
-			AuctionHouseFrameAuctionsFrame.AllAuctionsList.ScrollBar.BG:SetColorTexture(0, 0, 0, .85)
-			AuctionHouseFrameAuctionsFrame.AllAuctionsList.ScrollBar.BG:SetAllPoints()
-		end
-
-		ApplyScrollBarArrow(AuctionHouseFrameAuctionsFrame.AllAuctionsList.ScrollBar)
-		ApplyScrollBarTrack(AuctionHouseFrameAuctionsFrame.AllAuctionsList.ScrollBar.Track)
+		ApplyScrollBarHybrid(AuctionHouseFrameAuctionsFrame.AllAuctionsList.ScrollBar)
 		ApplyScrollBarThumb(AuctionHouseFrameAuctionsFrame.AllAuctionsList.ScrollBar.Track.Thumb)
 
-		AuctionHouseFrameAuctionsFrame.BidsList.ScrollBar:SetSize(25, 560)
 		AuctionHouseFrameAuctionsFrame.BidsList.ScrollBar:ClearAllPoints()
-		AuctionHouseFrameAuctionsFrame.BidsList.ScrollBar:SetPoint("TOPLEFT", AuctionHouseFrameAuctionsFrame.BidsList.ScrollBox, "TOPRIGHT", 2, 3)
-		AuctionHouseFrameAuctionsFrame.BidsList.ScrollBar:SetPoint("BOTTOMLEFT", AuctionHouseFrameAuctionsFrame.BidsList.ScrollBox, "BOTTOMRIGHT", 5, -3)
-
-		if (AuctionHouseFrameAuctionsFrame.BidsList.ScrollBar.BG == nil) then
-			AuctionHouseFrameAuctionsFrame.BidsList.ScrollBar.BG = AuctionHouseFrameAuctionsFrame.BidsList.ScrollBar:CreateTexture(nil, "BACKGROUND")
-			AuctionHouseFrameAuctionsFrame.BidsList.ScrollBar.BG:SetColorTexture(0, 0, 0, .85)
-			AuctionHouseFrameAuctionsFrame.BidsList.ScrollBar.BG:SetAllPoints()
-		end
+		AuctionHouseFrameAuctionsFrame.BidsList.ScrollBar:SetPoint("TOPLEFT", AuctionHouseFrameAuctionsFrame.BidsList.ScrollBox, "TOPRIGHT", 10, 4)
+		AuctionHouseFrameAuctionsFrame.BidsList.ScrollBar:SetPoint("BOTTOMLEFT", AuctionHouseFrameAuctionsFrame.BidsList.ScrollBox, "BOTTOMRIGHT", 10, -3)
 		
-		ApplyScrollBarArrow(AuctionHouseFrameAuctionsFrame.BidsList.ScrollBar)
-		ApplyScrollBarTrack(AuctionHouseFrameAuctionsFrame.BidsList.ScrollBar.Track)
+		ApplyScrollBarHybrid(AuctionHouseFrameAuctionsFrame.BidsList.ScrollBar)
 		ApplyScrollBarThumb(AuctionHouseFrameAuctionsFrame.BidsList.ScrollBar.Track.Thumb)
 
 		ApplyCloseButton(AuctionHouseFrame.WoWTokenResults.GameTimeTutorial.CloseButton)
@@ -184,19 +128,11 @@ f:SetScript("OnEvent", function(self, event, name)
 
 		AuctionHouseFrame.WoWTokenResults.GameTimeTutorial.CloseButton:SetFrameLevel(4)
 
-		AuctionHouseFrame.WoWTokenResults.DummyScrollBar:SetSize(25, 560)
 		AuctionHouseFrame.WoWTokenResults.DummyScrollBar:ClearAllPoints()
-		AuctionHouseFrame.WoWTokenResults.DummyScrollBar:SetPoint("TOPLEFT", AuctionHouseFrameAuctionsFrame.BidsList.ScrollBox, "TOPRIGHT", 2, 3)
-		AuctionHouseFrame.WoWTokenResults.DummyScrollBar:SetPoint("BOTTOMLEFT", AuctionHouseFrameAuctionsFrame.BidsList.ScrollBox, "BOTTOMRIGHT", 5, -3)
-
-		if (AuctionHouseFrame.WoWTokenResults.DummyScrollBar.BG == nil) then
-			AuctionHouseFrame.WoWTokenResults.DummyScrollBar.BG = AuctionHouseFrame.WoWTokenResults.DummyScrollBar:CreateTexture(nil, "BACKGROUND")
-			AuctionHouseFrame.WoWTokenResults.DummyScrollBar.BG:SetColorTexture(0, 0, 0, .85)
-			AuctionHouseFrame.WoWTokenResults.DummyScrollBar.BG:SetAllPoints()
-		end
+		AuctionHouseFrame.WoWTokenResults.DummyScrollBar:SetPoint("TOPLEFT", AuctionHouseFrameAuctionsFrame.BidsList.ScrollBox, "TOPRIGHT", 10, 4)
+		AuctionHouseFrame.WoWTokenResults.DummyScrollBar:SetPoint("BOTTOMLEFT", AuctionHouseFrameAuctionsFrame.BidsList.ScrollBox, "BOTTOMRIGHT", 10, -3)
 		
-		ApplyScrollBarArrow(AuctionHouseFrame.WoWTokenResults.DummyScrollBar)
-		ApplyScrollBarTrack(AuctionHouseFrame.WoWTokenResults.DummyScrollBar.Track)
+		ApplyScrollBarHybrid(AuctionHouseFrame.WoWTokenResults.DummyScrollBar)
 		ApplyScrollBarThumb(AuctionHouseFrame.WoWTokenResults.DummyScrollBar.Track.Thumb)
 
 		ApplyDropDown(AuctionHouseFrame.CommoditiesSellFrame.Duration.Dropdown)
