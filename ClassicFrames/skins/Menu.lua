3d75595f4f66d66acbda96aa86f15303b8dec3ff
Daenarys <androidwoz@gmail.com>
2026-04-20 14:04:30 +0200

    use old tooltip menu skin

--- ClassicFrames/skins/Menu.lua
+++ ClassicFrames/skins/Menu.lua
@@ -1,12 +1,40 @@
-if not _G.CompositorMixin then return end
+local TooltipSkin = CreateFrame("Frame", nil, UIParent, "TooltipBackdropTemplate")
+TooltipSkin:Hide()
 
-hooksecurefunc(CompositorMixin, "AttachFontString", function(parent)
-    if not parent.attachments then return end
+local SubTooltipSkin = CreateFrame("Frame", nil, UIParent, "TooltipBackdropTemplate")
+SubTooltipSkin:Hide()
 
-    for _, widget in ipairs(parent.attachments) do
-        local widgetType = widget:GetObjectType()
-        if widgetType == "FontString" then
-            widget:SetFontObject(GameFontHighlightSmallLeft)
-        end
+local function SkinMenu(manager, owner, menuDescription)
+    local menu = manager:GetOpenMenu()
+    if not menu then return end
+
+    if select(1, menu:GetRegions()):GetAtlas() == "common-dropdown-c-bg" then
+        TooltipSkin:Hide()
+        SubTooltipSkin:Hide()
+        return
     end
-end)
+
+    select(1, menu:GetRegions()):SetAlpha(0)
+    TooltipSkin:SetParent(menu)
+    TooltipSkin:ClearAllPoints()
+    TooltipSkin:SetPoint("TOPLEFT", menu, "TOPLEFT", -3, -1)
+    TooltipSkin:SetPoint("BOTTOMRIGHT", menu, "BOTTOMRIGHT", 3, 7)
+    TooltipSkin:SetFrameLevel(menu:GetFrameLevel())
+    TooltipSkin:Show()
+
+    menuDescription:AddMenuAcquiredCallback(function(self)
+        select(1, self:GetRegions()):SetAlpha(0)
+        SubTooltipSkin:SetParent(self)
+        SubTooltipSkin:ClearAllPoints()
+        SubTooltipSkin:SetPoint("TOPLEFT", self, "TOPLEFT", -3, -1)
+        SubTooltipSkin:SetPoint("BOTTOMRIGHT", self, "BOTTOMRIGHT", 3, 7)
+        SubTooltipSkin:SetFrameLevel(self:GetFrameLevel())
+        SubTooltipSkin:Show()
+    end)
+end
+
+local manager = _G.Menu.GetManager()
+if manager then
+    hooksecurefunc(manager, "OpenMenu", SkinMenu)
+    hooksecurefunc(manager, "OpenContextMenu", SkinMenu)
+end
