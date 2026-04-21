e7585fd7d812a9d4cc2c486e25e9062a0c41f751
Daenarys <androidwoz@gmail.com>
2026-04-21 11:14:50 +0200

    cleanup

--- ClassicFrames/skins/SettingsPanel.lua
+++ ClassicFrames/skins/SettingsPanel.lua
@@ -4,19 +4,3 @@ ApplyCloseButton(SettingsPanel.ClosePanelButton)
 ApplyTitleBg(SettingsPanel, true)
 ApplyNineSliceNoPortrait(SettingsPanel)
 SettingsPanel.NineSlice:SetFrameStrata("HIGH")
-
-SettingsPanel.CloseButton:Hide()
-
-SettingsPanel.Container.SettingsList.ScrollBar.Track.Begin:Hide()
-SettingsPanel.Container.SettingsList.ScrollBar.Track.End:Hide()
-SettingsPanel.Container.SettingsList.ScrollBar.Track.Middle:Hide()
-
-ApplyScrollBarArrow(SettingsPanel.Container.SettingsList.ScrollBar, true)
-ApplyScrollBarThumb(SettingsPanel.Container.SettingsList.ScrollBar.Track.Thumb)
-
-SettingsPanel.CategoryList.ScrollBar.Track.Begin:Hide()
-SettingsPanel.CategoryList.ScrollBar.Track.End:Hide()
-SettingsPanel.CategoryList.ScrollBar.Track.Middle:Hide()
-
-ApplyScrollBarArrow(SettingsPanel.CategoryList.ScrollBar, true)
-ApplyScrollBarThumb(SettingsPanel.CategoryList.ScrollBar.Track.Thumb)
