88544fa3ff5b1726b2fe752a1f0e35941501e9e6
Daenarys <androidwoz@gmail.com>
2026-04-22 13:55:38 +0200

    skin scrollbars

--- ClassicFrames/skins/SettingsPanel.lua
+++ ClassicFrames/skins/SettingsPanel.lua
@@ -4,3 +4,17 @@ ApplyCloseButton(SettingsPanel.ClosePanelButton)
 ApplyTitleBg(SettingsPanel, true)
 ApplyNineSliceNoPortrait(SettingsPanel)
 SettingsPanel.NineSlice:SetFrameStrata("HIGH")
+
+SettingsPanel.Container.SettingsList.ScrollBar.Track.Begin:Hide()
+SettingsPanel.Container.SettingsList.ScrollBar.Track.End:Hide()
+SettingsPanel.Container.SettingsList.ScrollBar.Track.Middle:Hide()
+
+ApplyScrollBarArrow(SettingsPanel.Container.SettingsList.ScrollBar, true)
+ApplyScrollBarThumb(SettingsPanel.Container.SettingsList.ScrollBar.Track.Thumb)
+
+SettingsPanel.CategoryList.ScrollBar.Track.Begin:Hide()
+SettingsPanel.CategoryList.ScrollBar.Track.End:Hide()
+SettingsPanel.CategoryList.ScrollBar.Track.Middle:Hide()
+
+ApplyScrollBarArrow(SettingsPanel.CategoryList.ScrollBar, true)
+ApplyScrollBarThumb(SettingsPanel.CategoryList.ScrollBar.Track.Thumb)
