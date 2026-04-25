088d6014e5c44b0786472134b65f363a5181bab4
Daenarys <androidwoz@gmail.com>
2026-04-25 14:48:34 +0200

    improved cinematic/movie frames

--- ClassicFrames/skins/Cinematic.lua
+++ ClassicFrames/skins/Cinematic.lua
@@ -1,8 +1,15 @@
 if _G.CinematicFrame then
+	CinematicFrameCloseDialogConfirmButton:SetSize(119, 21)
+	CinematicFrameCloseDialogResumeButton:SetSize(119, 21)
+
 	ApplyDialogBorder(CinematicFrame.closeDialog.Border)
 end
 
 if _G.MovieFrame then
 	MovieFrame.CloseDialog.BackgroundTile:SetAlpha(0)
+
+	MovieFrame.CloseDialog.Buttons.ConfirmButton:SetSize(119, 21)
+	MovieFrame.CloseDialog.Buttons.ResumeButton:SetSize(119, 21)
+	
 	ApplyDialogBorder(MovieFrame.CloseDialog.Border)
 end
