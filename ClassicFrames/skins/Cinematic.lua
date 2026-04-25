97be8a94e01265a056e9bd091848db6b9e838f62
Daenarys <androidwoz@gmail.com>
2026-04-25 11:55:07 +0200

    cleanup

--- ClassicFrames/skins/Cinematic.lua
+++ ClassicFrames/skins/Cinematic.lua
@@ -3,10 +3,6 @@ if _G.CinematicFrame then
 end
 
 if _G.MovieFrame then
-	MovieFrame.CloseDialog.topPadding = 15
-	MovieFrame.CloseDialog.leftPadding = 15
-	MovieFrame.CloseDialog.rightPadding = 15
-	MovieFrame.CloseDialog.bottomPadding = 15
 	MovieFrame.CloseDialog.BackgroundTile:SetAlpha(0)
 	ApplyDialogBorder(MovieFrame.CloseDialog.Border)
 end
