c73727773f10696c7d43692fff594a72004ff5e0
Daenarys <androidwoz@gmail.com>
2026-05-21 11:13:53 +0200

    update menu checks

--- ClassicFrames/skins/Menu.lua
+++ ClassicFrames/skins/Menu.lua
@@ -133,6 +133,13 @@ if manager then
 	hooksecurefunc(manager, "OpenContextMenu", SkinMenu)
 end
 
+local ATLAS_MAP = {
+	["common-dropdown-ticksquare"]             = {0.5,1,0,0.5},
+	["common-dropdown-icon-checkmark-yellow"]  = {0,0.5,0,0.5},
+	["common-dropdown-tickradial"]             = {0.5,1, 0.5,1},
+	["common-dropdown-icon-radialtick-yellow"] = {0,0.5,0.5,1},
+}
+
 hooksecurefunc(CompositorMixin, "AttachFontString", function(parent)
 	if not parent.attachments then return end
 
@@ -140,6 +147,13 @@ hooksecurefunc(CompositorMixin, "AttachFontString", function(parent)
 		local widgetType = widget:GetObjectType()
 		if widgetType == "FontString" then
 			widget:SetFontObject(GameFontHighlightSmallLeft)
+		elseif widgetType == "Texture" then
+			local coords = ATLAS_MAP[widget:GetAtlas()]
+			if coords then
+				widget:SetSize(16, 16)
+				widget:SetTexture([[Interface\Common\UI-DropDownRadioChecks]])
+				widget:SetTexCoord(unpack(coords))
+			end
 		end
 	end
 end)
