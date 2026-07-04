7d5fbb3a86c30ac610c093616f6adefc6c96b064
Daenarys <androidwoz@gmail.com>
2026-07-04 15:30:45 +0200

    update mirrortimer skin

--- ClassicFrames/skins/MirrorTimers.lua
+++ ClassicFrames/skins/MirrorTimers.lua
@@ -1,36 +1,37 @@
 if not _G.MirrorTimerContainer then return end
 
-hooksecurefunc(MirrorTimerContainer, "SetupTimer", function(self, timer, value, maxvalue, scale, paused, label)
-	local dialog
-	for index = 1, #MirrorTimerContainer.mirrorTimers, 1 do
-		local frame = self:GetAvailableTimer(timer)
-		if ( frame:IsShown() and (frame.timer == timer) ) then
-			dialog = frame;
-			break;
-		end
-	end
+local MirrorTimerColors = {
+	EXHAUSTION = { r = 1.00, g = 0.90, b = 0.00 },
+	BREATH = { r = 0.00, g = 0.50, b = 1.00 },
+	DEATH = { r = 1.00, g = 0.70, b = 0.00 },
+	FEIGNDEATH = { r = 1.00, g = 0.70, b = 0.00 },
+}
 
-	dialog:SetSize(206, 26)
-	dialog.TextBorder:Hide()
-	dialog.Text:ClearAllPoints()
-	dialog.Text:SetPoint("CENTER", dialog, "CENTER", 0, 7)
-	dialog.Text:SetFont(GameFontHighlight:GetFont())
-	select(2,dialog:GetRegions()):SetColorTexture(0, 0, 0, 0.5)
-	dialog.StatusBar:SetSize(195, 13)
-	dialog.StatusBar:ClearAllPoints()
-	dialog.StatusBar:SetPoint('TOP', 0, -2)
-	dialog.StatusBar:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
-	dialog.Border:SetTexture("Interface\\CastingBar\\UI-CastingBar-Border")
-	dialog.Border:SetSize(256, 64)
-	dialog.Border:ClearAllPoints()
-	dialog.Border:SetPoint("TOP", 0, 25)
-	if (timer == "EXHAUSTION") then
-		dialog.StatusBar:SetStatusBarColor(1.00, 0.90, 0.00)
-	elseif (timer == "BREATH") then
-		dialog.StatusBar:SetStatusBarColor(0.00, 0.50, 1.00)
-	elseif (timer == "DEATH") then
-		dialog.StatusBar:SetStatusBarColor(1.00, 0.70, 0.00)
-	elseif (timer == "FEIGNDEATH") then
-		dialog.StatusBar:SetStatusBarColor(1.00, 0.70, 0.00)
-	end
-end)
+local function SetupTimer(container, timer)
+	local bar = container:GetAvailableTimer(timer)
+	if not bar then return end
+
+	bar:SetSize(206, 26)
+
+	select(2, bar:GetRegions()):SetColorTexture(0, 0, 0, 0.5)
+
+	bar.Border:SetSize(256, 64)
+	bar.Border:SetTexture("Interface\\CastingBar\\UI-CastingBar-Border")
+	bar.Border:ClearAllPoints()
+	bar.Border:SetPoint("TOP", 0, 25)
+
+	local color = MirrorTimerColors[timer]
+	bar.StatusBar:SetSize(195, 13)
+	bar.StatusBar:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
+	bar.StatusBar:SetStatusBarColor(color.r, color.g, color.b)
+	bar.StatusBar:ClearAllPoints()
+	bar.StatusBar:SetPoint("TOP", 0, -2)
+
+	bar.Text:SetFont(GameFontHighlight:GetFont())
+	bar.Text:ClearAllPoints()
+	bar.Text:SetPoint("TOP")
+
+	bar.TextBorder:SetAlpha(0)
+end
+
+hooksecurefunc(_G.MirrorTimerContainer, "SetupTimer", SetupTimer)
