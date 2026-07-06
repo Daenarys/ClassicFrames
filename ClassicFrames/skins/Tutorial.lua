dba3a87c4e7392f999f1f3d2a6393b9df4df66aa
Daenarys <androidwoz@gmail.com>
2026-07-06 22:36:37 +0200

    skin tutorialframe closebutton

---
+++ ClassicFrames/skins/Tutorial.lua
@@ -0,0 +1,5 @@
+if not _G.TutorialFrame then return end
+
+ApplyCloseButton(TutorialFrameCloseButton)
+TutorialFrameCloseButton:ClearAllPoints()
+TutorialFrameCloseButton:SetPoint("TOPRIGHT", 4, -8)
