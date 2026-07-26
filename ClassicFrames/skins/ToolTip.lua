fdbd034df045ae3cccf23db2ff7bb6908143c1e5
Daenarys <androidwoz@gmail.com>
2026-07-26 18:55:43 +0200

    do this instead

--- ClassicFrames/skins/ToolTip.lua
+++ ClassicFrames/skins/ToolTip.lua
@@ -1,3 +1,16 @@
+hooksecurefunc("GameTooltip_OnTooltipAddMoney", function(self, cost)
+	local name = self:GetName()
+	local numLines = self:NumLines()
+
+	local line = _G[name .. "TextLeft" .. numLines]
+	if line then
+		local text = line:GetText()
+		if text then
+			line:SetText(string.format("%s: %s", SELL_PRICE, C_CurrencyInfo.GetCoinTextureString(cost, 13)))
+		end
+	end
+end)
+
 function UnitFrame_UpdateTooltip(self)
 	GameTooltip_SetDefaultAnchor(GameTooltip, self)
 	if ( GameTooltip:SetUnit(self.unit, self.hideStatusOnTooltip) ) then
@@ -43,41 +56,6 @@ TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Item, function(self
 			local lN, rN = _G[name.."TextLeft"..i], _G[name.."TextRight"..i]
 			if lN then lN:SetFontObject("GameTooltipTextSmall") end
 			if rN then rN:SetFontObject("GameFontHighlightSmall") end
-		end
-	end
-end)
-
-TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Item, function(self)
-	if self == _G.GameTooltip and self.GetItem then
-		local name = self:GetName()
-
-		local _, itemLink = self:GetItem()
-		if not itemLink then return end
-
-		local sellPrice = select(11, GetItemInfo(itemLink))
-		if not sellPrice or sellPrice <= 0 then return end
-
-		local coinString = C_CurrencyInfo.GetCoinTextureString(sellPrice, 12)
-
-		for i = 2, self:NumLines() do
-			local line = _G[name .. "TextLeft" .. i]
-			if line then
-				local text = line:GetText()
-				if text and text:find("Sell Price") then
-					line:SetText(("%s:"):format(SELL_PRICE))
-					if not priceFS then
-						priceFS = self:CreateFontString(nil, "OVERLAY")
-						priceFS:SetFontObject(NumberFontNormalRight)
-						self:HookScript("OnTooltipCleared", function()
-							priceFS:Hide()
-						end)
-					end
-					priceFS:ClearAllPoints()
-					priceFS:SetPoint("LEFT", line, "LEFT", 73, 0)
-					priceFS:SetText(coinString)
-					priceFS:Show()
-				end
-			end
 		end
 	end
 end)
