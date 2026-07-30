dfc1df0567a87a8c0da5e9d9cc6a556ea76354d8
Daenarys <androidwoz@gmail.com>
2026-07-30 19:51:00 +0200

    remove this

--- ClassicFrames/skins/RPE.lua
+++
@@ -1,9 +0,0 @@
-if RPETurnStrafeStyleMixin then
-	hooksecurefunc(RPETurnStrafeStyleMixin, "OnLoad", function(self)
-		self.BG.Top:SetAtlas("UI-DiamondDialogBox-ClassicBorder")
-
-		ApplyCloseButton(self.CloseButton, true)
-		self.CloseButton:ClearAllPoints()
-		self.CloseButton:SetPoint("TOPRIGHT", -4, -4)
-	end)
-end
