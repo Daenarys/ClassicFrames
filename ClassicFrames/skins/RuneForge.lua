d293fe888a2dccc61b4cb5b755767e204698333a
Daenarys <androidwoz@gmail.com>
2026-08-07 13:27:55 +0200

    remove old expansion stuff

--- ClassicFrames/skins/RuneForge.lua
+++
@@ -1,7 +0,0 @@
-local f = CreateFrame("Frame")
-f:RegisterEvent("ADDON_LOADED")
-f:SetScript("OnEvent", function(self, event, name)
-	if name == "Blizzard_RuneforgeUI" then
-		ApplyCloseButton(RuneforgeFrame.CloseButton, true)
-	end
-end)
