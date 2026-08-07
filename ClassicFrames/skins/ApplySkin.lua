1209f27e47b1ba0b447df1c79313f3f7b0c98b89
Daenarys <androidwoz@gmail.com>
2026-08-07 12:42:11 +0200

    button improvements

--- ClassicFrames/skins/ApplySkin.lua
+++ ClassicFrames/skins/ApplySkin.lua
@@ -917,6 +917,7 @@ end
 
 function ApplyCinematicButton(frame)
 	frame:SetSize(119, 21)
+	frame:SetPushedTextOffset(1.57, -1.57)
 
 	frame.Left:SetAlpha(0)
 	frame.Right:SetAlpha(0)
@@ -933,15 +934,15 @@ function ApplyCinematicButton(frame)
 end
 
 function ApplyRedButton(frame)
+	frame:SetPushedTextOffset(1.57, -1.57)
+
 	frame.Left:SetAlpha(0)
-	frame.Right:SetAlpha(0)
 	frame.Center:SetAlpha(0)
+	frame.Right:SetAlpha(0)
 
 	if (frame.CfLeft == nil) then
 		frame.CfLeft = frame:CreateTexture(nil, "BACKGROUND")
 		frame.CfLeft:SetSize(12, 22)
-		frame.CfLeft:SetTexture("Interface\\Buttons\\UI-Panel-Button-Up")
-		frame.CfLeft:SetTexCoord(0, 0.09375, 0, 0.6875)
 		frame.CfLeft:SetPoint("TOPLEFT")
 		frame.CfLeft:SetPoint("BOTTOMLEFT")
 	end
@@ -949,19 +950,37 @@ function ApplyRedButton(frame)
 	if (frame.CfRight == nil) then
 		frame.CfRight = frame:CreateTexture(nil, "BACKGROUND")
 		frame.CfRight:SetSize(12, 22)
-		frame.CfRight:SetTexture("Interface\\Buttons\\UI-Panel-Button-Up")
-		frame.CfRight:SetTexCoord(0.53125, 0.625, 0, 0.6875)
 		frame.CfRight:SetPoint("TOPRIGHT")
 		frame.CfRight:SetPoint("BOTTOMRIGHT")
 	end
 
 	if (frame.CfMiddle == nil) then
 		frame.CfMiddle = frame:CreateTexture(nil, "BACKGROUND")
-		frame.CfMiddle:SetTexture("Interface\\Buttons\\UI-Panel-Button-Up")
-		frame.CfMiddle:SetTexCoord(0.09375, 0.53125, 0, 0.6875)
 		frame.CfMiddle:SetPoint("TOPLEFT", frame.CfLeft, "TOPRIGHT")
 		frame.CfMiddle:SetPoint("BOTTOMRIGHT", frame.CfRight, "BOTTOMLEFT")
 	end
+
+	frame:HookScript("OnUpdate", function()
+		local buttonState = frame:GetButtonState()
+
+		frame.CfLeft:SetTexCoord(0, 0.09375, 0, 0.6875)
+		frame.CfRight:SetTexCoord(0.53125, 0.625, 0, 0.6875)
+		frame.CfMiddle:SetTexCoord(0.09375, 0.53125, 0, 0.6875)
+
+		if buttonState == "DISABLED" then
+			frame.CfLeft:SetTexture("Interface\\Buttons\\UI-Panel-Button-Disabled")
+			frame.CfMiddle:SetTexture("Interface\\Buttons\\UI-Panel-Button-Disabled")
+			frame.CfRight:SetTexture("Interface\\Buttons\\UI-Panel-Button-Disabled")
+		elseif buttonState == "PUSHED" then
+			frame.CfLeft:SetTexture("Interface\\Buttons\\UI-Panel-Button-Down")
+			frame.CfMiddle:SetTexture("Interface\\Buttons\\UI-Panel-Button-Down")
+			frame.CfRight:SetTexture("Interface\\Buttons\\UI-Panel-Button-Down")
+		else
+			frame.CfLeft:SetTexture("Interface\\Buttons\\UI-Panel-Button-Up")
+			frame.CfMiddle:SetTexture("Interface\\Buttons\\UI-Panel-Button-Up")
+			frame.CfRight:SetTexture("Interface\\Buttons\\UI-Panel-Button-Up")
+		end
+	end)
 end
 
 function ApplySquareButton(frame)
