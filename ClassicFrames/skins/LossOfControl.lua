a824bd21d53ac5a75c9e73192b9bab77ce13258b
Daenarys <androidwoz@gmail.com>
2026-08-14 15:57:18 +0200

    update swipe color

--- ClassicFrames/skins/LossOfControl.lua
+++ ClassicFrames/skins/LossOfControl.lua
@@ -1,3 +1,4 @@
 if not _G.LossOfControlFrame then return end
 
 LossOfControlFrame.Cooldown:SetEdgeTexture("Interface\\Cooldown\\edge-LoC")
+LossOfControlFrame.Cooldown:SetSwipeColor(0.17, 0, 0, 0.8)
