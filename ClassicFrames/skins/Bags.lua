b184ed6e176d8d7ccfffaf4d5b27562dbb6e3904
Daenarys <androidwoz@gmail.com>
2026-08-22 15:49:21 +0200

    update container cd stuff

--- ClassicFrames/skins/Bags.lua
+++ ClassicFrames/skins/Bags.lua
@@ -98,6 +98,8 @@ for i = 1, _G.NUM_CONTAINER_FRAMES do
 
 	hooksecurefunc(_G['ContainerFrame'..i], "UpdateItemLayout", function(self)
 		for i, itemButton in self:EnumerateValidItems() do
+			itemButton.Cooldown:SetBlingTexture("Interface\\Cooldown\\star4", 0.3, 0.6, 1, 0.8)
+			itemButton.Cooldown:SetSwipeColor(0, 0, 0, 0.8)
 			itemButton.Cooldown:SetHideCountdownNumbers(true)
 			itemButton.emptyBackgroundAtlas = nil
 		end
