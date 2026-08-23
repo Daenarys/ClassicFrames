82d3a44ab114bee73db9f9a7970932b5d8cf8144
Daenarys <androidwoz@gmail.com>
2026-08-23 15:53:04 +0200

    update text if unit is corpse

--- ClassicFrames/skins/ToolTip.lua
+++ ClassicFrames/skins/ToolTip.lua
@@ -66,24 +66,27 @@ TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Unit, function(self
 		local numLines = self:NumLines()
 
 		local _, unit = self:GetUnit()
-		if not unit or issecretvalue(unit) or not UnitIsPlayer(unit) then return end
+		if not unit or issecretvalue(unit) then return end
 
 		local className = UnitClass(unit)
 		if not className then return end
 
-		for i = 2, numLines - 1 do
-			local levelLine = _G[name .. "TextLeft" .. i]
-			local levelText = levelLine and levelLine:GetText()
-			if levelText and levelText:find("^Level") then
-				local classLine = _G[name .. "TextLeft" .. (i + 1)]
-				local classText = classLine and classLine:GetText()
-				if classText and classText:find(className, 1, true) then
-					levelLine:SetText((levelText:gsub("%s*%(Player%)$", "")) .. " " .. className .. " (Player)")
-					for j = i + 1, numLines - 1 do
-						_G[name .. "TextLeft" .. j]:SetText(_G[name .. "TextLeft" .. (j + 1)]:GetText())
+		local isCorpse = UnitIsCorpse(unit)
+
+		if UnitIsPlayer(unit) then
+			for i = 2, numLines - 1 do
+				local levelLine = _G[name .. "TextLeft" .. i]
+				local levelText = levelLine and levelLine:GetText()
+				if levelText and levelText:find("^Level") then
+					local classLine = _G[name .. "TextLeft" .. (i + 1)]
+					local classText = classLine and classLine:GetText()
+					if classText and ((isCorpse and classText == _G.CORPSE) or (not isCorpse and classText:find(className, 1, true))) then
+						levelLine:SetText((levelText:gsub("%s*%(Player%)$", "")) .. " " .. (isCorpse and classText or className) .. " (Player)")
+						for j = i + 1, numLines - 1 do
+							_G[name .. "TextLeft" .. j]:SetText(_G[name .. "TextLeft" .. (j + 1)]:GetText())
+						end
+						_G[name .. "TextLeft" .. numLines]:Hide()
 					end
-					local last = _G[name .. "TextLeft" .. numLines]
-					last:Hide()
 				end
 			end
 		end
