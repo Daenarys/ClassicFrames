2fd93c8c58fe8165004ee7bfb7742a61119dddc9
Daenarys <androidwoz@gmail.com>
2026-09-19 11:55:38 +0200

    cleanup

--- ClassicFrames/skins/LossOfControl.lua
+++
@@ -1,4 +0,0 @@
-if not _G.LossOfControlFrame then return end
-
-LossOfControlFrame.Cooldown:SetEdgeTexture("Interface\\Cooldown\\edge-LoC")
-LossOfControlFrame.Cooldown:SetSwipeColor(0.17, 0, 0, 0.8)
--- ClassicFrames/skins/SplashScreen.lua
+++
@@ -1,5 +0,0 @@
-if not _G.SplashFrame then return end
-
-ApplyCloseButton(SplashFrame.TopCloseButton, true)
-SplashFrame.TopCloseButton:ClearAllPoints()
-SplashFrame.TopCloseButton:SetPoint("TOPRIGHT", -14, -10)
