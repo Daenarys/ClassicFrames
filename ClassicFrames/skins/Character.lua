b76d04c0e5333a0629b149b0867f8f32e23030e0
Daenarys <androidwoz@gmail.com>
2026-09-19 10:43:10 +0200

    character frame cleanup

--- ClassicFrames/skins/Character.lua
+++ ClassicFrames/skins/Character.lua
@@ -13,40 +13,19 @@ CharacterFrame.TitleContainer:SetPoint("TOPRIGHT", CharacterFrame, "TOPRIGHT", -
 ApplyTitleBg(CharacterFrame)
 ApplyNineSlicePortrait(CharacterFrame)
 
-ReputationFrame.ScrollBar:SetSize(25, 560)
-ReputationFrame.ScrollBar:ClearAllPoints()
-ReputationFrame.ScrollBar:SetPoint("TOPLEFT", ReputationFrame.ScrollBox, "TOPRIGHT", -4, 0)
-ReputationFrame.ScrollBar:SetPoint("BOTTOMLEFT", ReputationFrame.ScrollBox, "BOTTOMRIGHT", 4, -3)
+ApplyScrollBarHybrid(CharacterStatsPaneScrollBox.ScrollBar, true)
+ApplyScrollBarThumb(CharacterStatsPaneScrollBox.ScrollBar.Track.Thumb)
 
-ApplyScrollBarArrow(ReputationFrame.ScrollBar)
-ApplyScrollBarTrack(ReputationFrame.ScrollBar.Track)
+ApplyScrollBarHybrid(ReputationFrame.ScrollBar, true)
 ApplyScrollBarThumb(ReputationFrame.ScrollBar.Track.Thumb)
 
-SkillsFrame.ScrollBar:SetSize(25, 560)
-SkillsFrame.ScrollBar:ClearAllPoints()
-SkillsFrame.ScrollBar:SetPoint("TOPLEFT", SkillsFrame.ScrollBox, "TOPRIGHT", -4, 0)
-SkillsFrame.ScrollBar:SetPoint("BOTTOMLEFT", SkillsFrame.ScrollBox, "BOTTOMRIGHT", 4, -3)
-
-ApplyScrollBarArrow(SkillsFrame.ScrollBar)
-ApplyScrollBarTrack(SkillsFrame.ScrollBar.Track)
+ApplyScrollBarHybrid(SkillsFrame.ScrollBar, true)
 ApplyScrollBarThumb(SkillsFrame.ScrollBar.Track.Thumb)
 
-TokenFrame.ScrollBar:SetSize(25, 560)
-TokenFrame.ScrollBar:ClearAllPoints()
-TokenFrame.ScrollBar:SetPoint("TOPLEFT", TokenFrame.ScrollBox, "TOPRIGHT", -4, 0)
-TokenFrame.ScrollBar:SetPoint("BOTTOMLEFT", TokenFrame.ScrollBox, "BOTTOMRIGHT", 4, -3)
-
-ApplyScrollBarArrow(TokenFrame.ScrollBar)
-ApplyScrollBarTrack(TokenFrame.ScrollBar.Track)
+ApplyScrollBarHybrid(TokenFrame.ScrollBar, true)
 ApplyScrollBarThumb(TokenFrame.ScrollBar.Track.Thumb)
 
-StatisticsFrame.ScrollBar:SetSize(25, 560)
-StatisticsFrame.ScrollBar:ClearAllPoints()
-StatisticsFrame.ScrollBar:SetPoint("TOPLEFT", StatisticsFrame.ScrollBox, "TOPRIGHT", -4, 0)
-StatisticsFrame.ScrollBar:SetPoint("BOTTOMLEFT", StatisticsFrame.ScrollBox, "BOTTOMRIGHT", 4, -3)
-
-ApplyScrollBarArrow(StatisticsFrame.ScrollBar)
-ApplyScrollBarTrack(StatisticsFrame.ScrollBar.Track)
+ApplyScrollBarHybrid(StatisticsFrame.ScrollBar, true)
 ApplyScrollBarThumb(StatisticsFrame.ScrollBar.Track.Thumb)
 
 ApplyScrollBarHybrid(PaperDollFrame.TitleManagerPane.ScrollBar, true)
@@ -64,15 +43,8 @@ ApplyScrollBarArrow(GearManagerPopupFrame.IconSelector.ScrollBar)
 ApplyScrollBarTrack(GearManagerPopupFrame.IconSelector.ScrollBar.Track)
 ApplyScrollBarThumb(GearManagerPopupFrame.IconSelector.ScrollBar.Track.Thumb)
 
-ApplyScrollBarHybrid(CharacterStatsPaneScrollBox.ScrollBar, true)
-ApplyScrollBarThumb(CharacterStatsPaneScrollBox.ScrollBar.Track.Thumb)
-
 ApplyDropDown(GearManagerPopupFrame.BorderBox.IconTypeDropdown)
 GearManagerPopupFrame.BorderBox.IconTypeDropdown.Text:SetJustifyH("RIGHT")
-
-CharacterModelScene.ControlFrame:HookScript("OnShow", function(self)
-	self:SetPoint("TOP")
-end)
 
 PaperDollFrame:HookScript("OnShow", function()
 	CharacterModelScene.ControlFrame:Hide()
